8ee5dcf59b0101eacd129d06625cb4067521b9a9
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-13 00:55:05 +0300

    Added react native elements

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./StackNavigator";
 import { Provider } from "react-redux";
 import store from "./store";
-
+import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <TailwindProvider>
-          <StackNavigator />
-        </TailwindProvider>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <TailwindProvider>
+            <StackNavigator />
+          </TailwindProvider>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </Provider>
   );
 }
