2655cc928d83e00c11ba1a1518e741602c8237be
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-12 16:22:41 +0300

    clicked button is working

--- components/NavbarButton.js
+++ components/NavbarButton.js
@@ -4,7 +4,7 @@ import { useNavigation } from "@react-navigation/native";
 
 const NavbarButton = (props) => {
   const navigation = useNavigation();
-  if (props.currentScreen != props.title) {
+  if (props.currentScreen != props.screen) {
     return (
       <TouchableOpacity
         onPress={() => navigation.navigate(props.screen)}
@@ -13,9 +13,24 @@ const NavbarButton = (props) => {
         <Text>{props.screen}</Text>
       </TouchableOpacity>
     );
+  } else {
+    console.log("this button is clicked already");
+    return (
+      <TouchableOpacity style={styles.currentPageButton}>
+        <Text>{props.screen}</Text>
+      </TouchableOpacity>
+    );
   }
 };
 const styles = StyleSheet.create({
+  currentPageButton: {
+    alignItems: "center",
+    backgroundColor: "#46a29f",
+    padding: 10,
+    flex: 1,
+    borderColor: "#202832",
+    borderWidth: 2,
+  },
   button: {
     alignItems: "center",
     backgroundColor: "#66fcf1",
