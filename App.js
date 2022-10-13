a69e6455bf042c4fd7e910dddda89c37a3d8ba1f
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-13 09:16:28 +0300

    Removed problemtaic packages

--- App.js
+++ App.js
@@ -3,17 +3,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./StackNavigator";
 import { Provider } from "react-redux";
 import store from "./store";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <TailwindProvider>
-            <StackNavigator />
-          </TailwindProvider>
-        </NavigationContainer>
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <TailwindProvider>
+          <StackNavigator />
+        </TailwindProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
