657bd5f5982e03a9bd545f5cde19c709926cb225
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-13 16:25:28 +0300

    workout minutes created

---
+++ components/WorkoutMinutes.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const WorkoutMinutes = () => {
+  return (
+    <View>
+      <Text>WorkoutMinutes</Text>
+    </View>
+  );
+};
+
+export default WorkoutMinutes;
