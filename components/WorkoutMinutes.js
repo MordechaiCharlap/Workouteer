71ef996f1f3d10f731a73678ba0ea61ef38673ad
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-13 16:32:22 +0300

    workoutMinutes dropdown added succesfuly

--- components/WorkoutMinutes.js
+++ components/WorkoutMinutes.js
@@ -1,12 +1,96 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { React, useState } from "react";
+import { Dropdown } from "react-native-element-dropdown";
+
+const data = [
+  { label: "0:30", value: "30" },
+  { label: "1:00", value: "60" },
+  { label: "1:30", value: "90" },
+  { label: "2:00", value: "120" },
+  { label: "2:30", value: "150" },
+  { label: "3:00", value: "180" },
+];
 
 const WorkoutMinutes = () => {
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const renderLabel = () => {
+    if (value || isFocus) {
+      return (
+        <Text style={[styles.label, isFocus && { color: "blue" }]}>
+          hours:minutes
+        </Text>
+      );
+    }
+    return null;
+  };
+
   return (
-    <View>
-      <Text>WorkoutMinutes</Text>
+    <View style={styles.container}>
+      {renderLabel()}
+      <Dropdown
+        style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={data}
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder={!isFocus ? "Select item" : "..."}
+        searchPlaceholder="Search..."
+        value={value}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={(item) => {
+          setValue(item.value);
+          setIsFocus(false);
+        }}
+      />
     </View>
   );
 };
 
 export default WorkoutMinutes;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 16,
+  },
+  dropdown: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: "absolute",
+    backgroundColor: "white",
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
