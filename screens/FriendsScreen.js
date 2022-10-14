bf0e600507f635a64fc94e01360038f9ecdc2d9e
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-14 23:08:13 +0300

    Friends screen added view per friend

--- screens/FriendsScreen.js
+++ screens/FriendsScreen.js
@@ -1,15 +1,55 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, FlatList } from "react-native";
+import { React, useLayoutEffect } from "react";
 import BottomNavbar from "../components/BottomNavbar";
+import { useNavigation } from "@react-navigation/native";
+
+const friendsData = [
+  {
+    id: 123,
+    username: "AlmogVoid",
+    workouts: 7,
+    league: "Silver",
+    img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    isConnected: false,
+  },
+  {
+    id: 456,
+    username: "MisterBean",
+    workouts: 3,
+    league: "Bronze",
+    img: "https://www.pexels.com/photo/monochrome-photography-of-person-laughing-1484799/",
+    isConnected: false,
+  },
+  {
+    id: 789,
+    username: "Lagertha",
+    workouts: 15,
+    league: "Diamond",
+    img: "https://historycouk.s3.eu-west-2.amazonaws.com/s3fs-public/2020-07/lagertha-interview.jpg",
+    isConnected: true,
+  },
+];
 
 const FriendsScreen = () => {
-  return (
-    <SafeAreaView>
-      <View className="flex-1">
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
+  return (
+    <SafeAreaView className="flex-1 bg-cyan-900">
+      <View className="flex-1">
+        <FlatList
+          data={friendsData}
+          keyExtractor={(item) => item.id}
+          renderItem={(user) => (
+            <View className="bg-slate-500 border-2 rounded w-full h-24 m-4"></View>
+          )}
+        />
       </View>
-      <BottomNavbar/>
-      <Text>FriendsScreen</Text>
+      <BottomNavbar />
     </SafeAreaView>
   );
 };
