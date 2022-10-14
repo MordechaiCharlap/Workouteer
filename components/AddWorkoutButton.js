039751941a395654c05b6a9723b9d2f0c927a3e8
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-14 16:18:12 +0300

    Animation finally fixed on the PlusButton

--- components/AddWorkoutButton.js
+++ components/AddWorkoutButton.js
@@ -1,26 +1,27 @@
 import { View, Text, TouchableOpacity, Animated } from "react-native";
-import { React, useRef } from "react";
+import { React, useRef, useState, useEffect } from "react";
 import { PlusCircleIcon } from "react-native-heroicons/solid";
 import NewWorkout from "./NewWorkout";
 
 const AddWorkoutButton = () => {
   const addWorkoutOpacity = useRef(new Animated.Value(0)).current;
-  const plusSize = useRef(new Animated.Value(200)).current;
+  const plusSize = useRef(new Animated.Value(300)).current;
   const marginTopRef = useRef(new Animated.Value(200)).current;
-
+  const plusViewOpa = new Animated.Value(1);
+  const animationMiliSec = 1500;
   const addWorkoutIn = () => {
     console.log("Queue animation");
     Animated.timing(plusSize, {
       toValue: 50,
-      duration: 2000,
+      duration: animationMiliSec,
     }).start();
     Animated.timing(addWorkoutOpacity, {
       toValue: 1,
-      duration: 2000,
+      duration: animationMiliSec,
     }).start();
     Animated.timing(marginTopRef, {
       toValue: 20,
-      duration: 2000,
+      duration: animationMiliSec,
     }).start();
   };
   return (
@@ -28,7 +29,15 @@ const AddWorkoutButton = () => {
       <Animated.View style={{ marginTop: marginTopRef }}>
         <Text className="font-bold text-center text-4xl">Add a workout</Text>
         <TouchableOpacity className="items-center" onPress={addWorkoutIn}>
-          <PlusCircleIcon size={plusSize._value} color="#03dac6" />
+          <Animated.View
+            style={{
+              opacity: plusViewOpa,
+              width: plusSize,
+              height: plusSize,
+            }}
+          >
+            <PlusCircleIcon size={plusSize} color="#03dac6" />
+          </Animated.View>
         </TouchableOpacity>
         <NewWorkout opacity={addWorkoutOpacity} />
       </Animated.View>
