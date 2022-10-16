dbe75b0abfcb42c484f8c3972d0fbc7bf6fee741
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-16 23:35:55 +0300

    Responsive stylng name change

---
+++ components/ResponsiveStyling.js
@@ -0,0 +1,30 @@
+import { Platform, StyleSheet, StatusBar } from "react-native";
+import * as appStyle from "./AppStyleSheet";
+var style = StyleSheet.create({});
+if (Platform.OS == "web") {
+  style = StyleSheet.create({
+    safeAreaStyle: {
+      height: "100%",
+      alignSelf: "center",
+      aspectRatio: "9/19",
+      backgroundColor: appStyle.appAzure,
+    },
+    newWorkoutScrollView: {
+      backgroundColor: appStyle.appGray,
+      width: "90%",
+      height: 550,
+    },
+  });
+} else if (Platform.OS == "android")
+  style = StyleSheet.create({
+    safeAreaStyle: {
+      height: "100%",
+      flex: 1,
+      backgroundColor: appStyle.appAzure,
+    },
+    newWorkoutScrollView: {
+      backgroundColor: appStyle.appGray,
+      width: "90%",
+    },
+  });
+export default style;
