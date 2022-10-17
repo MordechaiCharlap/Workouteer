c2b837b5cda582267e4d91183af7b5bbf859e4f5
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-17 14:22:02 +0300

    Login screen layout

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,37 @@
-import { SafeAreaView, Text } from "react-native";
-import React from "react";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { React, useLayoutEffect } from "react";
 import BottomNavbar from "../components/BottomNavbar";
+import { useNavigation } from "@react-navigation/native";
 import ResponsiveStyling from "../components/ResponsiveStyling";
+import * as appStyle from "../components/AppStyleSheet";
 const LoginScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
   return (
-    <SafeAreaView>
-      <Text>LoginScreen</Text>
-      <BottomNavbar />
+    <SafeAreaView style={ResponsiveStyling.safeAreaStyle}>
+      <View className="flex-1 my-32 mx-6">
+        <View
+          className="mb-5 h-4/5 rounded-t-xl "
+          style={{ backgroundColor: appStyle.appDarkBlue }}
+        ></View>
+        <TouchableOpacity
+          className="h-1/5 rounded-b-xl justify-center "
+          style={{ backgroundColor: appStyle.appDarkBlue }}
+        >
+          <Text
+            style={{ color: appStyle.appLightBlue }}
+            className="text-center tracking-widest font-bold text-xl"
+          >
+            REGISTER
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <BottomNavbar currentScreen="Login" />
     </SafeAreaView>
   );
 };
