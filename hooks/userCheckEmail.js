20f3779ea9a2c6014a91154d9e7b8c74ae802dea
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-20 09:47:47 +0300

    email instead of user

--- hooks/userCheckEmail.js
+++ hooks/userCheckEmail.js
@@ -3,10 +3,7 @@ import { collection, query, where, getDocs, getDoc } from "firebase/firestore";
 async function useCheckEmail(email) {
   const firestore = firestoreImport;
   const usersRef = collection(firestore, "users");
-  const q = query(
-    usersRef,
-    where("usernameLower", "==", username.toLowerCase())
-  );
+  const q = query(usersRef, where("email", "==", email));
   const querySnapshot = await getDocs(q);
   if (querySnapshot.size > 0) {
     return false;
