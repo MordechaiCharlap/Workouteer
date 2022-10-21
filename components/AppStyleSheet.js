f5059bb2b9a95c4e703d95c60d9b79013bcb4dc0
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-21 13:45:02 +0300

    Chats page is up and running, we got upper bar!

--- components/AppStyleSheet.js
+++ components/AppStyleSheet.js
@@ -4,3 +4,4 @@ export const appDarkBlue = "#14213D";
 export const appLightBlue = "#8ecae6";
 export const appAzure = "#219ebc";
 export const appGray = "#E5E5E5";
+export const appDarkBlueGrayer = "#4d5973";
--- screens/ChatsScreen.js
+++ screens/ChatsScreen.js
@@ -3,7 +3,10 @@ import { useNavigation } from "@react-navigation/native";
 import { React, useLayoutEffect } from "react";
 import ResponsiveStyling from "../components/ResponsiveStyling";
 import BottomNavbar from "../components/BottomNavbar";
-
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import * as appStyle from "../components/AppStyleSheet";
 const ChatsScreen = () => {
   const navigation = useNavigation();
   useLayoutEffect(() => {
@@ -13,7 +16,28 @@ const ChatsScreen = () => {
   });
   return (
     <SafeAreaView style={ResponsiveStyling.safeAreaStyle}>
-      <View className="flex-1"></View>
+      <View className="pt-5 px-5 flex-1">
+        <View className="flex-row justify-between">
+          <FontAwesomeIcon icon={faBars} size={48} color={appStyle.appGray} />
+          <Text
+            className="text-5xl font-bold"
+            style={{ color: appStyle.appGray }}
+          >
+            Chats
+          </Text>
+        </View>
+        <View
+          className="rounded-xl mt-4 p-3"
+          style={{ backgroundColor: appStyle.appDarkBlueGrayer }}
+        >
+          <View className="flex-row items-center">
+            <FontAwesomeIcon icon={faMagnifyingGlass} size={24} color="white" />
+            <Text className="text-white text-xl ml-3">Search</Text>
+          </View>
+        </View>
+        <View className="flex-1"></View>
+      </View>
+
       <BottomNavbar currentScreen="Chats" />
     </SafeAreaView>
   );
