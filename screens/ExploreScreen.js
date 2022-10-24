047c9ec95d8cdac9b6b0c3f428583f0bf93c82e9
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-24 22:10:01 +0300

    See friend requests button functional

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,25 +1,88 @@
-import { SafeAreaView, Text, View } from "react-native";
-import { React, useLayoutEffect } from "react";
-import ResponsiveStyling from "../components/ResponsiveStyling";
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  FlatList,
+  Image,
+} from "react-native";
+import { React, useLayoutEffect, useContext, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import BottomNavbar from "../components/BottomNavbar";
+import authContext from "../context/authContext";
+import * as appStyle from "../components/AppStyleSheet";
+import ResponsiveStyling from "../components/ResponsiveStyling";
 const ExploreScreen = () => {
+  const { user } = useContext(authContext);
   const navigation = useNavigation();
+  const [renderOption, setRenderOption] = useState("Explore");
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   }, []);
+  const renderExplorePage = () => {
+    if (renderOption == "Friend requests") {
+      return (
+        <View>
+          <FlatList
+            data={user.othersRequests}
+            keyExtractor={(item) => item.username}
+            renderItem={({ item }) => {
+              <View className="flex-row justify-between">
+                <Image
+                  source={{
+                    uri: item.img,
+                  }}
+                  className="h-15 w-15 bg-white rounded-full mb-2 self-center"
+                />
+                <Text>{item.username}</Text>
+                <TouchableOpacity className="bg-blue-500">
+                  <Text className="text-gray-300">Accept!</Text>
+                </TouchableOpacity>
+              </View>;
+            }}
+          />
+        </View>
+      );
+    }
+  };
   return (
     <SafeAreaView style={ResponsiveStyling.safeAreaStyle}>
-      <View className="flex-1 justify-center">
-        <Text className="text-4xl text-white font-bold text-center">
-          Coming soon!
-        </Text>
+      <View className="flex-1 p-3">
+        <View className="flex-row justify-between">
+          <TouchableOpacity onPress={() => setRenderOption("Friend requests")}>
+            <Text
+              className="text-2xl w-min bg-gray-500"
+              style={style.socialButton}
+            >
+              Friend requests: {user.othersRequests?.length || 0}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text
+              className="text-2xl w-min bg-gray-500"
+              style={style.socialButton}
+            >
+              Notifications: {user.notifications?.length || 0}
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
+      {renderExplorePage()}
       <BottomNavbar currentScreen="Explore" />
     </SafeAreaView>
   );
 };
-
+const style = StyleSheet.create({
+  socialButton: {
+    borderColor: appStyle.appGray,
+    borderWidth: 1,
+    color: appStyle.appGray,
+    borderRadius: 5,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+  },
+});
 export default ExploreScreen;
