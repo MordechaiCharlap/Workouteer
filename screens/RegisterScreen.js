555b6331b13b15727b482f68603c45e7b6dab69c
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-24 14:38:12 +0300

    firestore setDoc bug fixed

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -94,7 +94,7 @@ const LoginScreen = () => {
       .then(async (userCredential) => {
         console.log("signed in!");
         console.log(userCredential.user.uid);
-        await setDoc(doc(firestore, "users", usernameLower), {
+        await setDoc(doc(firestore, "users", username.toLocaleLowerCase()), {
           username: username,
           usernameLower: username.toLocaleLowerCase(),
           birthdate: date,
