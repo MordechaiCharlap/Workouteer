cd2493309d7b1de9d6e884a824347c547349087b
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-25 16:25:24 +0300

    getRequests service added

--- services/firebase.js
+++ services/firebase.js
@@ -128,22 +128,27 @@ export const createUserRequestsDocs = async (newUserData) => {
   await setDoc(doc(db, "requests", newUserData.username.toLowerCase()), {});
   setUser(db.userDataByEmail(newUserData.email.toLowerCase()));
 };
-export const checkFriendShipStatus = async (user, otherUser) => {
-  const friendsMap = new Map(Object.entries(user.friends));
-  if (friendsMap.has(otherUser.usernameLower)) {
+export const checkFriendShipStatus = async (userData, otherUserData) => {
+  const friendsMap = new Map(Object.entries(userData.friends));
+  if (friendsMap.has(otherUserData.usernameLower)) {
     return "Friends";
   } else {
-    const userReqDoc = await getDoc(doc(db, "requests", user.usernameLower));
+    const userReqDoc = await getDoc(
+      doc(db, "requests", userData.usernameLower)
+    );
     const ownReqMap = new Map(Object.entries(userReqDoc.data().ownRequests));
-    if (ownReqMap.has(otherUser.usernameLower)) {
+    if (ownReqMap.has(otherUserData.usernameLower)) {
       return "SentRequest";
     } else {
       const othersReqMap = new Map(
         Object.entries(userReqDoc.data().othersRequests)
       );
-      if (othersReqMap.has(otherUser.usernameLower)) {
+      if (othersReqMap.has(otherUserData.usernameLower)) {
         return "GotRequest";
       } else return "None";
     }
   }
+};
+export const getOthersRequests = async (user) => {
+  return await getDoc(doc(db, "requests", user.usernameLower));
 };
