3b4ede015a608dee0c1a87ce61920d195b01651a
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-26 11:19:07 +0300

    request is lookig FINE

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -47,22 +47,44 @@ const ExploreScreen = () => {
       console.log("rendering friends requests");
       console.log(friendRequests);
       return (
-        <View className="flex-1">
+        <View className="flex-1 mt-3">
           <FlatList
             data={friendRequests}
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
-              <View className="flex-row justify-between bg-slate-500">
-                <Image
-                  source={{
-                    uri: item.img,
-                  }}
-                  className="h-16 w-16 bg-white rounded-full mb-2 self-center"
-                />
-                <Text>{item.displayName}</Text>
-                <TouchableOpacity className="bg-blue-500">
-                  <Text className="text-gray-300">Accept!</Text>
-                </TouchableOpacity>
+              <View className="flex-row justify-between items-center">
+                <View className="flex-row items-center">
+                  <Image
+                    source={{
+                      uri: item.img,
+                    }}
+                    className="h-16 w-16 bg-white rounded-full mr-2"
+                  />
+                  <Text className="text-xl" style={{ color: appStyle.appGray }}>
+                    {item.displayName}
+                  </Text>
+                </View>
+                <View className="flex-row">
+                  <TouchableOpacity
+                    className="p-1 rounded"
+                    style={{ backgroundColor: appStyle.appAzure }}
+                  >
+                    <Text
+                      className="text-2xl"
+                      style={{ color: appStyle.appGray }}
+                    >
+                      Accept
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    className="ml-2 p-1 rounded"
+                    style={{ backgroundColor: appStyle.appGray }}
+                  >
+                    <Text className="text-2xl" style={{ color: "black" }}>
+                      Delete
+                    </Text>
+                  </TouchableOpacity>
+                </View>
               </View>
             )}
           />
@@ -91,8 +113,8 @@ const ExploreScreen = () => {
             </Text>
           </TouchableOpacity>
         </View>
+        {renderExplorePage()}
       </View>
-      {renderExplorePage()}
       <BottomNavbar currentScreen="Explore" />
     </SafeAreaView>
   );
