640fbb6a6f678e30c6fd8f6ca91b879a0e163c4f
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-10-27 11:45:33 +0300

    clean code  asf

--- screens/FriendsScreen.js
+++ screens/FriendsScreen.js
@@ -2,7 +2,6 @@ import {
   View,
   Text,
   SafeAreaView,
-  StyleSheet,
   Image,
   FlatList,
   TouchableOpacity,
@@ -128,57 +127,10 @@ const FriendsScreen = ({ route }) => {
             </View>
           )}
         />
-        {/* <FlatList
-          showsVerticalScrollIndicator={false}
-          className="flex-1 "
-          data={shownFriendsArray}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              style={{
-                backgroundColor: appStyle.appLightBlue,
-                marginTop: 1,
-              }}
-              className={`p-2 h-16 flex-row ${ResponsiveShadow}`}
-            >
-              <View className="items-center aspect-square">
-                <Image
-                  source={{
-                    uri: item.img,
-                  }}
-                  className="h-16 w-16 rounded-full bg-white aspect-square"
-                  // style={style.profileImg}
-                />
-              </View>
-              <View className="justify-center" style={{ flexBasis: "76%" }}>
-                <Text
-                  className="text-xl font-semibold text-center"
-                  style={{ color: appStyle.appDarkBlue }}
-                >
-                  {item.username}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          )}
-        /> */}
       </View>
       <BottomNavbar currentScreen="Friends" />
     </SafeAreaView>
   );
 };
-const style = StyleSheet.create({
-  profileImg: {
-    borderColor: appStyle.appDarkBlue,
-    borderWidth: 0.1,
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0 /* Make the picture taking the size of it's parent */,
-    // width: "100%" /* This if for the object-fit */,
-    width: "100%" /* This if for the object-fit */,
-    objectFit:
-      "cover" /* Equivalent of the background-size: cover; of a background-image */,
-    objectPosition: "center",
-  },
-});
+
 export default FriendsScreen;
