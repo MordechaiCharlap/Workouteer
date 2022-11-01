48d7db70b046e439d54674b4dca63104d65dacc7
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-01 22:18:38 +0200

    Move editData to different inside-component

--- screens/EditDataScreen.js
+++ screens/EditDataScreen.js
@@ -25,184 +25,15 @@ const EditDataScreen = () => {
 
   const { user, setUser } = useContext(authContext);
 
-  const [displayName, setDisplayName] = useState(user.displayName);
-  const [description, setDescription] = useState(user.description);
-  const [image, setImage] = useState(user.img);
-
   const [currentTab, setCurrentTab] = useState("ProfileData");
-  const [changesMade, setChangesMade] = useState(false);
-  const [isLoading, setLoading] = useState(false);
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   }, []);
-  useEffect(() => {
-    if (
-      description == user.description &&
-      displayName == user.displayName &&
-      image == user.img
-    )
-      setChangesMade(false);
-    else setChangesMade(true);
-  }, [displayName, description]);
-  const onImageLibraryPress = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 0.5,
-    });
 
-    if (!result.cancelled) {
-      const uploadUrl = await firebase.uploadProfileImage(
-        user.usernameLower,
-        result.uri
-      );
-      console.log("uploadUrl: " + uploadUrl);
-      setImage(uploadUrl);
-    }
-  };
-
-  const saveProfileChanges = async () => {
-    setLoading(true);
-    if (displayName == "") setDisplayName(user.username);
-    await firebase.saveProfileChanges(
-      user.usernameLower,
-      displayName == null ? "" : displayName,
-      description == null ? "" : description,
-      image == null ? "" : image
-      //should add deafult link to firebase storage deafult profile picture
-    );
-    setUser(await firebase.updateContext(user.usernameLower));
-    setLoading(false);
-    setChangesMade(false);
-  };
-  const SaveButton = () => {
-    if (changesMade == false)
-      return (
-        <TouchableOpacity
-          className="self-center py-1 px-5 w-9/12 rounded"
-          style={{ backgroundColor: appStyle.appAzure }}
-        >
-          <Text
-            className="text-2xl text-center"
-            style={{ color: appStyle.appGray }}
-          >
-            No changes made
-          </Text>
-        </TouchableOpacity>
-      );
-    else {
-      if (isLoading == false)
-        return (
-          <TouchableOpacity
-            onPress={saveProfileChanges}
-            className="self-center py-1 px-5 w-9/12 rounded"
-            style={{ backgroundColor: appStyle.appAzure }}
-          >
-            <Text
-              className="text-2xl text-center"
-              style={{ color: appStyle.appGray }}
-            >
-              Save changes
-            </Text>
-          </TouchableOpacity>
-        );
-      else {
-        return (
-          <TouchableOpacity
-            className="self-center py-1 px-5 w-9/12 rounded"
-            style={{ backgroundColor: appStyle.appAzure }}
-          >
-            <Text
-              className="text-2xl text-center"
-              style={{ color: appStyle.appGray }}
-            >
-              Loading...
-            </Text>
-          </TouchableOpacity>
-        );
-      }
-    }
-  };
-  const renderChosenSection = () => {
-    if (currentTab == "ProfileData")
-      return (
-        <View>
-          <View></View>
-          <View>
-            <View className="mb-5 self-center">
-              <Image
-                source={{
-                  uri: image,
-                }}
-                className="h-32 w-32 bg-white rounded-full mb-2"
-              />
-              <TouchableOpacity
-                onPress={onImageLibraryPress}
-                className="absolute right-0 bottom-0 rounded-full p-2"
-                style={{
-                  backgroundColor: appStyle.appGray,
-                  borderWidth: 1,
-                  borderColor: appStyle.appDarkBlue,
-                }}
-              >
-                <FontAwesomeIcon
-                  icon={faPen}
-                  size={20}
-                  color={appStyle.appDarkBlue}
-                />
-              </TouchableOpacity>
-            </View>
-            <View className="flex-row items-center mb-5">
-              <Text
-                className="mr-3 text-lg"
-                style={{ color: appStyle.appGray }}
-              >
-                Display name:
-              </Text>
-              <TextInput
-                className="rounded text-lg flex-1"
-                style={style.input}
-                placeholder={user.displayName}
-                placeholderTextColor={"#5f6b8b"}
-                maxLength={10}
-                onChangeText={(text) => setDisplayName(text)}
-              >
-                {user.displayName}
-              </TextInput>
-            </View>
-            <View className="mb-5">
-              <Text
-                className="mr-3 text-lg"
-                style={{ color: appStyle.appGray }}
-              >
-                Description
-              </Text>
-              <TextInput
-                style={{
-                  textAlignVertical: "top",
-                  backgroundColor: appStyle.appLightBlue,
-                  borderRadius: 8,
-                  padding: 8,
-                }}
-                multiline
-                placeholder="Optional text"
-                placeholderTextColor={appStyle.appDarkBlue}
-                numberOfLines={12}
-                maxLength={350}
-                onChangeText={(text) => setDescription(text)}
-              >
-                {user.description}
-              </TextInput>
-            </View>
-            {SaveButton()}
-          </View>
-        </View>
-      );
-    else return <View></View>;
-  };
+  const renderChosenSection = () => {};
   return (
     <SafeAreaView style={ResponsiveStyling.safeAreaStyle}>
       <View className="flex-1  p-3">
@@ -252,12 +83,181 @@ const EditDataScreen = () => {
             </Text>
           </TouchableOpacity>
         </View>
-        {renderChosenSection()}
+        {currentTab == "ProfileData" && (
+          <EditProfileData user={user} setUser={setUser} />
+        )}
       </View>
     </SafeAreaView>
   );
 };
 export default EditDataScreen;
+
+const EditProfileData = () => {
+  useEffect(() => {
+    if (
+      description == props.user.description &&
+      displayName == props.user.displayName &&
+      image == props.user.img
+    )
+      setChangesMade(false);
+    else setChangesMade(true);
+  }, [displayName, description]);
+  const [displayName, setDisplayName] = useState(props.user.displayName);
+  const [description, setDescription] = useState(props.user.description);
+  const [image, setImage] = useState(props.user.img);
+
+  const [changesMade, setChangesMade] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+
+  const onImageLibraryPress = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 0.5,
+    });
+
+    if (!result.cancelled) {
+      const uploadUrl = await firebase.uploadProfileImage(
+        props.user.usernameLower,
+        result.uri
+      );
+      console.log("uploadUrl: " + uploadUrl);
+      setImage(uploadUrl);
+    }
+  };
+  const saveProfileChanges = async () => {
+    setLoading(true);
+    if (displayName == "") setDisplayName(props.user.username);
+    await firebase.saveProfileChanges(
+      props.user.usernameLower,
+      displayName == null ? "" : displayName,
+      description == null ? "" : description,
+      image == null ? "" : image
+      //should add deafult link to firebase storage deafult profile picture
+    );
+    setUser(await firebase.updateContext(props.user.usernameLower));
+    setLoading(false);
+    setChangesMade(false);
+  };
+
+  const SaveButton = () => {
+    if (changesMade == false)
+      return (
+        <TouchableOpacity
+          className="self-center py-1 px-5 w-9/12 rounded"
+          style={{ backgroundColor: appStyle.appAzure }}
+        >
+          <Text
+            className="text-2xl text-center"
+            style={{ color: appStyle.appGray }}
+          >
+            No changes made
+          </Text>
+        </TouchableOpacity>
+      );
+    else {
+      if (isLoading == false)
+        return (
+          <TouchableOpacity
+            onPress={saveProfileChanges}
+            className="self-center py-1 px-5 w-9/12 rounded"
+            style={{ backgroundColor: appStyle.appAzure }}
+          >
+            <Text
+              className="text-2xl text-center"
+              style={{ color: appStyle.appGray }}
+            >
+              Save changes
+            </Text>
+          </TouchableOpacity>
+        );
+      else {
+        return (
+          <TouchableOpacity
+            className="self-center py-1 px-5 w-9/12 rounded"
+            style={{ backgroundColor: appStyle.appAzure }}
+          >
+            <Text
+              className="text-2xl text-center"
+              style={{ color: appStyle.appGray }}
+            >
+              Loading...
+            </Text>
+          </TouchableOpacity>
+        );
+      }
+    }
+  };
+  return (
+    <View>
+      <View></View>
+      <View>
+        <View className="mb-5 self-center">
+          <Image
+            source={{
+              uri: image,
+            }}
+            className="h-32 w-32 bg-white rounded-full mb-2"
+          />
+          <TouchableOpacity
+            onPress={onImageLibraryPress}
+            className="absolute right-0 bottom-0 rounded-full p-2"
+            style={{
+              backgroundColor: appStyle.appGray,
+              borderWidth: 1,
+              borderColor: appStyle.appDarkBlue,
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faPen}
+              size={20}
+              color={appStyle.appDarkBlue}
+            />
+          </TouchableOpacity>
+        </View>
+        <View className="flex-row items-center mb-5">
+          <Text className="mr-3 text-lg" style={{ color: appStyle.appGray }}>
+            Display name:
+          </Text>
+          <TextInput
+            className="rounded text-lg flex-1"
+            style={style.input}
+            placeholder={props.user.displayName}
+            placeholderTextColor={"#5f6b8b"}
+            maxLength={10}
+            onChangeText={(text) => setDisplayName(text)}
+          >
+            {props.user.displayName}
+          </TextInput>
+        </View>
+        <View className="mb-5">
+          <Text className="mr-3 text-lg" style={{ color: appStyle.appGray }}>
+            Description
+          </Text>
+          <TextInput
+            style={{
+              textAlignVertical: "top",
+              backgroundColor: appStyle.appLightBlue,
+              borderRadius: 8,
+              padding: 8,
+            }}
+            multiline
+            placeholder="Optional text"
+            placeholderTextColor={appStyle.appDarkBlue}
+            numberOfLines={12}
+            maxLength={350}
+            onChangeText={(text) => setDescription(text)}
+          >
+            {props.user.description}
+          </TextInput>
+        </View>
+        {SaveButton()}
+      </View>
+    </View>
+  );
+};
+
 const style = StyleSheet.create({
   input: {
     borderWidth: 1,
