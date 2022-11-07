26c1374e4309593b0b61faa8180bfee0f3be6222
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-07 11:57:41 +0200

    No requests text added

--- components/FriendRequests.js
+++ components/FriendRequests.js
@@ -15,64 +15,77 @@ const FriendRequests = (props) => {
     await props.deleteRequest(index);
     await firebase.rejectRequest(props.user.usernameLower, otherUserId);
   };
-  return (
-    <View className="flex-1 mt-3">
-      <FlatList
-        className="px-4 pt-3"
-        data={props.friendRequests}
-        keyExtractor={(item) => item.username}
-        renderItem={({ item, index }) => (
-          <View className="flex-row items-center mt-2">
-            <TouchableOpacity
-              onPress={() => props.userClicked(item)}
-              className="flex-row flex-1 items-center"
-            >
-              <Image
-                source={{
-                  uri: item.img,
-                }}
-                className="h-14 w-14 bg-white rounded-full mr-4"
-              />
-              <View>
-                <Text
-                  className="text-xl font-semibold tracking-wider"
-                  style={{ color: appStyle.appGray }}
+  if (props.friendRequests.length != 0)
+    return (
+      <View className="flex-1 mt-3">
+        <FlatList
+          className="px-4 pt-3"
+          data={props.friendRequests}
+          keyExtractor={(item) => item.username}
+          renderItem={({ item, index }) => (
+            <View className="flex-row items-center mt-2">
+              <TouchableOpacity
+                onPress={() => props.userClicked(item)}
+                className="flex-row flex-1 items-center"
+              >
+                <Image
+                  source={{
+                    uri: item.img,
+                  }}
+                  className="h-14 w-14 bg-white rounded-full mr-4"
+                />
+                <View>
+                  <Text
+                    className="text-xl font-semibold tracking-wider"
+                    style={{ color: appStyle.appGray }}
+                  >
+                    {item.username}
+                  </Text>
+                  <Text
+                    className="text-md opacity-60 tracking-wider"
+                    style={{ color: appStyle.appGray }}
+                  >
+                    {item.displayName}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+              <View className="flex-row">
+                <TouchableOpacity
+                  onPress={() => acceptRequest(item.usernameLower, index)}
+                  className="p-1 rounded"
+                  style={{ backgroundColor: appStyle.appAzure }}
                 >
-                  {item.username}
-                </Text>
-                <Text
-                  className="text-md opacity-60 tracking-wider"
-                  style={{ color: appStyle.appGray }}
+                  <Text
+                    className="text-2xl"
+                    style={{ color: appStyle.appGray }}
+                  >
+                    Accept
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => rejectRequest(item.usernameLower, index)}
+                  className="ml-2 p-1 rounded"
+                  style={{ backgroundColor: appStyle.appGray }}
                 >
-                  {item.displayName}
-                </Text>
+                  <Text className="text-2xl" style={{ color: "black" }}>
+                    Reject
+                  </Text>
+                </TouchableOpacity>
               </View>
-            </TouchableOpacity>
-            <View className="flex-row">
-              <TouchableOpacity
-                onPress={() => acceptRequest(item.usernameLower, index)}
-                className="p-1 rounded"
-                style={{ backgroundColor: appStyle.appAzure }}
-              >
-                <Text className="text-2xl" style={{ color: appStyle.appGray }}>
-                  Accept
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => rejectRequest(item.usernameLower, index)}
-                className="ml-2 p-1 rounded"
-                style={{ backgroundColor: appStyle.appGray }}
-              >
-                <Text className="text-2xl" style={{ color: "black" }}>
-                  Reject
-                </Text>
-              </TouchableOpacity>
             </View>
-          </View>
-        )}
-      />
-    </View>
-  );
+          )}
+        />
+      </View>
+    );
+  else {
+    return (
+      <View>
+        <Text className="text-2xl text-center p-3" style={{ color: "white" }}>
+          No requests!
+        </Text>
+      </View>
+    );
+  }
 };
 
 export default FriendRequests;
