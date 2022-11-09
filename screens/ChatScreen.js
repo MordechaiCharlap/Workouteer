7b057f965e13564d60110555012ab307d1de4fa9
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-09 11:41:06 +0200

    chat upper banner added

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -12,6 +12,7 @@ import authContext from "../context/authContext";
 import { useNavigation } from "@react-navigation/native";
 import ResponsiveStyling from "../components/ResponsiveStyling";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import * as appStyle from "../components/AppStyleSheet";
 const ChatScreen = ({ route }) => {
   const navigation = useNavigation();
@@ -24,7 +25,27 @@ const ChatScreen = ({ route }) => {
   });
   return (
     <SafeAreaView style={ResponsiveStyling.safeAreaStyle}>
-      <View className="flex-1"></View>
+      <View className="flex-1">
+        <View
+          className="flex-row items-center pb-3 pt-2"
+          style={{ backgroundColor: "#464f5b" }}
+        >
+          <FontAwesomeIcon
+            icon={faChevronLeft}
+            size={40}
+            color={appStyle.appGray}
+          />
+          <Image
+            source={{
+              uri: otherUser.img,
+            }}
+            className="h-14 w-14 bg-white rounded-full mr-4"
+          />
+          <Text className="text-2xl" style={{ color: appStyle.appGray }}>
+            {otherUser.username}
+          </Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
