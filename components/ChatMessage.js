711bcb5c40aa79bab2cc321f1c12f9475b662843
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-09 21:43:02 +0200

    other user message style added

--- components/ChatMessage.js
+++ components/ChatMessage.js
@@ -2,14 +2,24 @@ import { View, Text } from "react-native";
 import React from "react";
 import * as appStyle from "./AppStyleSheet";
 const ChatMessage = (props) => {
-  return (
-    <View className="rounded" style={{ backgroundColor: appStyle.appAzure }}>
-      <Text className="text-xl" style={{ color: appStyle.appGray }}>
-        {props.content}
-      </Text>
-      <Text style={{ color: appStyle.appGray }}>{props.createdAt}</Text>
-    </View>
-  );
+  if (props.sender == props.user.usernameLower)
+    return (
+      <View className="rounded" style={{ backgroundColor: appStyle.appAzure }}>
+        <Text className="text-xl" style={{ color: appStyle.appGray }}>
+          {props.content}
+        </Text>
+        <Text style={{ color: appStyle.appGray }}>{props.createdAt}</Text>
+      </View>
+    );
+  else
+    return (
+      <View className="rounded" style={{ backgroundColor: appStyle.appGray }}>
+        <Text className="text-xl" style={{ color: appStyle.appDarkBlue }}>
+          {props.content}
+        </Text>
+        <Text style={{ color: appStyle.appGray }}>{props.createdAt}</Text>
+      </View>
+    );
 };
 
 export default ChatMessage;
