0a7bc4d2ae69cdca3f6d0e2e59c8d7c51ea22136
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-09 11:06:32 +0200

    init chat screen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,7 +1,16 @@
 import { View, Text } from "react-native";
-import React from "react";
-
-const ChatScreen = () => {
+import React, { useContext, useLayoutEffect, useState } from "react";
+import authContext from "../context/authContext";
+import { useNavigation } from "@react-navigation/native";
+const ChatScreen = (props) => {
+  const navigation = useNavigation();
+  const { user } = useContext(authContext);
+  const otherUser = props.otherUser;
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
   return (
     <View>
       <Text>ChatScreen</Text>
