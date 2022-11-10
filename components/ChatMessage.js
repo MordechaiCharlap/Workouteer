2e94c6229d19243532f591e096770d6109b413e2
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-10 14:23:57 +0200

    even cleaner self/otherUser message

--- components/ChatMessage.js
+++ components/ChatMessage.js
@@ -2,23 +2,18 @@ import { View, Text } from "react-native";
 import React from "react";
 import * as appStyle from "./AppStyleSheet";
 const ChatMessage = (props) => {
+  const isSelfMessage = props.sender == props.user.usernameLower;
   return (
     <View
       className="rounded w-10/12"
       style={{
-        backgroundColor:
-          props.sender == props.user.usernameLower
-            ? appStyle.appAzure
-            : appStyle.appGray,
+        backgroundColor: isSelfMessage ? appStyle.appAzure : appStyle.appGray,
       }}
     >
       <Text
         className="text-xl"
         style={{
-          color:
-            props.sender == props.user.usernameLower
-              ? appStyle.appGray
-              : appStyle.appDarkBlue,
+          color: isSelfMessage ? appStyle.appGray : appStyle.appDarkBlue,
         }}
       >
         {props.content}
