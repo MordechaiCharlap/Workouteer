44e7891094249b10382ad0701ba8bad46eeb9397
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-10 14:22:00 +0200

    cleaner code

--- components/ChatMessage.js
+++ components/ChatMessage.js
@@ -2,24 +2,30 @@ import { View, Text } from "react-native";
 import React from "react";
 import * as appStyle from "./AppStyleSheet";
 const ChatMessage = (props) => {
-  if (props.sender == props.user.usernameLower)
-    return (
-      <View className="rounded" style={{ backgroundColor: appStyle.appAzure }}>
-        <Text className="text-xl" style={{ color: appStyle.appGray }}>
-          {props.content}
-        </Text>
-        <Text style={{ color: appStyle.appGray }}>{props.createdAt}</Text>
-      </View>
-    );
-  else
-    return (
-      <View className="rounded" style={{ backgroundColor: appStyle.appGray }}>
-        <Text className="text-xl" style={{ color: appStyle.appDarkBlue }}>
-          {props.content}
-        </Text>
-        <Text style={{ color: appStyle.appGray }}>{props.createdAt}</Text>
-      </View>
-    );
+  return (
+    <View
+      className="rounded w-10/12"
+      style={{
+        backgroundColor:
+          props.sender == props.user.usernameLower
+            ? appStyle.appAzure
+            : appStyle.appGray,
+      }}
+    >
+      <Text
+        className="text-xl"
+        style={{
+          color:
+            props.sender == props.user.usernameLower
+              ? appStyle.appGray
+              : appStyle.appDarkBlue,
+        }}
+      >
+        {props.content}
+      </Text>
+      <Text style={{ color: appStyle.appGray }}>{props.createdAt}</Text>
+    </View>
+  );
 };
 
 export default ChatMessage;
