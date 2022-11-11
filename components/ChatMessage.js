81e264be85fc2104ebd9365017761988e93a5463
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-11 09:56:43 +0200

    message styling

--- components/ChatMessage.js
+++ components/ChatMessage.js
@@ -4,21 +4,25 @@ import * as appStyle from "./AppStyleSheet";
 const ChatMessage = (props) => {
   const isSelfMessage = props.message.sender == props.user.usernameLower;
   return (
-    <View
-      className={`rounded ${isSelfMessage ? "mr-3" : "ml-3"}mt-1`}
-      style={{
-        backgroundColor: isSelfMessage ? appStyle.appAzure : appStyle.appGray,
-      }}
-    >
-      <Text
-        className="text-xl"
+    <View className={`${isSelfMessage ? "flex-row-reverse" : "flex-row"}`}>
+      <View
+        className={`rounded mt-1 px-3`}
         style={{
-          color: isSelfMessage ? appStyle.appGray : appStyle.appDarkBlue,
+          backgroundColor: isSelfMessage ? appStyle.appAzure : appStyle.appGray,
         }}
       >
-        {props.message.content}
-      </Text>
-      <Text style={{ color: appStyle.appGray }}>{props.message.createdAt}</Text>
+        <Text
+          className="text-xl"
+          style={{
+            color: isSelfMessage ? appStyle.appGray : appStyle.appDarkBlue,
+          }}
+        >
+          {props.message.content}
+        </Text>
+        <Text style={{ color: appStyle.appGray }}>
+          {props.message.createdAt}
+        </Text>
+      </View>
     </View>
   );
 };
