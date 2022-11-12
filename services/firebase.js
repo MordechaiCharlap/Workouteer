c29a5b6364c5282fbd623d3197821684c3de6c9a
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-12 22:24:37 +0200

    function calling bug fixed

--- services/firebase.js
+++ services/firebase.js
@@ -327,7 +327,7 @@ export const getFriendsArray = async (user) => {
   const allFriendsMap = new Map(Object.entries(user.friends));
   const friendsArr = [];
   for (var key of allFriendsMap.keys()) {
-    var userData = await firebase.userDataById(key);
+    var userData = await userDataById(key);
     friendsArr.push(userData);
   }
   return friendsArr;
