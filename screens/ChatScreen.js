e8198e5c39b9de4d294d2ee5638f33d2fb505551
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-13 15:24:59 +0200

    textInput styling fixed

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -126,7 +126,7 @@ const ChatScreen = ({ route }) => {
       </View>
       <View className="flex-row p-2 items-center">
         <TextInput
-          className="text-2xl flex-1 mr-2 rounded-full py-1 px-4"
+          className="text-2xl flex-1 mr-2 rounded py-1 px-4"
           multiline={true}
           placeholder="Message"
           placeholderTextColor={appStyle.appGray}
