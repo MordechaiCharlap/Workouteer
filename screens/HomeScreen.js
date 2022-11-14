8dbe3f6df7087136227c0ddbe245c44b333d047e
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-14 02:20:59 +0200

    Generic buttons added to homeScreen!

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,9 @@ import BottomNavbar from "../components/BottomNavbar";
 import AddWorkoutButton from "../components/AddWorkoutButton";
 import style from "../components/ResponsiveStyling";
 import SearchWorkoutButton from "../components/SearchWorkoutButton";
-
+import HomeScreenButton from "../components/HomeScreenButton";
+import * as appStyle from "../components/AppStyleSheet";
+import { faPlus, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 const HomeScreen = () => {
   const navigation = useNavigation();
   useLayoutEffect(() => {
@@ -13,16 +15,31 @@ const HomeScreen = () => {
       headerShown: false,
     });
   }, []);
+  const buttonStyle = {
+    color: appStyle.appGray,
+    backgroundColor: appStyle.appAzure,
+    buttonSize: 40,
+    iconSize: 40,
+  };
   return (
     <SafeAreaView style={style.safeAreaStyle}>
       <View className="flex-1">
         <View className="flex-row justify-around p-3">
-          <AddWorkoutButton />
-          <SearchWorkoutButton />
-          <FriendsWorkoutsButton />
+          <HomeScreenButton
+            buttonText="FIND A WORKOUT"
+            style={buttonStyle}
+            navigateScreen="SearchWorkout"
+            icon={faMagnifyingGlass}
+          />
+          <HomeScreenButton
+            buttonText="CREATE WORKOUT"
+            style={buttonStyle}
+            navigateScreen="NewWorkout"
+            icon={faPlus}
+          />
         </View>
       </View>
-      <BottomNavbar currentScreen="Home" display={navDisplay} />
+      <BottomNavbar currentScreen="Home" />
     </SafeAreaView>
   );
 };
