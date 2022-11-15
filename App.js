e1356c83eeeb946c6c592562b70cfc5876ff8042
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-15 12:31:53 +0200

    authProvider added

--- App.js
+++ App.js
@@ -3,15 +3,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./StackNavigator";
 import authContext from "./context/authContext";
 import { useState } from "react";
+import { AuthPrvider } from "./hooks/useAuth";
 export default function App() {
   const [user, setUser] = useState(null);
   return (
     <NavigationContainer>
-      <authContext.Provider value={{ user, setUser }}>
-        <TailwindProvider>
-          <StackNavigator />
-        </TailwindProvider>
-      </authContext.Provider>
+      <AuthPrvider>
+        <authContext.Provider value={{ user, setUser }}>
+          <TailwindProvider>
+            <StackNavigator />
+          </TailwindProvider>
+        </authContext.Provider>
+      </AuthPrvider>
     </NavigationContainer>
   );
 }
