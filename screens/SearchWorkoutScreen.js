8ec9b9e3f8485cddcfdf714ef5065353b01f9244
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-15 20:57:13 +0200

    removed unnecessary code

--- screens/SearchWorkoutScreen.js
+++ screens/SearchWorkoutScreen.js
@@ -1,10 +1,7 @@
 import { SafeAreaView, View, Text } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import BottomNavbar from "../components/BottomNavbar";
 import ResponsiveStyling from "../components/ResponsiveStyling";
-import HomeScreenButton from "../components/HomeScreenButton";
-import * as appStyle from "../components/AppStyleSheet";
 import Header from "../components/Header";
 const SearchWorkoutScreen = () => {
   const navigation = useNavigation();
@@ -16,7 +13,7 @@ const SearchWorkoutScreen = () => {
   return (
     <SafeAreaView style={ResponsiveStyling.safeAreaStyle}>
       <View className="flex-1">
-        <Header title="Workouts nearby" />
+        <Header title="Find workout" goBackOption={true} />
       </View>
     </SafeAreaView>
   );
