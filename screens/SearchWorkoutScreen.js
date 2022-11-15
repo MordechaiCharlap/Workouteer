24e6ed139ab53732437041f9f82af05dde81f798
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-15 20:45:32 +0200

    default stuff and header added

--- screens/SearchWorkoutScreen.js
+++ screens/SearchWorkoutScreen.js
@@ -1,11 +1,24 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { SafeAreaView, View, Text } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import BottomNavbar from "../components/BottomNavbar";
+import ResponsiveStyling from "../components/ResponsiveStyling";
+import HomeScreenButton from "../components/HomeScreenButton";
+import * as appStyle from "../components/AppStyleSheet";
+import Header from "../components/Header";
 const SearchWorkoutScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
   return (
-    <View>
-      <Text>SearchWorkoutScreen</Text>
-    </View>
+    <SafeAreaView style={ResponsiveStyling.safeAreaStyle}>
+      <View className="flex-1">
+        <Header title="Workouts nearby" />
+      </View>
+    </SafeAreaView>
   );
 };
 
