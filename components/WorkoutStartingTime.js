5d9aeab916f4cb1dca42807f6a15b39765bed798
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-20 15:36:56 +0200

    DatePicker instead of dropdown

--- components/WorkoutStartingTime.js
+++ components/WorkoutStartingTime.js
@@ -1,69 +1,63 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 import { React, useState } from "react";
-import { Dropdown } from "react-native-element-dropdown";
 import * as appStyle from "./AppStyleSheet";
-const data = [
-  { label: "00:00", value: 0 },
-
-  { label: "00:30", value: 30 },
-  { label: "1:00", value: 60 },
-  { label: "1:30", value: 90 },
-  { label: "2:00", value: 120 },
-  { label: "2:30", value: 150 },
-  { label: "3:00", value: 180 },
-];
-
+import DateTimePicker from "@react-native-community/datetimepicker";
 const WorkoutStartingTime = (props) => {
-  const [value, setValue] = useState(null);
-  const [isFocus, setIsFocus] = useState(false);
-
-  const renderLabel = () => {
-    if (value || isFocus) {
-      return (
-        <Text
-          className="rounded font-medium"
-          style={[styles.label, isFocus && { color: appStyle.appDarkBlue }]}
-        >
-          Planned starting time
-        </Text>
-      );
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+  const [changedOnce, setChangeOnce] = useState(false);
+  const [mode, setMode] = useState(null);
+  const onDateChange = (selectedDate) => {
+    const currentDate = selectedDate;
+    setDate(currentDate);
+    setShow(false);
+    setChangeOnce(true);
+    props.dateTimeChanged(selectedDate);
+  };
+  const showMode = (currentMode) => {
+    if (Platform.OS === "android") {
+      setShow(true);
+      // for iOS, add a button that closes the picker
     }
-    return null;
+    setMode(currentMode);
   };
 
+  const showDatepicker = () => {
+    showMode("date");
+  };
+
+  const showTimepicker = () => {
+    showMode("time");
+  };
   return (
     <View>
-      {renderLabel()}
-      <Dropdown
-        style={[styles.dropdown, isFocus && { borderColor: appStyle.appAzure }]}
-        placeholderStyle={styles.placeholderStyle}
-        selectedTextStyle={styles.selectedTextStyle}
-        inputSearchStyle={styles.inputSearchStyle}
-        iconStyle={styles.iconStyle}
-        data={data}
-        search
-        maxHeight={300}
-        labelField="label"
-        valueField="value"
-        placeholder={!isFocus ? "What time do you train?" : "..."}
-        searchPlaceholder="Search..."
-        value={value}
-        onFocus={() => setIsFocus(true)}
-        onBlur={() => setIsFocus(false)}
-        onChange={(item) => {
-          props.startingTimeSelected(item.value);
-          setValue(item.value);
-          setIsFocus(false);
-        }}
-      />
+      <TouchableOpacity
+        style={styles.input}
+        className="rounded mb-5 px-3 h-10 justify-center"
+        onPress={showDatepicker}
+      >
+        {!changedOnce && (
+          <Text style={{ color: "#5f6b8b" }}>
+            birthdate (works only on Android)
+          </Text>
+        )}
+        {changedOnce && (
+          <Text style={{ color: "#5f6b8b" }}>{date.toDateString()}</Text>
+        )}
+      </TouchableOpacity>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          onChange={onDateChange}
+        />
+      )}
     </View>
   );
 };
-
-export default WorkoutStartingTime;
-
 const styles = StyleSheet.create({
-  dropdown: {
+  input: {
     backgroundColor: appStyle.appGray,
     height: 50,
     borderColor: appStyle.appLightBlue,
@@ -71,37 +65,5 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     paddingHorizontal: 8,
   },
-  icon: {
-    marginRight: 5,
-    color: "white",
-  },
-  label: {
-    position: "absolute",
-    // fontWeight: 450,
-    color: appStyle.appLightBlue,
-    backgroundColor: appStyle.appDarkBlue,
-    left: 22,
-    top: -10,
-    zIndex: 999,
-    paddingHorizontal: 8,
-    fontSize: 14,
-  },
-  placeholderStyle: {
-    color: appStyle.appDarkBlue,
-    // fontWeight: 600,
-    fontSize: 16,
-  },
-  selectedTextStyle: {
-    // fontWeight: 600,
-    color: appStyle.appDarkBlue,
-    fontSize: 16,
-  },
-  iconStyle: {
-    width: 20,
-    height: 20,
-  },
-  inputSearchStyle: {
-    height: 40,
-    fontSize: 16,
-  },
 });
+export default WorkoutStartingTime;
