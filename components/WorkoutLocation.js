2a5d003b48a3ea1fd228f86977ea07271877d019
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-20 10:28:18 +0200

    first prototype styling

--- components/WorkoutLocation.js
+++ components/WorkoutLocation.js
@@ -5,15 +5,29 @@ import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 const WorkoutLocation = () => {
   return (
-    <View className="flex-row">
-      <FontAwesomeIcon
-        icon={faLocationDot}
-        size={30}
-        color={appStyle.appGray}
-      />
-      <Text style={{ color: appStyle.appGray }}>Location:</Text>
-      <TouchableOpacity className="bg-black">
-        <Text style={{ color: appStyle.appGray }}>Current location</Text>
+    <View className="flex-row items-center justify-between">
+      <View className="flex-row items-center">
+        <FontAwesomeIcon
+          icon={faLocationDot}
+          size={25}
+          color={appStyle.appGray}
+        />
+        <Text className="ml-1" style={{ color: appStyle.appGray }}>
+          Location:
+        </Text>
+      </View>
+
+      <TouchableOpacity
+        className="rounded justify-center p-1 px-3"
+        style={{ backgroundColor: appStyle.appLightBlue }}
+      >
+        <Text style={{ color: appStyle.appDarkBlue }}>Current location</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        className="rounded justify-center p-1 px-3"
+        style={{ backgroundColor: appStyle.appLightBlue }}
+      >
+        <Text style={{ color: appStyle.appDarkBlue }}>Set location</Text>
       </TouchableOpacity>
     </View>
   );
