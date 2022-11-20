c5f56dd60fa7ce6bd22c4636d08e8fccd6bc5305
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-20 10:45:42 +0200

    current location getting works

--- components/WorkoutLocation.js
+++ components/WorkoutLocation.js
@@ -1,9 +1,23 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as appStyle from "./AppStyleSheet";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import * as Location from "expo-location";
 const WorkoutLocation = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const getCurrentLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+    locationChanged(location);
+  };
   return (
     <View className="flex-row items-center justify-between">
       <View className="flex-row items-center">
@@ -16,15 +30,18 @@ const WorkoutLocation = () => {
           Location:
         </Text>
       </View>
-
+      <Text className="ml-1" style={{ color: appStyle.appGray }}>
+        {location == null ? "Not chosen" : JSON.stringify(location)}
+      </Text>
       <TouchableOpacity
-        className="rounded justify-center p-1 px-3"
+        onPress={getCurrentLocation}
+        className="rounded justify-center p-1"
         style={{ backgroundColor: appStyle.appLightBlue }}
       >
         <Text style={{ color: appStyle.appDarkBlue }}>Current location</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        className="rounded justify-center p-1 px-3"
+        className="rounded justify-center p-1"
         style={{ backgroundColor: appStyle.appLightBlue }}
       >
         <Text style={{ color: appStyle.appDarkBlue }}>Set location</Text>
