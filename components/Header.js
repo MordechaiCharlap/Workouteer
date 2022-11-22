5454d147a149f9fa24d33a5b29fdaba150372509
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-22 14:33:34 +0200

    header styling fixed

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   const navigation = useNavigation();
   return (
     <View
-      className="flex-row items-center h-10"
+      className="flex-row items-center h-10 mb-2"
       style={{
         backgroundColor: appDarkBlue,
         justifyContent: props.goBackOption ? "space-between" : "center",
