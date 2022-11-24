8be61d9ce87fb48c9650d8ea539bc6caf9d06386
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-24 15:47:20 +0200

    workout div

--- screens/PlannedWorkoutsScreen.js
+++ screens/PlannedWorkoutsScreen.js
@@ -4,6 +4,7 @@ import {
   ScrollView,
   TouchableOpacity,
   SafeAreaView,
+  FlatList,
 } from "react-native";
 import React, { useState, useEffect, useLayoutEffect } from "react";
 import * as appStyle from "../components/AppStyleSheet";
@@ -14,6 +15,7 @@ import * as firebase from "../services/firebase";
 import useAuth from "../hooks/useAuth";
 const PlannedWorkoutsScreen = () => {
   const { user } = useAuth();
+  const now = new Date();
   const [workouts, setWorkouts] = useState([]);
   const navigation = useNavigation();
   useLayoutEffect(() => {
@@ -23,14 +25,51 @@ const PlannedWorkoutsScreen = () => {
   }, []);
   useEffect(() => {
     const getWorkouts = async () => {
-      setWorkouts(await firebase.getPlannedWorkouts(user));
+      console.log("getting workouts");
+      const workoutsArr = await firebase.getPlannedWorkouts(user);
+      console.log(workoutsArr);
+      setWorkouts(workoutsArr);
     };
     getWorkouts();
   }, []);
+  const dateString = (date) => {
+    if (now.getDate() == date.getDate()) return "Today";
+    else if (now.getDate() + 1 == date.getDate()) return "Tomorrow";
+    else {
+      const dd = date.getDate();
+      const mm = date.getMonth() + 1;
+      return dd + "/" + mm;
+    }
+  };
+  const timeString = (date) => {
+    const hh = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    const mm =
+      date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    return hh + ":" + mm;
+  };
   return (
     <SafeAreaView style={ResponsiveStyling.safeAreaStyle}>
       <View className="flex-1 px-4">
         <Header title="Planned workouts" goBackOption={true} />
+        <FlatList
+          className="p-2"
+          showsVerticalScrollIndicator={false}
+          data={workouts}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View
+              className="rounded h-32"
+              style={{ backgroundColor: appStyle.appAzure }}
+            >
+              <View className="flex-row">
+                <Text>{dateString(item.startingTime.toDate())}</Text>
+                <Text>{timeString(item.startingTime.toDate())}</Text>
+              </View>
+
+              <Text>{item.creator}</Text>
+            </View>
+          )}
+        />
       </View>
     </SafeAreaView>
   );
