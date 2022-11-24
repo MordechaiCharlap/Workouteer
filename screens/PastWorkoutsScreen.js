30f23b843974489ec126d0edb14fb128d04d5445
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-24 19:44:35 +0200

    building workout div

--- screens/PastWorkoutsScreen.js
+++ screens/PastWorkoutsScreen.js
@@ -13,7 +13,10 @@ import { useNavigation } from "@react-navigation/native";
 import Header from "../components/Header";
 import * as firebase from "../services/firebase";
 import useAuth from "../hooks/useAuth";
+import { workoutTypes } from "../components/WorkoutType";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 const FutureWorkoutsScreen = () => {
+  const workoutTypesArray = workoutTypes;
   const { user } = useAuth();
   const now = new Date();
   const [workouts, setWorkouts] = useState([]);
@@ -32,25 +35,26 @@ const FutureWorkoutsScreen = () => {
     };
     getWorkouts();
   }, []);
-  const dateString = (date) => {
-    if (now.getDate() == date.getDate()) return "Today";
-    else if (now.getDate() + 1 == date.getDate()) return "Tomorrow";
+  const timeString = (date) => {
+    var day;
+    var time;
+    if (now.getDate() == date.getDate()) day = "Today";
+    else if (now.getDate() + 1 == date.getDate()) day = "Tomorrow";
     else {
       const dd = date.getDate();
       const mm = date.getMonth() + 1;
-      return dd + "/" + mm;
+      day = dd + "/" + mm;
     }
-  };
-  const timeString = (date) => {
     const hh = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
     const mm =
       date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-    return hh + ":" + mm;
+    time = hh + ":" + mm;
+    return day + ", " + time;
   };
   return (
     <SafeAreaView style={ResponsiveStyling.safeAreaStyle}>
       <View className="flex-1 px-4">
-        <Header title="Future workouts" goBackOption={true} />
+        <Header title="Past workouts" goBackOption={true} />
         <FlatList
           className="p-2"
           showsVerticalScrollIndicator={false}
@@ -58,15 +62,51 @@ const FutureWorkoutsScreen = () => {
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
             <View
-              className="rounded h-32"
-              style={{ backgroundColor: appStyle.appAzure }}
+              className="rounded h-28"
+              style={{
+                backgroundColor: appStyle.appLightBlue,
+              }}
             >
-              <View className="flex-row">
-                <Text>{dateString(item.startingTime.toDate())}</Text>
-                <Text>{timeString(item.startingTime.toDate())}</Text>
+              <View
+                className="flex-row justify-between"
+                style={{
+                  borderBottomColor: appStyle.appDarkBlue,
+                  borderBottomWidth: 2,
+                }}
+              >
+                <Text
+                  className="text-xl rounded-t"
+                  style={{
+                    color: appStyle.appDarkBlue,
+                  }}
+                >
+                  {timeString(item.startingTime.toDate())}
+                </Text>
+                <Text
+                  className="text-xl rounded-t"
+                  style={{
+                    color: appStyle.appDarkBlue,
+                  }}
+                >
+                  {item.creator}'s workout
+                </Text>
               </View>
 
-              <Text>{item.creator}</Text>
+              <View className="flex-row flex-1">
+                <View
+                  className="justify-around items-center aspect-square"
+                  style={{
+                    borderRightColor: appStyle.appDarkBlue,
+                    borderRightWidth: 1,
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={workoutTypesArray[item.type].icon}
+                    size={60}
+                    color={appStyle.appDarkBlue}
+                  />
+                </View>
+              </View>
             </View>
           )}
         />
