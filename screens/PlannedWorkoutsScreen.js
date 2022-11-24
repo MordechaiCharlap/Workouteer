9f35eef3da140856f6c459de446e61798e430b9d
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-24 14:58:52 +0200

    get planned workouts func created

--- screens/PlannedWorkoutsScreen.js
+++ screens/PlannedWorkoutsScreen.js
@@ -5,26 +5,27 @@ import {
   TouchableOpacity,
   SafeAreaView,
 } from "react-native";
-import WorkoutType from "../components/WorkoutType";
-import WorkoutMinutes from "../components/WorkoutMinutes";
-import WorkoutStartingTime from "../components/WorkoutStartingTime";
-import WorkoutMaximumWaiting from "../components/WorkoutMaximumWaiting";
-import WorkoutDescription from "../components/WorkoutDescription";
 import React, { useState, useEffect, useLayoutEffect } from "react";
 import * as appStyle from "../components/AppStyleSheet";
 import ResponsiveStyling from "../components/ResponsiveStyling";
 import { useNavigation } from "@react-navigation/native";
-import WorkoutLocation from "../components/WorkoutLocation";
 import Header from "../components/Header";
 import * as firebase from "../services/firebase";
 import useAuth from "../hooks/useAuth";
 const PlannedWorkoutsScreen = () => {
   const { user } = useAuth();
+  const [workouts, setWorkouts] = useState([]);
   const navigation = useNavigation();
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
+  }, []);
+  useEffect(() => {
+    const getWorkouts = async () => {
+      setWorkouts(await firebase.getPlannedWorkouts(user));
+    };
+    getWorkouts();
   }, []);
   return (
     <SafeAreaView style={ResponsiveStyling.safeAreaStyle}>
