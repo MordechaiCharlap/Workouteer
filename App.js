01c77cac71d37cceb64fa1080859c97a1d4bbfe4
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-27 14:55:14 +0200

    latest map renderer

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { TailwindProvider } from "tailwindcss-react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./StackNavigator";
 import { AuthPrvider } from "./hooks/useAuth";
+import { enableLatestRenderer } from "react-native-maps";
+enableLatestRenderer();
 export default function App() {
   return (
     <NavigationContainer>
