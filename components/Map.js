ebce5d15e63f026b981139fbaecfdc3998d84920
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-27 21:21:20 +0200

    marker config

--- components/Map.js
+++ components/Map.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import * as appStyle from "./AppStyleSheet";
 const Map = (props) => {
+  const [markerCoords, setMarkerCoords] = useState(props.defaultMarker);
+  const newMarkerCoords = (coords) => {
+    setMarkerCoords(coords);
+    console.log(coords);
+  };
   console.log(props.defaultMarker);
   return (
     <View className="items-center" style={styles.container}>
@@ -16,7 +22,11 @@ const Map = (props) => {
           longitudeDelta: 0.0421,
         }}
       >
-        <Marker draggable coordinate={props.defaultMarker} />
+        <Marker
+          draggable
+          coordinate={markerCoords}
+          onDragEnd={(e) => newMarkerCoords(e.nativeEvent.coordinate)}
+        />
       </MapView>
     </View>
   );
