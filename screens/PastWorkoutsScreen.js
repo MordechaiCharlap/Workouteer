a4332846205fba86a561c69295dbbecb52c7612d
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-27 13:43:35 +0200

    removed unnecessary timeString func

--- screens/PastWorkoutsScreen.js
+++ screens/PastWorkoutsScreen.js
@@ -36,22 +36,6 @@ const FutureWorkoutsScreen = () => {
     };
     getWorkouts();
   }, []);
-  const timeString = (date) => {
-    var day;
-    var time;
-    if (now.getDate() == date.getDate()) day = "Today";
-    else if (now.getDate() + 1 == date.getDate()) day = "Tomorrow";
-    else {
-      const dd = date.getDate();
-      const mm = date.getMonth() + 1;
-      day = dd + "/" + mm;
-    }
-    const hh = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
-    const mm =
-      date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-    time = hh + ":" + mm;
-    return day + ", " + time;
-  };
   return (
     <SafeAreaView style={ResponsiveStyling.safeAreaStyle}>
       <Header title="Past workouts" goBackOption={true} />
