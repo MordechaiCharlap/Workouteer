fc945bed9ceacb65872a57fe327cfc8ce1efad25
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-27 21:55:42 +0200

    marker is moving to the chosen click!

--- components/Map.js
+++ components/Map.js
@@ -1,41 +1,70 @@
+import { faDotCircle, faLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import React from "react";
 import { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import * as appStyle from "./AppStyleSheet";
 const Map = (props) => {
-  const [markerCoords, setMarkerCoords] = useState(props.defaultMarker);
-  const newMarkerCoords = (coords) => {
-    setMarkerCoords(coords);
-    console.log(coords);
+  const [coords, setCoords] = useState(props.defaultMarker);
+  const pressed = (ltLng) => {
+    console.log(ltLng);
+    setCoords(ltLng);
   };
-  console.log(props.defaultMarker);
   return (
-    <View className="items-center" style={styles.container}>
-      <MapView
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: props.defaultMarker.latitude,
-          longitude: props.defaultMarker.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <Marker
-          draggable
-          coordinate={markerCoords}
-          onDragEnd={(e) => newMarkerCoords(e.nativeEvent.coordinate)}
-        />
-      </MapView>
+    <View className="items-center">
+      <View>
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          showsUserLocation={true}
+          onPress={(e) => pressed(e.nativeEvent.coordinate)}
+          initialRegion={{
+            latitude: props.defaultMarker.latitude,
+            longitude: props.defaultMarker.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker coordinate={coords} />
+        </MapView>
+        {/* <View className="absolute" style={{ left: "50%", top: "50%" }}>
+          <View className="flex-row">
+            <View>
+              <FontAwesomeIcon
+                icon={faDotCircle}
+                color={appStyle.appDarkBlue}
+                size={30}
+              />
+              <FontAwesomeIcon
+                icon={faDotCircle}
+                color={appStyle.appDarkBlue}
+                size={30}
+              />
+            </View>
+            <View>
+              <FontAwesomeIcon
+                icon={faDotCircle}
+                color={appStyle.appDarkBlue}
+                size={30}
+              />
+              <FontAwesomeIcon
+                icon={faDotCircle}
+                color={appStyle.appDarkBlue}
+                size={30}
+              />
+            </View>
+          </View>
+        </View> */}
+      </View>
+
+      <TouchableOpacity onPress={() => saveRegion()}>
+        <Text>Save location</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  container: {
-    height: 400,
-    width: 400,
-  },
   map: {
     height: 400,
     width: 400,
