fa9c4d651c4f92f16b4f8880bc13367e0f6305bc
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-29 12:18:22 +0200

    useEffecting mySex and everyone

--- components/WorkoutSex.js
+++ components/WorkoutSex.js
@@ -15,29 +15,57 @@ const WorkoutSex = (props) => {
   const [mySexCB, setMySexCB] = useState(false);
   const [everyoneCB, setEveryoneCB] = useState(false);
   useEffect(() => {
-    if (mySexCB == true && everyoneCB == false)
+    if (mySexCB) {
+      if (workoutSex != null) {
+        console.log("changing everyone to false");
+        setEveryoneCB(false);
+      }
       setWorkoutSex(props.user.isMale ? "men" : "women");
-  }, [mySexCB, everyoneCB]);
+    }
+  }, [mySexCB]);
+  useEffect(() => {
+    if (everyoneCB) {
+      if (workoutSex != null) {
+        console.log("changing mySex to false");
+        setMySexCB(false);
+      }
+      setWorkoutSex("everyone");
+    }
+  }, [everyoneCB]);
+
+  const renderCheckBox = (type) => {
+    if (type == "mySex") {
+      console.log("rendering mySex with", mySexCB);
+      return (
+        <View className="flex-row mr-5 items-center">
+          <CheckBox
+            onValueChange={(value) => setMySexCB(value)}
+            backgroundColor={appStyle.appAzure}
+            valueColor={appStyle.appDarkBlue}
+            value={mySexCB}
+          />
+          <Text>Only {props.user.isMale ? "Men" : "Women"}</Text>
+        </View>
+      );
+    } else {
+      console.log("rendering mySex with", everyoneCB);
+      return (
+        <View className="flex-row items-center">
+          <CheckBox
+            onValueChange={(value) => setEveryoneCB(value)}
+            backgroundColor={appStyle.appAzure}
+            valueColor={appStyle.appDarkBlue}
+            value={everyoneCB}
+          />
+          <Text>Everyone</Text>
+        </View>
+      );
+    }
+  };
   return (
     <View className="flex-row">
-      <View className="flex-row mr-5 items-center">
-        <CheckBox
-          onValueChange={(value) => setMySexCB(value)}
-          backgroundColor={appStyle.appAzure}
-          valueColor={appStyle.appDarkBlue}
-          value={mySexCB}
-        />
-        <Text>Only {props.user.isMale ? "Men" : "Women"}</Text>
-      </View>
-      <View className="flex-row items-center">
-        <CheckBox
-          onValueChange={(value) => setEveryoneCB(value)}
-          backgroundColor={appStyle.appAzure}
-          valueColor={appStyle.appDarkBlue}
-          value={everyoneCB}
-        />
-        <Text>Everyone</Text>
-      </View>
+      {renderCheckBox("mySex")}
+      {renderCheckBox("everyone")}
     </View>
   );
 };
