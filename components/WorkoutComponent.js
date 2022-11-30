c24121e232df05c73edc8fec916c046466f51220
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-11-30 13:01:17 +0200

    using timeString from timeFunctions

--- components/WorkoutComponent.js
+++ components/WorkoutComponent.js
@@ -7,27 +7,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faStopwatch, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 import useAuth from "../hooks/useAuth";
 import { useNavigation } from "@react-navigation/native";
+import { timeString } from "../services/timeFunctions";
 const WorkoutComponent = (props) => {
   const navigation = useNavigation();
   const [buttonText, setButtonText] = useState(null);
   const { user, setUser } = useAuth();
   const workoutTypesArray = workoutTypes;
-  const timeString = (date) => {
-    var day;
-    var time;
-    if (props.now.getDate() == date.getDate()) day = "Today";
-    else if (props.now.getDate() + 1 == date.getDate()) day = "Tomorrow";
-    else {
-      const dd = date.getDate();
-      const mm = date.getMonth() + 1;
-      day = dd + "/" + mm;
-    }
-    const hh = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
-    const mm =
-      date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-    time = hh + ":" + mm;
-    return day + ", " + time;
-  };
   const leaveWorkout = async (workout) => {
     setButtonText("Left");
     await firebase.leaveWorkout(user, workout);
@@ -122,7 +107,10 @@ const WorkoutComponent = (props) => {
         <View className="justify-around flex-1">
           <TouchableOpacity
             onPress={() =>
-              navigation.navigate("WorkoutDetails", { workout: props.workout })
+              navigation.navigate("WorkoutDetails", {
+                workout: props.workout,
+                timeString,
+              })
             }
             className="mx-2 h-8 justify-center rounded"
             style={{
