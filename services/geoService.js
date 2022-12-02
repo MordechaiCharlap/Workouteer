b64fc751ed234955e6b8d26c805118a0541bb31b
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-02 09:01:57 +0200

    asking for permission

--- services/geoService.js
+++ services/geoService.js
@@ -1,5 +1,10 @@
 import * as Location from "expo-location";
 export const getCurrentLocation = async () => {
+  let { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== "granted") {
+    setErrorMsg("Permission to access location was denied");
+    return;
+  }
   const location = await Location.getCurrentPositionAsync({});
   const latLongLocation = {
     latitude: location.coords.latitude,
