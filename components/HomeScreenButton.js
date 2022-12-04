f51eef1e5e0d9c023fcf677a5f1b4ac78c1b9164
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-04 13:41:00 +0200

    passing User to pastWorkouts

--- components/HomeScreenButton.js
+++ components/HomeScreenButton.js
@@ -2,9 +2,10 @@ import { TouchableOpacity, Text, View } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-
+import useAuth from "../hooks/useAuth";
 const HomeScreenButton = (props) => {
   const navigation = useNavigation();
+  const { user } = useAuth();
   return (
     <View>
       {props.spaceHolderButton ? (
@@ -13,7 +14,7 @@ const HomeScreenButton = (props) => {
         <TouchableOpacity
           className={`items-center justify-center rounded-lg w-36 h-36`}
           style={{ backgroundColor: props.style.backgroundColor }}
-          onPress={() => navigation.push(props.navigateScreen)}
+          onPress={() => navigation.push(props.navigateScreen, { user: user })}
         >
           <FontAwesomeIcon
             icon={props.icon}
