ec4dadbab4331a4fb6d80b21ae186a11fdf9851a
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-08 15:13:10 +0200

    simpler signIn for now

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -9,16 +9,22 @@ import {
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "@react-native-firebase/auth";
 import { Alert } from "react-native";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+
+GoogleSignin.configure({
+  webClientId:
+    "371037963339-poup230qmc5e6s484udrhch0m8g2ngd5.apps.googleusercontent.com",
+});
 const AuthContext = createContext({});
 
 export const AuthPrvider = ({ children }) => {
-  const auth = firebase.auth;
+  const configauth = firebase.auth;
   const [initialLoading, setInitialLoading] = useState(true);
   const [user, setUser] = useState(null);
   useEffect(() => {
-    onAuthStateChanged(auth, (authUser) => {
+    onAuthStateChanged(configauth, (authUser) => {
       if (authUser) {
         const setUserAsync = async () => {
           setUser(await firebase.userDataByEmail(authUser.email.toLowerCase()));
@@ -34,40 +40,23 @@ export const AuthPrvider = ({ children }) => {
     });
   }, []);
   const signInGoogleAccount = async () => {
-    const authTest = getAuth();
-    const provider = new GoogleAuthProvider();
-    const result = await signInWithRedirect(authTest, provider);
-    console.log(result);
-    // try {
-    //   await GoogleSignin.hasPlayServices();
-    //   const userInfo = await GoogleSignin.signIn();
-    //   console.log({ userInfo });
-    // } catch (error) {
-    //   if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-    //     // user cancelled the login flow
-    //   } else if (error.code === statusCodes.IN_PROGRESS) {
-    //     // operation (e.g. sign in) is in progress already
-    //   } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-    //     // play services not available or outdated
-    //   } else {
-    //     // some other error happened
-    //   }
-    // }
+    const userInfo = await GoogleSignin.signIn();
+    console.log(userInfo);
   };
 
   const signInEmailPassword = (email, password, rememberMe) => {
     if (!rememberMe) {
       console.log("not remembering user");
-      setPersistence(auth, inMemoryPersistence)
+      setPersistence(configauth, inMemoryPersistence)
         .then(() => {
-          signInWithEmailAndPassword(auth, email, password);
+          signInWithEmailAndPassword(configauth, email, password);
         })
         .catch((error) => {
           console.log(error);
         });
     } else {
       console.log("remembering user");
-      signInWithEmailAndPassword(auth, email, password)
+      signInWithEmailAndPassword(configauth, email, password)
         .then(async () => {
           console.log("signed in!");
         })
@@ -79,7 +68,7 @@ export const AuthPrvider = ({ children }) => {
   };
   const userSignOut = () => {
     console.log("trying to sign out");
-    signOut(auth)
+    signOut(configauth)
       .then(() => {})
       .catch((error) => {
         console.log("signed out succesfuly");
