1d9a7d38fc9279f978f1674299ddc64500d21b21
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-09 20:06:37 +0200

    new maps api key

--- geocoder.js
+++ geocoder.js
@@ -1,4 +1,4 @@
 import Geocoder from "react-native-geocoding";
 export const initGeocoder = () => {
-  Geocoder.init("AIzaSyDSprFxxCTG8mwLEfhsun43nSOvzos2-To", { language: "en" });
+  Geocoder.init("AIzaSyB82d0m9dRBff144fQmGIIHQYYOrNdDdWQ", { language: "en" });
 };
