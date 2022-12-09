157ea45bdd55e636d999bb792ad7219093fba361
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-09 20:53:41 +0200

    removing redux shit

--- store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import workoutSlice from "./slices/workoutSlice";
-
-const store = configureStore({
-  reducer: workoutSlice.reducer,
-});
-export default store;
