6175b7f90dae088b44162ca3112a60a976b6d2ca
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-09 15:50:12 +0200

    removing unsopported fb auth funcs

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -10,9 +10,7 @@ import {
   signOut,
 } from "firebase/auth";
 import { Alert } from "react-native";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 const AuthContext = createContext({});
-const provider = new GoogleAuthProvider();
 export const AuthPrvider = ({ children }) => {
   const auth = firebase.auth;
   const [initialLoading, setInitialLoading] = useState(true);
@@ -35,25 +33,6 @@ export const AuthPrvider = ({ children }) => {
   }, []);
   const signInGoogleAccount = () => {
     console.log("Opening login-google func!");
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        // ...
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-      });
   };
   const signInEmailPassword = (email, password, rememberMe) => {
     if (!rememberMe) {
