8878ee18cf40844bd8a8ea6b637f9dfac31c67cb
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-09 15:47:39 +0200

    trying firebase app auth

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -10,8 +10,9 @@ import {
   signOut,
 } from "firebase/auth";
 import { Alert } from "react-native";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 const AuthContext = createContext({});
-
+const provider = new GoogleAuthProvider();
 export const AuthPrvider = ({ children }) => {
   const auth = firebase.auth;
   const [initialLoading, setInitialLoading] = useState(true);
@@ -32,7 +33,28 @@ export const AuthPrvider = ({ children }) => {
       }
     });
   }, []);
-  const signInGoogleAccount = () => {};
+  const signInGoogleAccount = () => {
+    console.log("Opening login-google func!");
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
   const signInEmailPassword = (email, password, rememberMe) => {
     if (!rememberMe) {
       console.log("not remembering user");
