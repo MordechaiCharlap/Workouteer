eac4be736d8829ac9137719e156d9092fd776923
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-26 11:14:18 +0200

    more codeeetsafsa\f

--- hooks/useNotifications.js
+++ hooks/useNotifications.js
@@ -1,8 +1,38 @@
-import { createContext } from "react";
+import { createContext, useState, useRef, useEffect } from "react";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 const NotificationsContext = createContext();
 export const NotificationsProvider = ({ children }) => {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   const registerForPushNotificationsAsync = async () => {
     if (Device.isDevice) {
       const { status: existingStatus } =
