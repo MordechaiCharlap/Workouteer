47434a5847f7fa54b20e426fbc7bf1bb8688ee4e
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-26 11:25:14 +0200

    sendPushNotification function

--- hooks/useNotifications.js
+++ hooks/useNotifications.js
@@ -32,7 +32,25 @@ export const NotificationsProvider = ({ children }) => {
       Notifications.removeNotificationSubscription(responseListener.current);
     };
   }, []);
+  const sendPushNotification = async (expoPushToken) => {
+    const message = {
+      to: expoPushToken,
+      sound: "default",
+      title: "Original Title",
+      body: "And here is the body!",
+      data: { someData: "goes here" },
+    };
 
+    await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Accept-encoding": "gzip, deflate",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    });
+  };
   const registerForPushNotificationsAsync = async () => {
     if (Device.isDevice) {
       const { status: existingStatus } =
