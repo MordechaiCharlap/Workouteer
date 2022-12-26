b6ac936c1c350737c0f007836fcedb919f6c3c1e
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-26 11:09:51 +0200

    added notifications provider

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { TailwindProvider } from "tailwindcss-react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./StackNavigator";
 import { AuthPrvider } from "./hooks/useAuth";
+import { NotificationsProvider } from "./hooks/useNotifications";
 import { enableLatestRenderer } from "react-native-maps";
 import { initGeocoder } from "./geocoder";
 import * as WebBrowser from "expo-web-browser";
@@ -14,9 +15,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <AuthPrvider>
-        <TailwindProvider>
-          <StackNavigator />
-        </TailwindProvider>
+        <NotificationsProvider>
+          <TailwindProvider>
+            <StackNavigator />
+          </TailwindProvider>
+        </NotificationsProvider>
       </AuthPrvider>
     </NavigationContainer>
   );
