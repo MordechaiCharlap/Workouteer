e39896e4bbad4eeed36b680a2d61018b7d8b1101
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-27 19:43:53 +0200

    location not showing bug fixed

--- services/geoService.js
+++ services/geoService.js
@@ -1,11 +1,15 @@
 import * as Location from "expo-location";
 export const getCurrentLocation = async () => {
   let { status } = await Location.requestForegroundPermissionsAsync();
+
   if (status !== "granted") {
-    setErrorMsg("Permission to access location was denied");
+    console.log("Permission to access location was denied");
     return;
   }
-  const location = await Location.getCurrentPositionAsync({});
+  const location = await Location.getCurrentPositionAsync({
+    accuracy: Location.Accuracy.Highest,
+    maximumAge: 10000,
+  });
   const latLongLocation = {
     latitude: location.coords.latitude,
     longitude: location.coords.longitude,
