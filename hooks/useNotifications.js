f6a4568b56b057bb4a8b295cd476289282c190b5
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-27 20:44:36 +0200

    better code same shit

--- hooks/useNotifications.js
+++ hooks/useNotifications.js
@@ -79,18 +79,17 @@ const registerForPushNotifications = async () => {
       return;
     }
     const token = (await Notifications.getExpoPushTokenAsync()).data;
-    setExpoPushToken(token);
+    console.log(token);
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+    return token;
   } else {
     alert("Must use physical device for Push Notifications");
   }
-
-  if (Platform.OS === "android") {
-    Notifications.setNotificationChannelAsync("default", {
-      name: "default",
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: "#FF231F7C",
-    });
-  }
-  return token;
 };
