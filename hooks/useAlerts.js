d9420c7e2da7712e66b9e952022058f1f6de39d8
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-30 00:56:51 +0200

    cleaner useAuth

--- hooks/useAlerts.js
+++ hooks/useAlerts.js
@@ -1,10 +1,12 @@
-import React from "react";
-import { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect } from "react";
 import * as firebase from "../services/firebase";
-import { useEffect, useState } from "react";
 const AlertsContext = createContext({});
 export const AuthPrvider = ({ children }) => {
-  return <AlertsContext.Provider value={{}}>{children}</AlertsContext.Provider>;
+  return (
+    <AlertsContext.Provider value={{ test: "test" }}>
+      {children}
+    </AlertsContext.Provider>
+  );
 };
 export default function useAlerts() {
   return useContext(AlertsContext);
