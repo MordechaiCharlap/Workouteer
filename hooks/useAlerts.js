86aa40cd0e392a2e185851857adc6bc079e9efcf
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2022-12-30 01:22:38 +0200

    typo fixed

--- hooks/useAlerts.js
+++ hooks/useAlerts.js
@@ -1,7 +1,6 @@
 import React, { createContext, useContext, useEffect } from "react";
-import * as firebase from "../services/firebase";
 const AlertsContext = createContext({});
-export const AuthPrvider = ({ children }) => {
+export const AlertsProvider = ({ children }) => {
   return (
     <AlertsContext.Provider value={{ test: "test" }}>
       {children}
