98459ce30bb5cd17232f276bbd018f08749c7144
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-04 12:57:19 +0200

    listening to all kinds of alerts

--- hooks/useAlerts.js
+++ hooks/useAlerts.js
@@ -1,10 +1,22 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import useAuth from "./useAuth";
 const AlertsContext = createContext({});
 export const AlertsProvider = ({ children }) => {
+  const [chatAlerts, setChatAlerts] = useState(false);
+  const [workoutRequestsAlerts, setWorkoutRequestsAlerts] = useState(false);
+  const [workoutInvitesAlerts, setWorkoutInvitesAlerts] = useState(false);
+  const [friendRequestsAlerts, setFriendRequestsAlerts] = useState(false);
   return (
     <AlertsContext.Provider
-      value={{}}
+      value={{
+        chatAlerts,
+        setChatAlerts,
+        workoutRequestsAlerts,
+        setWorkoutRequestsAlerts,
+        workoutInvitesAlerts,
+        setWorkoutInvitesAlerts,
+        friendRequestsAlerts,
+        setFriendRequestsAlerts,
+      }}
     >
       {children}
     </AlertsContext.Provider>
