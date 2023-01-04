ebbbc0a5f5a075554433a30f9647b620fdb07787
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-04 15:15:52 +0200

    styling home button

--- components/NavbarButton.js
+++ components/NavbarButton.js
@@ -34,26 +34,29 @@ const NavbarButton = (props) => {
         props.screen != props.currentScreen ? style.button : style.currentButton
       }
     >
+      {props.screen == "Home" && (
+        <View className="absolute h-max w-max items-center justify-center">
+          <FontAwesomeIcon icon={getIcon()} size={50} color={appStyle.appRed} />
+        </View>
+      )}
+
       <View>
         <FontAwesomeIcon
           icon={getIcon()}
-          size={props.screen == "Home" ? 40 : 30}
-          color={
-            props.screen == "Home" ? appStyle.appRed : appStyle.appDarkBlue
-          }
+          size={props.screen == "Home" ? 45 : 30}
+          color={appStyle.appDarkBlue}
         />
-        {props.screen != "Home" &&
-          props.alert != null &&
-          props.alert == true && (
-            <View
-              className="absolute bg-white w-4 h-4 left-0 bottom-0 rounded-full justify-center items-center"
-              style={{
-                backgroundColor: appStyle.appRed,
-                borderColor: appStyle.appDarkBlue,
-                borderWidth: 2,
-              }}
-            ></View>
-          )}
+
+        {props.alert != null && props.alert == true && (
+          <View
+            className="absolute bg-white w-4 h-4 left-0 bottom-0 rounded-full justify-center items-center"
+            style={{
+              backgroundColor: appStyle.appRed,
+              borderColor: appStyle.appDarkBlue,
+              borderWidth: 2,
+            }}
+          ></View>
+        )}
       </View>
     </TouchableOpacity>
   );
