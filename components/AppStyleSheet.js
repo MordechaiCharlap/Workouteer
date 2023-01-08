1d0f5071f006560fbcedc36aab2a5bc1f3a992f8
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-08 13:37:38 +0200

    status bar default

--- components/AppStyleSheet.js
+++ components/AppStyleSheet.js
@@ -1,11 +1,11 @@
-export const color_primary = "#219ebc";
+export const color_primary = "#023047";
 export const color_primary_variant = "#023047";
 export const color_on_primary = "#ffffff";
 export const color_secondary = "#ffb703";
 export const color_secondary_variant = "#fb8500";
 export const color_on_secondary = "#000000";
 export const color_bg = "#8ecae6";
-
+export const color_bg_variant = "#1e688b";
 export const appLightBlue = "#8ecae6";
 export const appAzure = "#219ebc";
 export const appDarkBlue = "#14213D";
@@ -16,6 +16,6 @@ export const appDarkBlueGrayer = "#4d5973";
 export const appNeonAzure = "#03dac6";
 export const appRed = "#de265f";
 export const statusBarStyle = {
-  backgroundColor: appLightBlue,
-  barStyle: "dark-content",
+  backgroundColor: color_bg_variant,
+  barStyle: "light-content",
 };
