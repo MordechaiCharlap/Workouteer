e8b81dd48cff894de0d05d11e41c3a4f20fc7bab
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-08 13:12:44 +0200

    color names palette

--- components/AppStyleSheet.js
+++ components/AppStyleSheet.js
@@ -1,8 +1,16 @@
-export const appOrange = "#fb8500";
-export const appYellow = "#FCA311";
-export const appDarkBlue = "#14213D";
+export const color_primary = "#219ebc";
+export const color_primary_variant = "#023047";
+export const color_on_primary = "#ffffff";
+export const color_secondary = "#ffb703";
+export const color_secondary_variant = "#fb8500";
+export const color_on_secondary = "#000000";
+export const color_bg = "#8ecae6";
+
 export const appLightBlue = "#8ecae6";
 export const appAzure = "#219ebc";
+export const appDarkBlue = "#14213D";
+export const appOrange = "#fb8500";
+export const appYellow = "#ffb703";
 export const appGray = "#E5E5E5";
 export const appDarkBlueGrayer = "#4d5973";
 export const appNeonAzure = "#03dac6";
