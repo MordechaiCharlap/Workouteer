5ba1a3a04e735bf122cab9325ba2cd1be37f67e3
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-11 12:39:42 +0200

    styling

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -37,107 +37,99 @@ const LoginScreen = () => {
       {initialLoading ? (
         <LoadingAnimation />
       ) : (
-        <View className="flex-1 my-20 mx-6">
-          <View
-            className={`mb-5 basis-4/5 rounded-t-xl p-4  justify-between ${ResponsiveShadow}`}
-            style={{
-              backgroundColor: appStyle.color_primary,
-              shadowColor: "#000",
-            }}
-          >
-            <View className="my-3 items-center">
-              <FontAwesomeIcon
-                icon={faCircleUser}
-                color={appStyle.color_on_primary}
-                size={50}
-              />
-              <Text
-                style={{ color: appStyle.color_on_primary }}
-                className="text-2xl my-4"
-              >
-                {"Welcome :)"}
-              </Text>
-              <Text style={{ color: appStyle.color_on_primary }}>
-                Sign in and find a partner for your next workout TODAY!
-              </Text>
-            </View>
-            <View>
-              {/* focus:border-sky-500 focus:border-2 */}
-              <TextInput
-                className="rounded mb-5 px-3 py-1 focus:"
-                style={style.input}
-                placeholder="Email"
-                placeholderTextColor={"#5f6b8b"}
-                onChangeText={(text) => setEmail(text)}
-              ></TextInput>
-              <TextInput
-                className="rounded mb-5 px-3 py-1"
-                secureTextEntry={true}
-                style={style.input}
-                placeholder="Password"
-                placeholderTextColor={"#5f6b8b"}
-                onChangeText={(text) => setPassword(text)}
-              ></TextInput>
-              <View className="flex-row items-center">
-                <CheckBox
-                  valueColor={appStyle.color_primary}
-                  backgroundColor={appStyle.color_bg}
-                  value={false}
-                  onValueChange={(value) => setRememberMe(value)}
-                />
-                <Text
-                  className="ml-2"
-                  style={{ color: appStyle.color_on_primary }}
-                >
-                  Remember me!
-                </Text>
-              </View>
-            </View>
-            <TouchableOpacity
-              onPress={() => signInEmailPassword(email, password, rememberMe)}
-              className={`self-center rounded py-2 px-8 w-full`}
+        <View className="flex-1 justify-center">
+          <View className="mx-6">
+            <View
+              className={`mb-5 rounded-t-xl p-3 justify-between ${ResponsiveShadow}`}
               style={{
                 backgroundColor: appStyle.color_primary,
-                borderWidth: 0.5,
-                borderColor: appStyle.color_on_primary,
+                shadowColor: "#000",
               }}
             >
-              <Text
-                className="text-center tracking-widest font-bold text-xl"
+              <View className="my-3 items-center">
+                <FontAwesomeIcon
+                  icon={faCircleUser}
+                  color={appStyle.color_on_primary}
+                  size={50}
+                />
+                <Text
+                  style={{ color: appStyle.color_on_primary }}
+                  className="text-2xl my-4"
+                >
+                  {"Welcome :)"}
+                </Text>
+                <Text style={{ color: appStyle.color_on_primary }}>
+                  Sign in and find a partner for your next workout TODAY!
+                </Text>
+              </View>
+              <View>
+                {/* focus:border-sky-500 focus:border-2 */}
+                <TextInput
+                  className="rounded mb-5 px-3 py-1 focus:"
+                  style={style.input}
+                  placeholder="Email"
+                  placeholderTextColor={"#5f6b8b"}
+                  onChangeText={(text) => setEmail(text)}
+                ></TextInput>
+                <TextInput
+                  className="rounded mb-5 px-3 py-1"
+                  secureTextEntry={true}
+                  style={style.input}
+                  placeholder="Password"
+                  placeholderTextColor={"#5f6b8b"}
+                  onChangeText={(text) => setPassword(text)}
+                ></TextInput>
+                <View className="flex-row items-center mb-5">
+                  <CheckBox
+                    valueColor={appStyle.color_primary}
+                    backgroundColor={appStyle.color_bg}
+                    value={false}
+                    onValueChange={(value) => setRememberMe(value)}
+                  />
+                  <Text
+                    className="ml-2"
+                    style={{ color: appStyle.color_on_primary }}
+                  >
+                    Remember me!
+                  </Text>
+                </View>
+              </View>
+              <TouchableOpacity
+                onPress={() => signInEmailPassword(email, password, rememberMe)}
+                className={`self-center rounded py-2 px-8 w-full mb-3`}
                 style={{
-                  color: appStyle.color_on_primary,
+                  backgroundColor: appStyle.color_bg,
                 }}
               >
-                Login
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => signInGoogleAccount()}
-              className={`self-center rounded py-2 px-8 w-full`}
-              style={{
-                backgroundColor: appStyle.color_on_primary,
-              }}
-            >
-              <Text
-                className="text-center tracking-widest font-bold text-xl"
+                <Text
+                  className="text-center tracking-widest font-bold text-xl"
+                  style={{
+                    color: appStyle.color_primary,
+                  }}
+                >
+                  Login
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => signInGoogleAccount()}
+                className={`self-center rounded py-2 px-8 w-full`}
                 style={{
-                  color: appStyle.color_primary,
+                  backgroundColor: appStyle.color_on_primary,
                 }}
               >
-                Continue with Google
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View
-            className={`basis-1/5 rounded-b-xl justify-center p-2 ${ResponsiveShadow}`}
-            style={{
-              backgroundColor: appStyle.color_on_primary,
-              shadowColor: "#000",
-            }}
-          >
+                <Text
+                  className="text-center tracking-widest font-bold text-xl"
+                  style={{
+                    color: appStyle.color_primary,
+                  }}
+                >
+                  Continue with Google
+                </Text>
+              </TouchableOpacity>
+            </View>
             <TouchableOpacity
               onPress={() => navigation.navigate("Register")}
-              className={`flex-1 rounded-b-xl justify-center ${ResponsiveShadow}`}
+              className={`flex-1 rounded-b-xl justify-center p-3 ${ResponsiveShadow}`}
               style={{
                 backgroundColor: appStyle.color_primary,
                 shadowColor: appStyle.appLightBlue,
@@ -160,9 +152,11 @@ const LoginScreen = () => {
 export default LoginScreen;
 const style = StyleSheet.create({
   input: {
-    backgroundColor: appStyle.color_primary,
-    borderWidth: 1,
-    borderColor: "#5f6b8b",
-    color: appStyle.color_on_primary,
+    paddingLeft: 10,
+    height: 40,
+    borderWidth: 2,
+    borderColor: appStyle.color_bg,
+    color: appStyle.color_primary,
+    backgroundColor: appStyle.color_on_primary,
   },
 });
