9185c06d823e2a7b80932f76407f7538f7639715
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-13 14:25:07 +0200

    SO MUCH cleaner

--- screens/EditDataScreen.js
+++ screens/EditDataScreen.js
@@ -23,7 +23,6 @@ import * as defaultValues from "../services/defaultValues";
 import Header from "../components/Header";
 const EditDataScreen = () => {
   const navigation = useNavigation();
-  const [currentTab, setCurrentTab] = useState("ProfileData");
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -39,55 +38,203 @@ const EditDataScreen = () => {
       <Header title={"Edit personal data"} goBackOption={true} />
       <View className="flex-1 p-4">
         <EditProfileData navigation={navigation} />
-        {/* <View className="flex-row justify-around mb-8">
-          <TouchableOpacity
-            onPress={() => setCurrentTab("ProfileData")}
-            className="w-1/2"
-          >
-            <Text
-              className="text-lg text-center"
-              style={
-                currentTab == "ProfileData" ? style.currentTab : style.otherTab
-              }
-            >
-              Profile data
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => setCurrentTab("WorkoutPreferences")}
-            className="w-1/2"
-          >
-            <Text
-              className="text-lg text-center"
-              style={
-                currentTab == "WorkoutPreferences"
-                  ? style.currentTab
-                  : style.otherTab
-              }
-            >
-              Workout preferences
-            </Text>
-          </TouchableOpacity>
-        </View>
-        {currentTab == "ProfileData" && (
-          <EditProfileData
-            user={user}
-            setUser={setUser}
-            navigation={navigation}
-          />
-        )}
-        {currentTab == "WorkoutPreferences" && (
-          <EditWorkoutPreferences
-            user={user}
-            setUser={setUser}
-            navigation={navigation}
-          />
-        )} */}
       </View>
     </View>
   );
 };
 export default EditDataScreen;
+
+const EditProfileData = (props) => {
+  const { user, setUser } = useAuth();
+  const [displayName, setDisplayName] = useState(user.displayName);
+  const [description, setDescription] = useState(user.description);
+  const [image, setImage] = useState(user.img);
+  const [imageLoading, setImageLoading] = useState(false);
+  useEffect(() => {
+    if (
+      description == user.description &&
+      displayName == user.displayName &&
+      image == user.img
+    )
+      setChangesMade(false);
+    else setChangesMade(true);
+  }, [displayName, description, image]);
+  const [updated, setUpdated] = useState(false);
+  const [changesMade, setChangesMade] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+
+  const onImageLibraryPress = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      setImageLoading(true);
+      const manipResult = await ImageManipulator.manipulateAsync(
+        result.localUri || result.uri,
+        [{ resize: { height: 1080, width: 1080 } }],
+        {
+          compress: 0.5,
+          height: 1080,
+          width: 1080,
+          format: ImageManipulator.SaveFormat.JPEG,
+        }
+      );
+      const uploadUrl = await firebase.uploadProfileImage(
+        user.id,
+        manipResult.uri
+      );
+      console.log("uploadUrl: " + uploadUrl);
+      setImage(uploadUrl);
+    }
+    setImageLoading(false);
+  };
+  const saveProfileChanges = async () => {
+    setLoading(true);
+    if (displayName == "") setDisplayName(user.username);
+    await firebase.saveProfileChanges(
+      user.id,
+      displayName == null ? "" : displayName,
+      description == null ? "" : description,
+      image == null ? defaultValues.defaultProfilePic : image
+    );
+    setUser(await firebase.updateContext(user.id));
+
+    setUpdated(true);
+    setTimeout(() => {
+      setLoading(false);
+      setChangesMade(false);
+      setUpdated(false);
+      props.navigation.navigate("MyUser");
+    }, 500);
+  };
+
+  const saveButtonClicked = () => {
+    if (changesMade == true) {
+      if (isLoading == false) saveProfileChanges();
+    }
+  };
+  const SaveButton = () => {
+    return (
+      <TouchableOpacity
+        onPress={saveButtonClicked}
+        className="self-center py-1 px-5 w-9/12 rounded"
+        style={{
+          backgroundColor:
+            updated == false ? appStyle.color_primary : appStyle.color_primary,
+        }}
+      >
+        <Text
+          className="text-2xl text-center"
+          style={{ color: appStyle.color_on_primary }}
+        >
+          {updated == true && "Updated successfuly!"}
+          {updated == false && changesMade == false && "No changes made"}
+          {updated == false &&
+            changesMade == true &&
+            isLoading == false &&
+            "Save Changes"}
+          {updated == false &&
+            changesMade == true &&
+            isLoading == true &&
+            "Loading"}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+  return (
+    <View>
+      <View className="mb-5 self-center">
+        {imageLoading ? (
+          <View
+            className="h-32 w-32 rounded-full mb-2 items-center justify-center"
+            style={{
+              borderWidth: 1,
+              borderColor: appStyle.color_primary,
+              backgroundColor: appStyle.color_bg,
+            }}
+          >
+            <Text
+              className="text-xl font-bold"
+              style={{ color: appStyle.color_primary }}
+            >
+              Loading
+            </Text>
+          </View>
+        ) : (
+          <Image
+            source={{
+              uri: image,
+            }}
+            className="h-32 w-32 bg-white rounded-full mb-2"
+            style={{ borderWidth: 1, borderColor: appStyle.color_primary }}
+          />
+        )}
+        <TouchableOpacity
+          onPress={onImageLibraryPress}
+          className="absolute right-0 bottom-0 rounded-full p-2"
+          style={{
+            backgroundColor: appStyle.color_primary,
+            borderColor: appStyle.color_bg,
+            borderWidth: 3,
+          }}
+        >
+          <FontAwesomeIcon
+            icon={faPen}
+            size={20}
+            color={appStyle.color_on_primary}
+          />
+        </TouchableOpacity>
+      </View>
+      <View className="flex-row items-center mb-5">
+        <Text
+          className="mr-3 text-lg"
+          style={{ color: appStyle.color_primary }}
+        >
+          Display name:
+        </Text>
+        <TextInput
+          className="rounded text-lg flex-1 px-2"
+          style={style.input}
+          placeholder={user.displayName}
+          placeholderTextColor={"#5f6b8b"}
+          maxLength={10}
+          onChangeText={(text) => setDisplayName(text)}
+        >
+          {user.displayName}
+        </TextInput>
+      </View>
+      <View className="mb-5">
+        <Text
+          className="mr-3 text-lg"
+          style={{ color: appStyle.color_primary }}
+        >
+          Description
+        </Text>
+        <TextInput
+          style={{
+            textAlignVertical: "top",
+            color: appStyle.color_on_primary,
+            backgroundColor: appStyle.color_bg_variant,
+            borderRadius: 8,
+            padding: 8,
+          }}
+          multiline
+          placeholder="Optional text"
+          placeholderTextColor={appStyle.color_primary}
+          numberOfLines={12}
+          maxLength={350}
+          onChangeText={(text) => setDescription(text)}
+        >
+          {user.description}
+        </TextInput>
+      </View>
+      {SaveButton()}
+    </View>
+  );
+};
 const EditWorkoutPreferences = (props) => {
   const [minAge, setMinAge] = useState(props.user.acceptMinAge);
   const [maxAge, setMaxAge] = useState(props.user.acceptMaxAge);
@@ -260,176 +407,6 @@ const EditWorkoutPreferences = (props) => {
             Female
           </Text>
         </View>
-      </View>
-      {SaveButton()}
-    </View>
-  );
-};
-const EditProfileData = (props) => {
-  const { user, setUser } = useAuth();
-  const [displayName, setDisplayName] = useState(user.displayName);
-  const [description, setDescription] = useState(user.description);
-  const [image, setImage] = useState(user.img);
-  useEffect(() => {
-    if (
-      description == user.description &&
-      displayName == user.displayName &&
-      image == user.img
-    )
-      setChangesMade(false);
-    else setChangesMade(true);
-  }, [displayName, description, image]);
-  const [updated, setUpdated] = useState(false);
-  const [changesMade, setChangesMade] = useState(false);
-  const [isLoading, setLoading] = useState(false);
-
-  const onImageLibraryPress = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1,
-    });
-    if (!result.cancelled) {
-      const manipResult = await ImageManipulator.manipulateAsync(
-        result.localUri || result.uri,
-        [{ resize: { height: 1080, width: 1080 } }],
-        {
-          compress: 0.5,
-          height: 1080,
-          width: 1080,
-          format: ImageManipulator.SaveFormat.JPEG,
-        }
-      );
-      const uploadUrl = await firebase.uploadProfileImage(
-        user.id,
-        manipResult.uri
-      );
-      console.log("uploadUrl: " + uploadUrl);
-      setImage(uploadUrl);
-    }
-  };
-  const saveProfileChanges = async () => {
-    setLoading(true);
-    if (displayName == "") setDisplayName(user.username);
-    await firebase.saveProfileChanges(
-      user.id,
-      displayName == null ? "" : displayName,
-      description == null ? "" : description,
-      image == null ? defaultValues.defaultProfilePic : image
-    );
-    setUser(await firebase.updateContext(user.id));
-
-    setUpdated(true);
-    setTimeout(() => {
-      setLoading(false);
-      setChangesMade(false);
-      setUpdated(false);
-      props.navigation.navigate("MyUser");
-    }, 500);
-  };
-
-  const saveButtonClicked = () => {
-    if (changesMade == true) {
-      if (isLoading == false) saveProfileChanges();
-    }
-  };
-  const SaveButton = () => {
-    return (
-      <TouchableOpacity
-        onPress={saveButtonClicked}
-        className="self-center py-1 px-5 w-9/12 rounded"
-        style={{
-          backgroundColor:
-            updated == false ? appStyle.color_primary : appStyle.color_primary,
-        }}
-      >
-        <Text
-          className="text-2xl text-center"
-          style={{ color: appStyle.color_on_primary }}
-        >
-          {updated == true && "Updated successfuly!"}
-          {updated == false && changesMade == false && "No changes made"}
-          {updated == false &&
-            changesMade == true &&
-            isLoading == false &&
-            "Save Changes"}
-          {updated == false &&
-            changesMade == true &&
-            isLoading == true &&
-            "Loading"}
-        </Text>
-      </TouchableOpacity>
-    );
-  };
-  return (
-    <View>
-      <View className="mb-5 self-center">
-        <Image
-          source={{
-            uri: image,
-          }}
-          className="h-32 w-32 bg-white rounded-full mb-2"
-          style={{ borderWidth: 1, borderColor: appStyle.color_primary }}
-        />
-        <TouchableOpacity
-          onPress={onImageLibraryPress}
-          className="absolute right-0 bottom-0 rounded-full p-2"
-          style={{
-            backgroundColor: appStyle.color_primary,
-            borderColor: appStyle.color_bg,
-            borderWidth: 3,
-          }}
-        >
-          <FontAwesomeIcon
-            icon={faPen}
-            size={20}
-            color={appStyle.color_on_primary}
-          />
-        </TouchableOpacity>
-      </View>
-      <View className="flex-row items-center mb-5">
-        <Text
-          className="mr-3 text-lg"
-          style={{ color: appStyle.color_primary }}
-        >
-          Display name:
-        </Text>
-        <TextInput
-          className="rounded text-lg flex-1 px-2"
-          style={style.input}
-          placeholder={user.displayName}
-          placeholderTextColor={"#5f6b8b"}
-          maxLength={10}
-          onChangeText={(text) => setDisplayName(text)}
-        >
-          {user.displayName}
-        </TextInput>
-      </View>
-      <View className="mb-5">
-        <Text
-          className="mr-3 text-lg"
-          style={{ color: appStyle.color_primary }}
-        >
-          Description
-        </Text>
-        <TextInput
-          style={{
-            textAlignVertical: "top",
-            color: appStyle.color_on_primary,
-            backgroundColor: appStyle.color_bg_variant,
-            borderRadius: 8,
-            padding: 8,
-          }}
-          multiline
-          placeholder="Optional text"
-          placeholderTextColor={appStyle.color_primary}
-          numberOfLines={12}
-          maxLength={350}
-          onChangeText={(text) => setDescription(text)}
-        >
-          {user.description}
-        </TextInput>
       </View>
       {SaveButton()}
     </View>
