9f496b0faf4d4ba32fd95637bd35f9b9d3857cd3
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-13 18:38:39 +0200

    Home buttons strings added

---
+++ services/languageService.js
@@ -0,0 +1,21 @@
+const languageService = {
+  english: {
+    login: "Login",
+    createWorkout: "CREATE WORKOUT",
+    findWorkout: "FIND A WORKOUT",
+    pastWorkouts: "PAST WORKOUTS",
+    futureWorkouts: "FUTURE WORKOUTS",
+    friendsWorkouts: "FRIENDS WORKOUTS",
+    workoutInvites: "WORKOUT INVITES",
+  },
+  hebrew: {
+    login: "התחבר",
+    createWorkout: "יצירת אימון חדש",
+    findWorkout: "חיפוש אימונים",
+    pastWorkouts: "אימונים קודמים",
+    futureWorkouts: "אימונים עתידיים",
+    friendsWorkouts: "אימונים של חברים",
+    workoutInvites: "הזמנות לאימונים",
+  },
+};
+export default languageService;
