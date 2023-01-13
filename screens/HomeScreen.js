eb8b5c6023a64a996433ff893054b92737ab3316
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-13 18:39:44 +0200

    generic strings for multilanguage added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,11 +15,15 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import useAlerts from "../hooks/useAlerts";
 import useNavbarNavigation from "../hooks/useNavbarNavigation";
+import languageService from "../services/languageService";
+import useAuth from "../hooks/useAuth";
 const HomeScreen = () => {
   const navigation = useNavigation();
+
   const { workoutRequestsAlerts, newWorkoutsAlerts, workoutInvitesAlerts } =
     useAlerts();
   const { setScreen } = useNavbarNavigation();
+  const { user } = useAuth();
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -45,13 +49,13 @@ const HomeScreen = () => {
       <View className="flex-1 p-3 justify-center">
         <View className="flex-row justify-around my-5">
           <HomeScreenButton
-            buttonText="FIND A WORKOUT"
+            buttonText={languageService[user.language].findWorkout}
             style={buttonStyle}
             navigateScreen="FindWorkout"
             icon={faMagnifyingGlass}
           />
           <HomeScreenButton
-            buttonText="CREATE WORKOUT"
+            buttonText={languageService[user.language].createWorkout}
             style={buttonStyle}
             navigateScreen="NewWorkout"
             icon={faPlus}
@@ -67,13 +71,13 @@ const HomeScreen = () => {
               Object.keys(workoutRequestsAlerts).length +
               Object.keys(newWorkoutsAlerts).length
             }
-            buttonText="FUTURE WORKOUTS"
+            buttonText={languageService[user.language].futureWorkouts}
             style={buttonStyle}
             navigateScreen="FutureWorkouts"
             icon={faClock}
           />
           <HomeScreenButton
-            buttonText="PAST WORKOUTS"
+            buttonText={languageService[user.language].pastWorkouts}
             style={buttonStyle}
             navigateScreen="PastWorkouts"
             icon={faCalendarCheck}
@@ -81,7 +85,7 @@ const HomeScreen = () => {
         </View>
         <View className="flex-row justify-around my-5">
           <HomeScreenButton
-            buttonText="FRIENDS WORKOUTS"
+            buttonText={languageService[user.language].friendsWorkouts}
             style={buttonStyle}
             navigateScreen="FriendsWorkouts"
             icon={faUserGroup}
@@ -89,7 +93,7 @@ const HomeScreen = () => {
           <HomeScreenButton
             alert={Object.keys(workoutInvitesAlerts).length > 0}
             alertNumber={Object.keys(workoutInvitesAlerts).length}
-            buttonText="WORKOUT INVITES"
+            buttonText={languageService[user.language].workoutInvites}
             style={buttonStyle}
             navigateScreen="WorkoutInvites"
             icon={faEnvelopeOpenText}
