66a3d8e52993a5be5e8c21b492e34ce73b9ca534
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-14 14:48:41 +0200

    implementing create user with email and password

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -6,6 +6,7 @@ import {
   setPersistence,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
 import { onSnapshot, doc } from "firebase/firestore";
@@ -103,6 +104,18 @@ export const AuthPrvider = ({ children }) => {
     console.log("promptAsyncing!");
     await promptAsync({ useProxy: false, showInRecents: true });
   };
+  const createUserEmailAndPassword = async (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
   const setGoogleUserAsync = async () => {
     setInitialLoading(true);
     if (
@@ -179,6 +192,7 @@ export const AuthPrvider = ({ children }) => {
         user,
         setUser,
         googleUserInfo,
+        createUserEmailAndPassword,
         signInEmailPassword,
         signInGoogleAccount,
         setGoogleUserAsync,
