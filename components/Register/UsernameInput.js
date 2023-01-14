8ef80299d0f9f414f0c9206dee871a74375ce8e0
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-14 14:21:31 +0200

    cleaner code

--- components/Register/UsernameInput.js
+++ components/Register/UsernameInput.js
@@ -1,32 +1,39 @@
-import { TextInput } from "react-native";
-import React from "react";
-
+import { TextInput, View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import * as appStyle from "../AppStyleSheet";
 const UsernameInput = (props) => {
   const [usernameStyle, setUsernameStyle] = useState(props.style.input);
+  const [error, setError] = useState(null);
 
-  const isRegexUsername = (text) => {
-    var validRegex = /^[a-zA-Z0-9]{6,20}$/;
-    if (text.match(validRegex)) {
-      return true;
-    } else return false;
-  };
-
-  const usernameLostFocus = () => {
-    if (isRegexUsername(username)) {
+  const changedTextHandler = (text) => {
+    const validUsername = /^[a-zA-Z0-9]{6,20}$/.test(text);
+    if (validUsername) {
       setUsernameStyle(props.style.input);
+      setError(null);
+      props.valueChanged(text);
     } else {
       setUsernameStyle(props.style.badInput);
+      setError("English/numbers, 6-20 characters");
+      props.valueChanged(null);
     }
   };
   return (
-    <TextInput
-      onBlur={usernameLostFocus}
-      className="justify-center mb-5"
-      style={usernameStyle}
-      placeholder="Username (6+ English characters/numbers)"
-      placeholderTextColor={"#5f6b8b"}
-      onChangeText={(text) => setUsername(text)}
-    ></TextInput>
+    <View style={props.style.inputContainer}>
+      <TextInput
+        style={usernameStyle}
+        placeholder="Username"
+        placeholderTextColor={"#5f6b8b"}
+        onChangeText={(text) => changedTextHandler(text)}
+      ></TextInput>
+      <Text
+        style={{
+          color: appStyle.color_error,
+          display: error ? "flex" : "none",
+        }}
+      >
+        {error}
+      </Text>
+    </View>
   );
 };
 
