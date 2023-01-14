247227865310dfdacd440555f6b9505d7a190eda
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-14 14:20:57 +0200

    removed from RegisterScreen and works fine

--- components/Register/SexDropdown.js
+++ components/Register/SexDropdown.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 import React, { useEffect, useState } from "react";
 import { Dropdown } from "react-native-element-dropdown";
 import * as appStyle from "../AppStyleSheet";
@@ -7,49 +7,51 @@ const SexDropdown = (props) => {
   useEffect(() => {
     props.valueChanged(isMale);
   }, [isMale]);
-  const style = new StyleSheet.create({
-    iconStyle: {
-      width: 20,
-      height: 20,
-    },
-  });
-
   return (
-    <Dropdown
-      style={props.style}
-      containerStyle={{
-        borderTopWidth: 0,
-        borderBottomWidth: 2,
-        borderRightWidth: 2,
-        borderLeftWidth: 2,
-        borderColor: appStyle.color_bg,
-      }}
-      itemContainerStyle={{
-        position: "relative",
-        paddingLeft: 10,
-        height: 40,
-      }}
-      itemTextStyle={{
-        position: "absolute",
-        fontSize: 16,
-      }}
-      selectedTextStyle={{ fontSize: 16 }}
-      placeholderStyle={{ color: "#5f6b8b", fontSize: 16 }}
-      dropdownPosition="bottom"
-      placeholder="Sex"
-      iconStyle={style.iconStyle}
-      data={[
-        { label: "Male", value: true },
-        { label: "Female", value: false },
-      ]}
-      maxHeight={300}
-      labelField="label"
-      valueField="value"
-      value={isMale}
-      onChange={(item) => {
-        setIsMale(item.value);
-      }}
-    />
+    <View style={props.style.inputContainer}>
+      <Dropdown
+        style={props.style.input}
+        containerStyle={{
+          borderTopWidth: 0,
+          borderBottomWidth: 2,
+          borderRightWidth: 2,
+          borderLeftWidth: 2,
+          borderColor: appStyle.color_bg,
+        }}
+        itemContainerStyle={{
+          position: "relative",
+          paddingLeft: 10,
+          height: 40,
+        }}
+        itemTextStyle={{
+          position: "absolute",
+          fontSize: 16,
+        }}
+        selectedTextStyle={{ fontSize: 16 }}
+        placeholderStyle={{ color: "#5f6b8b", fontSize: 16 }}
+        dropdownPosition="bottom"
+        placeholder="Sex"
+        data={[
+          { label: "Male", value: true },
+          { label: "Female", value: false },
+        ]}
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        value={isMale}
+        onChange={(item) => {
+          setIsMale(item.value);
+        }}
+      />
+      <Text
+        style={{
+          color: appStyle.color_error,
+          display: props.error ? "flex" : "none",
+        }}
+      >
+        {props.error}
+      </Text>
+    </View>
   );
 };
 
