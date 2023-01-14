66d543fdb1d0fc1137a16ff5802a4ae6757a67ee
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-14 14:14:11 +0200

    Finished !web datePicker

--- components/Register/BirthdayDatePicker.js
+++ components/Register/BirthdayDatePicker.js
@@ -1,13 +1,14 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import * as appStyle from "../AppStyleSheet";
 
 const BirthdayDatePicker = (props) => {
   const [dateStyle, setDateStyle] = useState(props.style.input);
   const [show, setShow] = useState(false);
   const [date, setDate] = useState(new Date());
   const [changedOnce, setChangeOnce] = useState(false);
-
+  const [error, setError] = useState(null);
   const showDatepicker = () => {
     setShow(true);
   };
@@ -18,13 +19,18 @@ const BirthdayDatePicker = (props) => {
     setShow(false);
     setChangeOnce(true);
     const age = calculateAge(currentDate);
-    if (age < 16) {
-      console.log(age);
-      if (Platform.OS != "web")
-        alert("You need to be at least 16 to use this app");
+    if (age < 0) {
       setDateStyle(props.style.badInput);
+      setError("0_0");
+      props.valueChanged(null);
+    } else if (age < 16) {
+      setDateStyle(props.style.badInput);
+      setError("You have to be 16 and up");
+      props.valueChanged(null);
     } else {
       setDateStyle(props.style.input);
+      setError(null);
+      props.valueChanged(date);
     }
   };
 
@@ -40,24 +46,34 @@ const BirthdayDatePicker = (props) => {
   };
 
   return (
+    // Added view to remove error when compiling DatePicker
     <View>
-      <TouchableOpacity
-        className="justify-center"
-        style={dateStyle}
-        onPress={showDatepicker}
-      >
-        {!changedOnce && <Text style={{ color: "#5f6b8b" }}>Birthdate</Text>}
-        {changedOnce && (
-          <Text style={{ color: "#5f6b8b" }}>{date.toDateString()}</Text>
-        )}
-      </TouchableOpacity>
+      <View className="gap-1" style={props.style.inputContainer}>
+        <TouchableOpacity style={dateStyle} onPress={showDatepicker}>
+          {!changedOnce ? (
+            <Text style={{ color: "#5f6b8b" }}>Birthdate</Text>
+          ) : (
+            <Text style={{ color: "#5f6b8b" }}>{date.toDateString()}</Text>
+          )}
+        </TouchableOpacity>
+        <Text
+          style={{
+            color: appStyle.color_error,
+            display: error ? "flex" : "none",
+          }}
+        >
+          {error}
+        </Text>
+      </View>
       {show && (
-        <DateTimePicker
-          testID="dateTimePicker"
-          value={date}
-          mode="date"
-          onChange={onDateChange}
-        />
+        <View>
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode="date"
+            onChange={onDateChange}
+          />
+        </View>
       )}
     </View>
   );
