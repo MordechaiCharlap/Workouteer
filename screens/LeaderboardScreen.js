0855f136afafde49cce16daa02c2ea6105d53746
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-23 15:24:48 +0200

    basic implementation new screen

--- screens/LeaderboardScreen.js
+++ screens/LeaderboardScreen.js
@@ -1,10 +1,34 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { View, Text, StatusBar } from "react-native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { React, useLayoutEffect, useCallback } from "react";
+import responsiveStyle from "../components/ResponsiveStyling";
+import BottomNavbar from "../components/BottomNavbar";
+import * as appStyle from "../components/AppStyleSheet";
+import useNavbarNavigation from "../hooks/useNavbarNavigation";
+import Header from "../components/Header";
 const LeaderboardScreen = () => {
+  const { setScreen } = useNavbarNavigation();
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
+  useFocusEffect(
+    useCallback(() => {
+      setScreen("Leaderboard");
+    }, [])
+  );
   return (
-    <View>
-      <Text>LeaderboardScreen</Text>
+    <View style={responsiveStyle.safeAreaStyle}>
+      <StatusBar
+        backgroundColor={appStyle.statusBarStyle.backgroundColor}
+        barStyle={appStyle.statusBarStyle.barStyle}
+      />
+      <View className="flex-1">
+        <Header title="Leaderboard" />
+      </View>
+      <BottomNavbar currentScreen="Leaderboard" />
     </View>
   );
 };
