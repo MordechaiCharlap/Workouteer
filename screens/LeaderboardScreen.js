951ad49afc2ec09b716a37ad2fee915a66c7e411
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-31 13:36:15 +0200

    fetching leaderbard function created

--- screens/LeaderboardScreen.js
+++ screens/LeaderboardScreen.js
@@ -20,6 +20,8 @@ import * as appStyle from "../components/AppStyleSheet";
 import useNavbarNavigation from "../hooks/useNavbarNavigation";
 import Header from "../components/Header";
 import useAuth from "../hooks/useAuth";
+import { db } from "../services/firebase";
+import { getDoc } from "firebase/firestore";
 const LeaderboardScreen = () => {
   const navigation = useNavigation();
   useLayoutEffect(() => {
@@ -30,8 +32,7 @@ const LeaderboardScreen = () => {
 
   const { setScreen } = useNavbarNavigation();
   const { user } = useAuth();
-
-  const [leaderboardList, setLeaderboardList] = useState([user]);
+  const [leaderboardList, setLeaderboardList] = useState();
   const rank = user.rank == 1 ? "Bronze" : "Silver";
   useFocusEffect(
     useCallback(() => {
@@ -40,6 +41,7 @@ const LeaderboardScreen = () => {
   );
   useEffect(() => {
     const fetchLeaderboard = async () => {
+      if (user.leaderboard.id == null || user.leaderboard.points == 0) return;
       const startingTime = user.leaderboard.startingTime.toDate();
 
       const date = new Date();
@@ -52,6 +54,12 @@ const LeaderboardScreen = () => {
         startingTime.getMonth() === lastSunday.getMonth() &&
         startingTime.getDate() === lastSunday.getDate()
       ) {
+        const leaderboardList = await getDoc(
+          doc(db, `leaderboards/${user.rank}/${user.leaderboard.id}`)
+        );
+        setLeaderboardList(leaderboardList);
+      } else {
+        return;
       }
     };
     fetchLeaderboard();
@@ -62,35 +70,49 @@ const LeaderboardScreen = () => {
         backgroundColor={appStyle.statusBarStyle.backgroundColor}
         barStyle={appStyle.statusBarStyle.barStyle}
       />
-      <Header title={rank + " league"} />
-      <FlatList
-        data={leaderboardList}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <TouchableOpacity className="flex-row flex-1 items-center mt-2">
-            <Image
-              source={{
-                uri: item.img,
-              }}
-              className="h-14 w-14 bg-white rounded-full mr-4"
-            />
-            <View>
-              <Text
-                className="text-xl font-semibold tracking-wider"
-                style={{ color: appStyle.color_primary }}
-              >
-                {item.id}
-              </Text>
-              <Text
-                className="text-md opacity-60 tracking-wider"
-                style={{ color: appStyle.color_primary }}
-              >
-                {item.displayName}
-              </Text>
-            </View>
-          </TouchableOpacity>
+      <View className="flex-1">
+        <Header title={rank + " league"} />
+        {leaderboardList == null ? (
+          <Text
+            className="text-center font-semibold text-lg"
+            style={{
+              backgroundColor: appStyle.color_primary,
+              color: appStyle.color_on_primary,
+            }}
+          >
+            Get some points in order to compete
+          </Text>
+        ) : (
+          <FlatList
+            data={leaderboardList}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <TouchableOpacity className="flex-row flex-1 items-center mt-2">
+                <Image
+                  source={{
+                    uri: item.img,
+                  }}
+                  className="h-14 w-14 bg-white rounded-full mr-4"
+                />
+                <View>
+                  <Text
+                    className="text-xl font-semibold tracking-wider"
+                    style={{ color: appStyle.color_primary }}
+                  >
+                    {item.id}
+                  </Text>
+                  <Text
+                    className="text-md opacity-60 tracking-wider"
+                    style={{ color: appStyle.color_primary }}
+                  >
+                    {item.displayName}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
         )}
-      />
+      </View>
       <BottomNavbar currentScreen="Leaderboard" />
     </View>
   );
