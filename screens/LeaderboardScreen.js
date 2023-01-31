d62c5ccef39369993883f0ddc410b42d7403728e
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-01-31 13:07:20 +0200

    leaderboard list styling

--- screens/LeaderboardScreen.js
+++ screens/LeaderboardScreen.js
@@ -1,24 +1,41 @@
 import { View, Text, StatusBar } from "react-native";
 import { useNavigation, useFocusEffect } from "@react-navigation/native";
-import { React, useLayoutEffect, useCallback } from "react";
+import {
+  React,
+  useLayoutEffect,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import responsiveStyle from "../components/ResponsiveStyling";
 import BottomNavbar from "../components/BottomNavbar";
 import * as appStyle from "../components/AppStyleSheet";
 import useNavbarNavigation from "../hooks/useNavbarNavigation";
 import Header from "../components/Header";
+import useAuth from "../hooks/useAuth";
+import { FlatList } from "react-native-gesture-handler";
 const LeaderboardScreen = () => {
-  const { setScreen } = useNavbarNavigation();
   const navigation = useNavigation();
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
   });
+
+  const { setScreen } = useNavbarNavigation();
+  const { user } = useAuth();
+
+  const [leaderboardList, setLeaderboardList] = useState([]);
+  const rank = user.rank == 1 ? "Bronze" : "Silver";
   useFocusEffect(
     useCallback(() => {
       setScreen("Leaderboard");
     }, [])
   );
+  useEffect(() => {
+    const fetchLeaderboard = async () => {};
+    fetchLeaderboard();
+  }, []);
   return (
     <View style={responsiveStyle.safeAreaStyle}>
       <StatusBar
@@ -26,8 +43,36 @@ const LeaderboardScreen = () => {
         barStyle={appStyle.statusBarStyle.barStyle}
       />
       <View className="flex-1">
-        <Header title="Leaderboard" />
+        <Header title={rank + " league"} />
       </View>
+      <FlatList
+        data={leaderboardList}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity className="flex-row flex-1 items-center mt-2">
+            <Image
+              source={{
+                uri: item.img,
+              }}
+              className="h-14 w-14 bg-white rounded-full mr-4"
+            />
+            <View>
+              <Text
+                className="text-xl font-semibold tracking-wider"
+                style={{ color: appStyle.color_primary }}
+              >
+                {item.id}
+              </Text>
+              <Text
+                className="text-md opacity-60 tracking-wider"
+                style={{ color: appStyle.color_primary }}
+              >
+                {item.displayName}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
       <BottomNavbar currentScreen="Leaderboard" />
     </View>
   );
