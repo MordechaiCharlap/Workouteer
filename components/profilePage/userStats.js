cb6219749ccdc679a8591e74e30136bd7d84bed0
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-02-01 14:38:01 +0200

    userStats component created

---
+++ components/profilePage/userStats.js
@@ -0,0 +1,20 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faFire,
+  faLightning,
+  faDumbbell,
+} from "@fortawesome/free-solid-svg-icons";
+import * as appStyle from "../AppStyleSheet";
+const userStats = (props) => {
+  const iconSize = 30;
+  const iconColor = appStyle.color_primary;
+  return (
+    <View className="flex-row rounded-2xl">
+      <FontAwesomeIcon icon={faFire} size={iconSize} color={iconColor} />
+    </View>
+  );
+};
+
+export default userStats;
