fcf71c805dc71a4eed6e264cf0069baad5cd0ad0
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-02-01 16:12:04 +0200

    styling

--- components/profilePage/userStats.js
+++ components/profilePage/userStats.js
@@ -1,18 +1,25 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import {
-  faFire,
-  faLightning,
-  faDumbbell,
-} from "@fortawesome/free-solid-svg-icons";
+import { faFire, faDumbbell, faBolt } from "@fortawesome/free-solid-svg-icons";
 import * as appStyle from "../AppStyleSheet";
 const UserStats = (props) => {
   const iconSize = 30;
   const iconColor = appStyle.color_primary;
   return (
-    <View className="flex-row rounded-2xl">
-      <FontAwesomeIcon icon={faFire} size={iconSize} color={iconColor} />
+    <View
+      className="flex-row items-center rounded-2xl"
+      style={{ borderWidth: 1, borderColor: appStyle.color_bg_variant }}
+    >
+      <View>
+        <FontAwesomeIcon icon={faFire} size={iconSize} color={iconColor} />
+      </View>
+      <View>
+        <FontAwesomeIcon icon={faDumbbell} size={iconSize} color={iconColor} />
+      </View>
+      <View>
+        <FontAwesomeIcon icon={faBolt} size={iconSize} color={iconColor} />
+      </View>
     </View>
   );
 };
