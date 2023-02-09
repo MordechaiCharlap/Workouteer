ba39790b377b266fdc8dc19b3fcc7288ce326526
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-02-09 19:19:07 +0200

    styling+streak added

--- components/profileScreen/UserStats.js
+++ components/profileScreen/UserStats.js
@@ -1,30 +1,36 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faFire, faDumbbell, faBolt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFire,
+  faRankingStar,
+  faBolt,
+} from "@fortawesome/free-solid-svg-icons";
 import * as appStyle from "../AppStyleSheet";
 import useAuth from "../../hooks/useAuth";
 import WorkoutsStats from "./UserStats/WorkoutsStats";
-const UserStats = () => {
+const UserStats = (props) => {
   const { user } = useAuth();
   const iconSize = 30;
   const iconColor = appStyle.color_primary;
   const renderStat = (icon, data) => {
     return (
-      <View className="flex-row">
+      <View
+        className="flex-row items-center py-2 w-1/3 rounded justify-center gap-x-1"
+        style={{ borderWidth: 2, borderColor: appStyle.color_primary }}
+      >
         <FontAwesomeIcon icon={icon} size={iconSize} color={iconColor} />
-        <Text>{data}</Text>
+        <Text className="text-lg font-semibold">{data}</Text>
       </View>
     );
   };
+  const renderStreak = () => {};
   return (
     <View>
-      <View
-        className="flex-row items-center rounded-2xl"
-        style={{ borderWidth: 1, borderColor: appStyle.color_bg_variant }}
-      >
+      <View className="flex-row items-center justify-evenly">
         {renderStat(faBolt, user.totalPoints)}
-        {renderStat(faFire, user.rank)}
+        {renderStat(faRankingStar, user.rank)}
+        {renderStreak()}
       </View>
       <WorkoutsStats workouts={user.workouts} />
     </View>
