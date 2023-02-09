3b59cbf845c5b242d5e1e5ddea5770fbcec3d791
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-02-09 19:23:48 +0200

    styling

--- components/profileScreen/UserStats.js
+++ components/profileScreen/UserStats.js
@@ -9,16 +9,13 @@ import {
 import * as appStyle from "../AppStyleSheet";
 import useAuth from "../../hooks/useAuth";
 import WorkoutsStats from "./UserStats/WorkoutsStats";
-const UserStats = (props) => {
+const UserStats = () => {
   const { user } = useAuth();
   const iconSize = 30;
   const iconColor = appStyle.color_primary;
   const renderStat = (icon, data) => {
     return (
-      <View
-        className="flex-row items-center py-2 w-1/3 rounded justify-center gap-x-1"
-        style={{ borderWidth: 2, borderColor: appStyle.color_primary }}
-      >
+      <View className="flex-row items-center py-2 w-1/3 rounded justify-center gap-x-1">
         <FontAwesomeIcon icon={icon} size={iconSize} color={iconColor} />
         <Text className="text-lg font-semibold">{data}</Text>
       </View>
@@ -26,10 +23,14 @@ const UserStats = (props) => {
   };
   const renderStreak = () => {};
   return (
-    <View>
-      <View className="flex-row items-center justify-evenly">
+    <View className="gap-y-2">
+      <View
+        className="flex-row items-center justify-evenly"
+        style={{ borderWidth: 2, borderColor: appStyle.color_primary }}
+      >
         {renderStat(faBolt, user.totalPoints)}
         {renderStat(faRankingStar, user.rank)}
+        {renderStat(faFire, user.rank)}
         {renderStreak()}
       </View>
       <WorkoutsStats workouts={user.workouts} />
