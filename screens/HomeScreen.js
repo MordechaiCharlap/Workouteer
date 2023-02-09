f3d452405c7887c3a19b75d719ed33e6c7b823e3
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-02-09 14:29:52 +0200

    ConfirmCurrentWorkoutButton imported

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -23,8 +23,7 @@ import useNavbarNavigation from "../hooks/useNavbarNavigation";
 import languageService from "../services/languageService";
 import useAuth from "../hooks/useAuth";
 import * as firebase from "../services/firebase";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { workoutTypes } from "../components/WorkoutType";
+import ConfirmCurrentWorkoutButton from "../components/ConfirmCurrentWorkoutButton";
 const HomeScreen = () => {
   const navigation = useNavigation();
   const { setScreen } = useNavbarNavigation();
@@ -49,16 +48,20 @@ const HomeScreen = () => {
     console.log("checking if theres current workout");
     for (var [key, value] of Object.entries(user.workouts)) {
       if (
-        new Date(value[0].toDate().getTime() + value[1] * 60000) > now &&
-        value[0].toDate() > now
+        !(
+          new Date(value[0].toDate().getTime() + value[1] * 60000) > now &&
+          value[0].toDate() < now &&
+          !value[2]
+        )
         //should be <now but just for testing
       ) {
         console.log("found current workout");
         const workout = await firebase.getWorkout(key);
-        return workout;
+
+        return { ...workout, id: key };
       }
     }
-    console.log("havent current workout");
+    console.log("havent found current workout");
   };
   useFocusEffect(
     useCallback(() => {
@@ -68,11 +71,8 @@ const HomeScreen = () => {
         const currentWorkoutReturned = await checkIfCurrentWorkout(now);
         if (!currentWorkoutReturned) {
           const lastQuarter = now.getMinutes() % 15;
-          console.log(lastQuarter);
           //16 so there wont be bug checking 16:15:89 workout at 16:15:75. prefer to check at 16:16:XX to make sure
           var nextCheck = new Date(now.getTime() + (16 - lastQuarter) * 60000);
-          console.log(now);
-          console.log(nextCheck);
           const interval = setInterval(async () => {
             const now = new Date();
             if (now > nextCheck) {
@@ -152,20 +152,11 @@ const HomeScreen = () => {
         </View>
 
         {currentWorkout != null && (
-          <View className="absolute bottom-0 right-0 left-0 mx-0 items-center mb-3">
-            <TouchableOpacity
-              className="rounded-full p-2 flex-row"
-              style={{
-                backgroundColor: appStyle.color_bg_variant,
-              }}
-            >
-              <FontAwesomeIcon
-                icon={workoutTypes[currentWorkout.type].icon}
-                size={30}
-                color={"black"}
-              />
-              <Text>Confirm workout to get points</Text>
-            </TouchableOpacity>
+          <View className="absolute bottom-1 right-0 left-0 mx-0 items-center">
+            <ConfirmCurrentWorkoutButton
+              currentWorkout={currentWorkout}
+              userId={user.id}
+            />
           </View>
         )}
       </View>
