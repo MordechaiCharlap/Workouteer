a05ddfb715e445cc65ab3cfa8deaa814ac480037
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-02-09 14:29:23 +0200

    styling+functionality of confirm button

--- components/ConfirmCurrentWorkoutButton.js
+++ components/ConfirmCurrentWorkoutButton.js
@@ -1,24 +1,58 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import { workoutTypes } from "./WorkoutType";
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import * as appStyle from "../components/AppStyleSheet";
-
+import { db } from "../services/firebase";
+import { doc, updateDoc } from "firebase/firestore";
 const ConfirmCurrentWorkoutButton = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [confirmed, setConfirmed] = useState(false);
+  const confirmWorkout = async () => {
+    setLoading(true);
+    const newWorkoutArray = [
+      props.currentWorkout.startingTime.toDate(),
+      props.currentWorkout.minutes,
+      true,
+    ];
+    await updateDoc(doc(db, `users/${props.userId}`), {
+      [`workouts.${props.currentWorkout.id}`]: { ...newWorkoutArray },
+    });
+    setConfirmed(true);
+    setLoading(false);
+  };
   return (
-    <View>
+    <View className="w-2/3">
       <TouchableOpacity
-        className="rounded-full p-2 flex-row"
+        onPress={!loading && !confirmed ? async () => confirmWorkout() : {}}
+        className="rounded-full p-2 flex-row items-center w-full justify-evenly"
         style={{
+          borderWidth: 2,
+          borderColor: appStyle.color_on_primary,
           backgroundColor: appStyle.color_bg_variant,
         }}
       >
-        <FontAwesomeIcon
-          icon={workoutTypes[props.currentWorkout.type].icon}
-          size={30}
-          color={"black"}
-        />
-        <Text>Confirm workout to get points</Text>
+        <View
+          className="rounded-full p-2"
+          style={{
+            borderWidth: 0.5,
+            borderColor: appStyle.color_on_primary,
+            backgroundColor: appStyle.color_primary,
+          }}
+        >
+          <FontAwesomeIcon
+            icon={workoutTypes[props.currentWorkout.type].icon}
+            size={30}
+            color={appStyle.color_on_primary}
+          />
+        </View>
+        <Text style={{ color: appStyle.color_on_primary }}>
+          {loading
+            ? "Loading"
+            : !loading && !confirmed
+            ? "Confirm workout to get points"
+            : "Workout confirmed"}
+        </Text>
       </TouchableOpacity>
     </View>
   );
