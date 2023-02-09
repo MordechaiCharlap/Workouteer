8addb93c4317ec804fea7fd98d3173b17867ec74
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-02-09 16:54:31 +0200

    using useAuth instead of props

--- components/profileScreen/UserStats.js
+++ components/profileScreen/UserStats.js
@@ -3,7 +3,10 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faFire, faDumbbell, faBolt } from "@fortawesome/free-solid-svg-icons";
 import * as appStyle from "../AppStyleSheet";
-const UserStats = (props) => {
+import useAuth from "../../hooks/useAuth";
+import WorkoutsStats from "./UserStats/WorkoutsStats";
+const UserStats = () => {
+  const { user } = useAuth();
   const iconSize = 30;
   const iconColor = appStyle.color_primary;
   const renderStat = (icon, data) => {
@@ -14,15 +17,16 @@ const UserStats = (props) => {
       </View>
     );
   };
-  const renderWorkoutsStat = (icon, data) => {};
   return (
-    <View
-      className="flex-row items-center rounded-2xl"
-      style={{ borderWidth: 1, borderColor: appStyle.color_bg_variant }}
-    >
-      {renderStat(faBolt, props.totalPoints)}
-      {renderStat(faFire, props.rank)}
-      {renderWorkoutsStat(faDumbbell, props.workouts)}
+    <View>
+      <View
+        className="flex-row items-center rounded-2xl"
+        style={{ borderWidth: 1, borderColor: appStyle.color_bg_variant }}
+      >
+        {renderStat(faBolt, user.totalPoints)}
+        {renderStat(faFire, user.rank)}
+      </View>
+      <WorkoutsStats workouts={user.workouts} />
     </View>
   );
 };
