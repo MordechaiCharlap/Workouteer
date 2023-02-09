ff3728091fcd08427275df224959cc50c6812f45
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-02-09 20:18:51 +0200

    isSameDay function created

--- services/timeFunctions.js
+++ services/timeFunctions.js
@@ -15,3 +15,10 @@ export const timeString = (date) => {
   time = hh + ":" + mm;
   return day + ", " + time;
 };
+export const isSameDay = (d1, d2) => {
+  return (
+    d1.getFullYear() === d2.getFullYear() &&
+    d1.getMonth() === d2.getMonth() &&
+    d1.getDate() === d2.getDate()
+  );
+};
