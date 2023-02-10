5430e7568105686f922d3468e5c3483b63f429e6
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-02-10 13:28:48 +0200

    seperate compionent craeted

---
+++ components/profileScreen/NameAndAge.js
@@ -0,0 +1,20 @@
+import { View, Text } from "react-native";
+import React from "react";
+import * as appStyle from "../AppStyleSheet";
+const NameAndAge = (props) => {
+  return (
+    <View className="flex-row">
+      <Text
+        className="px-4 py-2 rounded-xl text-3xl"
+        style={{
+          color: appStyle.color_on_primary,
+          backgroundColor: appStyle.color_primary,
+        }}
+      >
+        {props.name} {props.age}
+      </Text>
+    </View>
+  );
+};
+
+export default NameAndAge;
