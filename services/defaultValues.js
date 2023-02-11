a800a39182a02aff82909311ef7fa716496c8a3d
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-02-11 09:46:43 +0200

    leagues default values added

--- services/defaultValues.js
+++ services/defaultValues.js
@@ -1,2 +1,3 @@
 export const defaultProfilePic =
   "https://firebasestorage.googleapis.com/v0/b/workouteer-54450.appspot.com/o/profile-pics%2Fdefaults%2Fdefault-profile-image.jpg?alt=media&token=e6cf13be-9b7b-4d6c-9769-9e18813dafd2";
+export const leagues = ["", "Bronze", "Silver", "Gold"];
