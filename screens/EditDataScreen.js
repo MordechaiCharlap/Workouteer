bebd848083a1f1c3375f884430c84e015a0f43fd
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-02-11 15:55:42 +0200

    keyboard hiding text bug fixed

--- screens/EditDataScreen.js
+++ screens/EditDataScreen.js
@@ -5,7 +5,10 @@ import {
   View,
   TouchableOpacity,
   StyleSheet,
+  Platform,
   TextInput,
+  KeyboardAvoidingView,
+  ScrollView,
 } from "react-native";
 
 import React, { useState, useEffect, useCallback } from "react";
@@ -153,272 +156,279 @@ const EditProfileData = (props) => {
   };
   return (
     <View>
-      <View className="mb-5 self-center">
-        {imageLoading ? (
-          <View
-            className="h-32 w-32 rounded-full mb-2 items-center justify-center"
-            style={{
-              borderWidth: 1,
-              borderColor: appStyle.color_primary,
-              backgroundColor: appStyle.color_bg,
-            }}
-          >
-            <Text
-              className="text-xl font-bold"
-              style={{ color: appStyle.color_primary }}
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "android" ? null : "padding"}
+        enabled={true}
+      >
+        <ScrollView>
+          <View className="mb-5 self-center">
+            {imageLoading ? (
+              <View
+                className="h-32 w-32 rounded-full mb-2 items-center justify-center"
+                style={{
+                  borderWidth: 1,
+                  borderColor: appStyle.color_primary,
+                  backgroundColor: appStyle.color_bg,
+                }}
+              >
+                <Text
+                  className="text-xl font-bold"
+                  style={{ color: appStyle.color_primary }}
+                >
+                  {languageService[user.language].loading}
+                </Text>
+              </View>
+            ) : (
+              <Image
+                source={{
+                  uri: image,
+                }}
+                className="h-32 w-32 bg-white rounded-full mb-2"
+                style={{ borderWidth: 1, borderColor: appStyle.color_primary }}
+              />
+            )}
+            <TouchableOpacity
+              onPress={onImageLibraryPress}
+              className="absolute right-0 bottom-0 rounded-full p-2"
+              style={{
+                backgroundColor: appStyle.color_primary,
+                borderColor: appStyle.color_bg,
+                borderWidth: 3,
+              }}
             >
-              {languageService[user.language].loading}
-            </Text>
+              <FontAwesomeIcon
+                icon={faPen}
+                size={20}
+                color={appStyle.color_on_primary}
+              />
+            </TouchableOpacity>
           </View>
-        ) : (
-          <Image
-            source={{
-              uri: image,
-            }}
-            className="h-32 w-32 bg-white rounded-full mb-2"
-            style={{ borderWidth: 1, borderColor: appStyle.color_primary }}
-          />
-        )}
-        <TouchableOpacity
-          onPress={onImageLibraryPress}
-          className="absolute right-0 bottom-0 rounded-full p-2"
-          style={{
-            backgroundColor: appStyle.color_primary,
-            borderColor: appStyle.color_bg,
-            borderWidth: 3,
-          }}
-        >
-          <FontAwesomeIcon
-            icon={faPen}
-            size={20}
-            color={appStyle.color_on_primary}
-          />
-        </TouchableOpacity>
-      </View>
-      <View
-        className={`items-center mb-5 gap-x-3 ${
-          user.language == "hebrew" ? "flex-row-reverse" : "flex-row"
-        }`}
-      >
-        <Text className="text-lg" style={{ color: appStyle.color_primary }}>
-          {languageService[user.language].displayName}:
-        </Text>
-        <TextInput
-          className="rounded text-lg flex-1 px-2"
-          style={style.input}
-          placeholder={user.displayName}
-          placeholderTextColor={"#5f6b8b"}
-          maxLength={20}
-          onChangeText={(text) => setDisplayName(text)}
-          value={displayName}
-        />
-      </View>
-      <View className="mb-5">
-        <Text className="text-lg" style={{ color: appStyle.color_primary }}>
-          {languageService[user.language].description}:
-        </Text>
-        <TextInput
-          style={{
-            textAlignVertical: "top",
-            color: appStyle.color_on_primary,
-            backgroundColor: appStyle.color_bg_variant,
-            borderRadius: 8,
-            padding: 8,
-            borderWidth: 1,
-            borderColor: appStyle.color_primary,
-          }}
-          multiline
-          spellCheck={false}
-          autoCorrect={false}
-          placeholder="Optional text"
-          placeholderTextColor={appStyle.color_primary}
-          numberOfLines={12}
-          maxLength={350}
-          onChangeText={(text) => setDescription(text)}
-          value={description}
-        />
-      </View>
-      {SaveButton()}
+          <View
+            className={`items-center mb-5 gap-x-3 ${
+              user.language == "hebrew" ? "flex-row-reverse" : "flex-row"
+            }`}
+          >
+            <Text className="text-lg" style={{ color: appStyle.color_primary }}>
+              {languageService[user.language].displayName}:
+            </Text>
+            <TextInput
+              className="rounded text-lg flex-1 px-2"
+              style={style.input}
+              placeholder={user.displayName}
+              placeholderTextColor={"#5f6b8b"}
+              maxLength={20}
+              onChangeText={(text) => setDisplayName(text)}
+              value={displayName}
+            />
+          </View>
+          <View className="mb-5">
+            <Text className="text-lg" style={{ color: appStyle.color_primary }}>
+              {languageService[user.language].description}:
+            </Text>
+            <TextInput
+              style={{
+                textAlignVertical: "top",
+                color: appStyle.color_on_primary,
+                backgroundColor: appStyle.color_bg_variant,
+                borderRadius: 8,
+                padding: 8,
+                borderWidth: 1,
+                borderColor: appStyle.color_primary,
+              }}
+              multiline
+              spellCheck={false}
+              autoCorrect={false}
+              placeholder="Optional text"
+              placeholderTextColor={appStyle.color_primary}
+              numberOfLines={12}
+              maxLength={350}
+              onChangeText={(text) => setDescription(text)}
+              value={description}
+            />
+          </View>
+          {SaveButton()}
+        </ScrollView>
+      </KeyboardAvoidingView>
     </View>
   );
 };
-const EditWorkoutPreferences = (props) => {
-  const [minAge, setMinAge] = useState(props.user.acceptMinAge);
-  const [maxAge, setMaxAge] = useState(props.user.acceptMaxAge);
-  const [acceptMale, setAcceptMale] = useState(props.user.acceptMale);
-  const [acceptFemale, setAcceptFemale] = useState(props.user.acceptFemale);
+// const EditWorkoutPreferences = (props) => {
+//   const [minAge, setMinAge] = useState(props.user.acceptMinAge);
+//   const [maxAge, setMaxAge] = useState(props.user.acceptMaxAge);
+//   const [acceptMale, setAcceptMale] = useState(props.user.acceptMale);
+//   const [acceptFemale, setAcceptFemale] = useState(props.user.acceptFemale);
 
-  const [invalidInput, setInvalidInput] = useState(false);
-  const [updated, setUpdated] = useState(false);
-  const [changesMade, setChangesMade] = useState(false);
-  const [isLoading, setLoading] = useState(false);
-  const onChangedMinAge = (text) => {
-    setMinAge(text.replace(/[^0-9]/g, ""));
-  };
-  const onChangedMaxAge = (text) => {
-    setMaxAge(text.replace(/[^0-9]/g, ""));
-  };
-  useEffect(() => {
-    if (
-      minAge == "" ||
-      maxAge == "" ||
-      parseInt(minAge) > parseInt(maxAge) ||
-      (acceptMale == false && acceptFemale == false)
-    ) {
-      setInvalidInput(true);
-    } else setInvalidInput(false);
-    if (
-      minAge != props.user.acceptMinAge ||
-      maxAge != props.user.acceptMaxAge ||
-      acceptMale != props.user.acceptMale ||
-      acceptFemale != props.user.acceptFemale
-    ) {
-      setChangesMade(true);
-    } else setChangesMade(false);
-  }, [minAge, maxAge, acceptFemale, acceptMale]);
-  const savePreferencesChanges = async () => {
-    setLoading(true);
-    await firebase.savePreferencesChanges(
-      props.user.id,
-      Math.max(parseInt(minAge), 16).toString(),
-      Math.min(parseInt(maxAge), 100).toString(),
-      acceptMale,
-      acceptFemale
-    );
-    // props.setUser(await firebase.updateContext(props.user.id));
+//   const [invalidInput, setInvalidInput] = useState(false);
+//   const [updated, setUpdated] = useState(false);
+//   const [changesMade, setChangesMade] = useState(false);
+//   const [isLoading, setLoading] = useState(false);
+//   const onChangedMinAge = (text) => {
+//     setMinAge(text.replace(/[^0-9]/g, ""));
+//   };
+//   const onChangedMaxAge = (text) => {
+//     setMaxAge(text.replace(/[^0-9]/g, ""));
+//   };
+//   useEffect(() => {
+//     if (
+//       minAge == "" ||
+//       maxAge == "" ||
+//       parseInt(minAge) > parseInt(maxAge) ||
+//       (acceptMale == false && acceptFemale == false)
+//     ) {
+//       setInvalidInput(true);
+//     } else setInvalidInput(false);
+//     if (
+//       minAge != props.user.acceptMinAge ||
+//       maxAge != props.user.acceptMaxAge ||
+//       acceptMale != props.user.acceptMale ||
+//       acceptFemale != props.user.acceptFemale
+//     ) {
+//       setChangesMade(true);
+//     } else setChangesMade(false);
+//   }, [minAge, maxAge, acceptFemale, acceptMale]);
+//   const savePreferencesChanges = async () => {
+//     setLoading(true);
+//     await firebase.savePreferencesChanges(
+//       props.user.id,
+//       Math.max(parseInt(minAge), 16).toString(),
+//       Math.min(parseInt(maxAge), 100).toString(),
+//       acceptMale,
+//       acceptFemale
+//     );
+//     // props.setUser(await firebase.updateContext(props.user.id));
 
-    setUpdated(true);
-    setTimeout(() => {
-      setLoading(false);
-      setChangesMade(false);
-      setUpdated(false);
-      props.navigation.navigate("MyUser");
-    }, 1000);
-  };
-  const saveButtonClicked = () => {
-    if (!invalidInput) {
-      if (changesMade == true) {
-        if (isLoading == false) savePreferencesChanges();
-      }
-    }
-  };
-  const SaveButton = () => {
-    const getBackgroundColor = () => {
-      if (invalidInput || !changesMade) return appStyle.color_bg_variant;
-      return updated == false
-        ? appStyle.color_primary
-        : appStyle.color_bg_variant;
-    };
-    return (
-      <TouchableOpacity
-        onPress={saveButtonClicked}
-        className="self-center py-1 px-5 w-9/12 rounded"
-        style={{
-          backgroundColor: getBackgroundColor(),
-        }}
-      >
-        <Text
-          className="text-2xl text-center"
-          style={{ color: appStyle.color_on_primary }}
-        >
-          {invalidInput == true && "Invalid input"}
-          {invalidInput == false && updated == true && "Updated successfuly!"}
-          {invalidInput == false &&
-            updated == false &&
-            changesMade == false &&
-            "No changes made"}
-          {invalidInput == false &&
-            updated == false &&
-            changesMade == true &&
-            isLoading == false &&
-            "Save Changes"}
-          {invalidInput == false &&
-            updated == false &&
-            changesMade == true &&
-            isLoading == true &&
-            "Loading"}
-        </Text>
-      </TouchableOpacity>
-    );
-  };
-  return (
-    <View>
-      <View className="flex-row mb-5">
-        <Text
-          className="text-xl font-semibold mr-2"
-          style={{ color: appStyle.color_primary }}
-        >
-          Partner's age:
-        </Text>
-        <TextInput
-          keyboardType="numeric"
-          className="rounded text-lg px-2 text-center w-16"
-          style={style.input}
-          maxLength={3}
-          onChangeText={(text) => onChangedMinAge(text)}
-          onBlur={() => {
-            if (minAge != "")
-              setMinAge(
-                Math.min(Math.max(parseInt(minAge), 16), maxAge).toString()
-              );
-          }}
-          value={minAge}
-        ></TextInput>
-        <Text
-          className="text-xl font-semibold mx-3"
-          style={{ color: appStyle.color_primary }}
-        >
-          -
-        </Text>
-        <TextInput
-          keyboardType="numeric"
-          className="rounded text-lg px-2 text-center w-16"
-          style={style.input}
-          maxLength={3}
-          onChangeText={(text) => onChangedMaxAge(text)}
-          onBlur={() => {
-            if (maxAge != "")
-              setMaxAge(
-                Math.max(Math.min(parseInt(maxAge), 100), minAge).toString()
-              );
-          }}
-          value={maxAge}
-        ></TextInput>
-      </View>
-      <View className="flex-row items-center mb-5">
-        <Text
-          className="text-xl font-semibold mr-2"
-          style={{ color: appStyle.color_primary }}
-        >
-          Partner's sex:
-        </Text>
-        <View className="flex-row items-center mr-2">
-          <CheckBox
-            onValueChange={(value) => setAcceptMale(value)}
-            backgroundColor={appStyle.color_primary}
-            valueColor={appStyle.color_on_primary}
-            value={acceptMale}
-          />
-          <Text className="ml-1" style={{ color: appStyle.color_primary }}>
-            Male
-          </Text>
-        </View>
-        <View className="flex-row items-center">
-          <CheckBox
-            onValueChange={(value) => setAcceptFemale(value)}
-            backgroundColor={appStyle.color_primary}
-            valueColor={appStyle.color_on_primary}
-            value={acceptFemale}
-          />
-          <Text className="ml-1" style={{ color: appStyle.color_primary }}>
-            Female
-          </Text>
-        </View>
-      </View>
-      {SaveButton()}
-    </View>
-  );
-};
+//     setUpdated(true);
+//     setTimeout(() => {
+//       setLoading(false);
+//       setChangesMade(false);
+//       setUpdated(false);
+//       props.navigation.navigate("MyUser");
+//     }, 1000);
+//   };
+//   const saveButtonClicked = () => {
+//     if (!invalidInput) {
+//       if (changesMade == true) {
+//         if (isLoading == false) savePreferencesChanges();
+//       }
+//     }
+//   };
+//   const SaveButton = () => {
+//     const getBackgroundColor = () => {
+//       if (invalidInput || !changesMade) return appStyle.color_bg_variant;
+//       return updated == false
+//         ? appStyle.color_primary
+//         : appStyle.color_bg_variant;
+//     };
+//     return (
+//       <TouchableOpacity
+//         onPress={saveButtonClicked}
+//         className="self-center py-1 px-5 w-9/12 rounded"
+//         style={{
+//           backgroundColor: getBackgroundColor(),
+//         }}
+//       >
+//         <Text
+//           className="text-2xl text-center"
+//           style={{ color: appStyle.color_on_primary }}
+//         >
+//           {invalidInput == true && "Invalid input"}
+//           {invalidInput == false && updated == true && "Updated successfuly!"}
+//           {invalidInput == false &&
+//             updated == false &&
+//             changesMade == false &&
+//             "No changes made"}
+//           {invalidInput == false &&
+//             updated == false &&
+//             changesMade == true &&
+//             isLoading == false &&
+//             "Save Changes"}
+//           {invalidInput == false &&
+//             updated == false &&
+//             changesMade == true &&
+//             isLoading == true &&
+//             "Loading"}
+//         </Text>
+//       </TouchableOpacity>
+//     );
+//   };
+//   return (
+//     <View>
+//       <View className="flex-row mb-5">
+//         <Text
+//           className="text-xl font-semibold mr-2"
+//           style={{ color: appStyle.color_primary }}
+//         >
+//           Partner's age:
+//         </Text>
+//         <TextInput
+//           keyboardType="numeric"
+//           className="rounded text-lg px-2 text-center w-16"
+//           style={style.input}
+//           maxLength={3}
+//           onChangeText={(text) => onChangedMinAge(text)}
+//           onBlur={() => {
+//             if (minAge != "")
+//               setMinAge(
+//                 Math.min(Math.max(parseInt(minAge), 16), maxAge).toString()
+//               );
+//           }}
+//           value={minAge}
+//         ></TextInput>
+//         <Text
+//           className="text-xl font-semibold mx-3"
+//           style={{ color: appStyle.color_primary }}
+//         >
+//           -
+//         </Text>
+//         <TextInput
+//           keyboardType="numeric"
+//           className="rounded text-lg px-2 text-center w-16"
+//           style={style.input}
+//           maxLength={3}
+//           onChangeText={(text) => onChangedMaxAge(text)}
+//           onBlur={() => {
+//             if (maxAge != "")
+//               setMaxAge(
+//                 Math.max(Math.min(parseInt(maxAge), 100), minAge).toString()
+//               );
+//           }}
+//           value={maxAge}
+//         ></TextInput>
+//       </View>
+//       <View className="flex-row items-center mb-5">
+//         <Text
+//           className="text-xl font-semibold mr-2"
+//           style={{ color: appStyle.color_primary }}
+//         >
+//           Partner's sex:
+//         </Text>
+//         <View className="flex-row items-center mr-2">
+//           <CheckBox
+//             onValueChange={(value) => setAcceptMale(value)}
+//             backgroundColor={appStyle.color_primary}
+//             valueColor={appStyle.color_on_primary}
+//             value={acceptMale}
+//           />
+//           <Text className="ml-1" style={{ color: appStyle.color_primary }}>
+//             Male
+//           </Text>
+//         </View>
+//         <View className="flex-row items-center">
+//           <CheckBox
+//             onValueChange={(value) => setAcceptFemale(value)}
+//             backgroundColor={appStyle.color_primary}
+//             valueColor={appStyle.color_on_primary}
+//             value={acceptFemale}
+//           />
+//           <Text className="ml-1" style={{ color: appStyle.color_primary }}>
+//             Female
+//           </Text>
+//         </View>
+//       </View>
+//       {SaveButton()}
+//     </View>
+//   );
+// };
 const style = StyleSheet.create({
   input: {
     backgroundColor: appStyle.color_bg_variant,
