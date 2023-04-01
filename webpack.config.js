9503c69aba4fe2b3effeb36ed66f21eed1ea8900
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-04-01 13:21:47 +0300

    gpt advice

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,35 @@
 const createExpoWebpackConfigAsync = require("@expo/webpack-config");
+const path = require("path");
+const webpack = require("webpack");
+
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
+
+  config.module.rules.push({
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: ["babel-loader"],
+  });
+
+  config.resolve.extensions.push(".jsx");
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    "react-native": "react-native-web",
+  };
+
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      __DEV__: true,
+    })
+  );
+
+  config.devServer = {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    hot: true,
+  };
+
   // Customize the config before returning it.
   return config;
 };
