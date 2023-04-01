6ac7b09707a685bad703cf6d9a461f01b3ab803a
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-04-01 15:23:43 +0300

    --web doesnt crash rn

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,50 @@
 const createExpoWebpackConfigAsync = require("@expo/webpack-config");
 const path = require("path");
 const webpack = require("webpack");
-
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-
-  config.module.rules.push({
-    test: /\.(js|jsx)$/,
-    exclude: /node_modules/,
-    use: ["babel-loader"],
-  });
-
+  delete config.output.uniqueName;
+  delete config.output.assetModuleFilename;
+  config.module.rules[1].oneOf = [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ["babel-loader"],
+      type: "javascript/dynamic",
+    },
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ["babel-loader"],
+      type: "json",
+    },
+    {
+      test: /\.m?js/,
+      type: "javascript/esm",
+    },
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ["babel-loader"],
+      type: "javascript/auto",
+    },
+  ];
   config.resolve.extensions.push(".jsx");
   config.resolve.alias = {
     ...config.resolve.alias,
     "react-native": "react-native-web",
   };
-
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      __DEV__: true,
-    })
-  );
-
+  config.target = "web";
   config.devServer = {
     static: {
       directory: path.join(__dirname, "dist"),
     },
     hot: true,
   };
-
-  // Customize the config before returning it.
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      __DEV__: true,
+    })
+  );
   return config;
 };
