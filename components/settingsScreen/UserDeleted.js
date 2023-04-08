d92df38bfc5d115eda78a0034acbf7d4abaa6458
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-04-08 18:03:13 +0300

    user deleted styling

--- components/settingsScreen/UserDeleted.js
+++ components/settingsScreen/UserDeleted.js
@@ -2,19 +2,24 @@ import { View, Text } from "react-native";
 import * as appStyle from "../AppStyleSheet";
 export const UserDeleted = (props) => {
   return (
-    <View
-      className="p-2 rounded-lg"
-      style={{ backgroundColor: appStyle.color_on_primary }}
-    >
-      <Text
-        className="text-2xl font-semibold"
-        style={{ color: appStyle.color_primary }}
+    <View className="justify-center flex-1">
+      <View
+        className="rounded-lg m-4 p-4 gap-y-2"
+        style={{ backgroundColor: appStyle.color_primary }}
       >
-        {props.id} deleted succesfuly.
-      </Text>
-      <Text className="text-lg" style={{ color: appStyle.color_primary }}>
-        Feel free to comeback whenever
-      </Text>
+        <Text
+          className="text-4xl font-semibold text-center"
+          style={{ color: appStyle.color_on_primary }}
+        >
+          {props.id} deleted succesfuly.
+        </Text>
+        <Text
+          className="text-lg text-center"
+          style={{ color: appStyle.color_on_primary }}
+        >
+          Feel free to comeback whenever
+        </Text>
+      </View>
     </View>
   );
 };
