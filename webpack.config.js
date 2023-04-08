4848df18b4ffc1eb66570e3e26a8b5510212ee28
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-04-08 19:27:13 +0300

    default webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,5 @@
 const createExpoWebpackConfigAsync = require("@expo/webpack-config");
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-  config.resolve.extensions = [
-    ".tsx",
-    ".ts",
-    ".js",
-    ".ios.js",
-    ".android.js",
-    ".web.js",
-  ];
   return config;
 };
