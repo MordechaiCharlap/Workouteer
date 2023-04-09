66076fac78d0a781cb395c521120943fb4b585be
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-04-09 15:21:01 +0300

    web component bug fixed

--- components/BottomNavbar.js
+++ components/BottomNavbar.js
@@ -6,26 +6,29 @@ import useAlerts from "../hooks/useAlerts";
 const BottomNavbar = (props) => {
   const { chatsAlerts, friendRequestsAlerts } = useAlerts();
   return (
-    <View
-      className={`flex-row flex-grow-0 shrink-0 justify-around`}
-      style={{
-        backgroundColor: appStyle.color_primary,
-        height: props.height,
-        borderTopWidth: 1,
-        borderTopColor: appStyle.color_bg_variant,
-      }}
-    >
-      <NavbarButton
-        screen="MyUser"
-        alert={Object.keys(friendRequestsAlerts).length > 0}
-      />
-      <NavbarButton screen="Leaderboard" />
-      <NavbarButton screen="Home" />
-      <NavbarButton
-        screen="Chats"
-        alert={Object.keys(chatsAlerts).length > 0}
-      />
-      <NavbarButton screen="Explore" />
+    <View className="items-center" style={{ backgroundColor: "#f2f2f2" }}>
+      <View
+        className={`flex-row flex-grow-0 shrink-0 justify-around`}
+        style={{
+          width: props.width,
+          backgroundColor: appStyle.color_primary,
+          height: props.height,
+          borderTopWidth: 1,
+          borderTopColor: appStyle.color_bg_variant,
+        }}
+      >
+        <NavbarButton
+          screen="MyUser"
+          alert={Object.keys(friendRequestsAlerts).length > 0}
+        />
+        <NavbarButton screen="Leaderboard" />
+        <NavbarButton screen="Home" />
+        <NavbarButton
+          screen="Chats"
+          alert={Object.keys(chatsAlerts).length > 0}
+        />
+        <NavbarButton screen="Explore" />
+      </View>
     </View>
   );
 };
