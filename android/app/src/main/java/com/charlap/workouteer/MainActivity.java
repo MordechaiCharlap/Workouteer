b9efe28a235e054358e69edf25d3dc4731e14e36
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-04-12 18:44:52 +0300

    react-navigation bugs fixed

--- android/app/src/main/java/com/charlap/workouteer/MainActivity.java
+++ android/app/src/main/java/com/charlap/workouteer/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
@@ -28,7 +30,15 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "main";
   }
-
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
