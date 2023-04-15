a7507acf7735456a6a3390c24f316aaf3703acbf
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-04-15 16:55:06 +0300

    cleaner code

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -2,7 +2,6 @@ import { View, StatusBar } from "react-native";
 import { React, useState, useCallback } from "react";
 import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import * as appStyle from "../components/AppStyleSheet";
-import * as firebase from "../services/firebase";
 import responsiveStyle from "../components/ResponsiveStyling";
 import SearchUsers from "../components/SearchUsers";
 import Explore from "../components/Explore";
@@ -13,8 +12,6 @@ import useNavbarDisplay from "../hooks/useNavbarDisplay";
 const ExploreScreen = () => {
   const { setCurrentScreen } = useNavbarDisplay();
   const { setScreen } = useNavbarNavigation();
-
-  const navigation = useNavigation();
   const { user } = useAuth();
 
   const [renderOption, setRenderOption] = useState("Explore");
@@ -25,16 +22,7 @@ const ExploreScreen = () => {
       setCurrentScreen("Explore");
     }, [])
   );
-  const userClicked = async (userData) => {
-    const friendshipStatus = await firebase.checkFriendShipStatus(
-      user,
-      userData.id
-    );
-    navigation.navigate("User", {
-      shownUser: userData,
-      friendshipStatus: friendshipStatus,
-    });
-  };
+
   return (
     <View style={responsiveStyle.safeAreaStyle}>
       <StatusBar
@@ -45,8 +33,7 @@ const ExploreScreen = () => {
         {renderOption == "Explore" && (
           <SearchUsers
             language={user.language}
-            userClicked={userClicked}
-            isEmpty={(isEmpty) => setSearchInputEmpty(isEmpty)}
+            setIsEmpty={(isEmpty) => setSearchInputEmpty(isEmpty)}
           />
         )}
         {renderOption == "Explore" && searchInputEmpty == true && <Explore />}
