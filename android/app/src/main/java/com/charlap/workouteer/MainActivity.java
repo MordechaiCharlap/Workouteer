42916bcde770f615cd931ecd71be7784365962a8
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-04-16 08:40:11 +0300

    removed react-native-gestures

--- android/app/src/main/java/com/charlap/workouteer/MainActivity.java
+++ android/app/src/main/java/com/charlap/workouteer/MainActivity.java
@@ -30,15 +30,6 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "main";
   }
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
-  }
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
