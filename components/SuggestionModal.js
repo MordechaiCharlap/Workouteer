09e2b4b0c242a786e38717ea72f496d1aa95adf1
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-04-27 12:47:21 +0300

    Suggestion modal generic language created

---
+++ components/SuggestionModal.js
@@ -0,0 +1,41 @@
+import { View, Text, TextInput } from "react-native";
+import React, { useState } from "react";
+import { Modal } from "react-native";
+import languageService from "../services/languageService";
+
+const SuggestionModal = (props) => {
+  const id = props.id;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const isValidTitle = () => {
+    const titleRegex = /^.{5,}$/;
+    if (title.match(titleRegex)) return true;
+    return false;
+  };
+  return (
+    <Modal visible={props.showSuggestionModal}>
+      <View
+        className={`flex-row${props.language == "hebrew" ? "-reverse" : ""}`}
+      >
+        <Text>{languageService[props.language].title + ":"}</Text>
+        <TextInput onChangeText={(text) => setTitle(text)} />
+      </View>
+      <Text>
+        {isValidTitle() ? "" : languageService[props.language].titleInstruction}
+      </Text>
+      <View
+        className={`flex-row${props.language == "hebrew" ? "-reverse" : ""}`}
+      >
+        <Text>{languageService[props.language].details + ":"}</Text>
+        <TextInput onChangeText={(text) => setContent(text)} />
+      </View>
+      <Text>
+        {isValidContent()
+          ? ""
+          : languageService[props.language].contentInstruction}
+      </Text>
+    </Modal>
+  );
+};
+
+export default SuggestionModal;
