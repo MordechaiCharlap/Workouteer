ec8d41aee8678d8f3b0a39469047f49b1fb4c8c6
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-01 08:18:01 +0300

    modal functionality implemented

--- hooks/useLeaderboardUpdates.js
+++ hooks/useLeaderboardUpdates.js
@@ -1,15 +1,41 @@
-import { useEffect, createContext, useContext } from "react";
+import { useEffect, createContext, useContext, useState } from "react";
+import { Modal, Text, View } from "react-native";
+
 import useAuth from "./useAuth";
 const LeaderboardUpdatesContext = createContext({});
 export const LeaderboardUpdatesProvider = (children) => {
   const { user } = useAuth();
+  const [modalMessage, setModalMessage] = useState("");
+  const [showModal, setShowModal] = useState(false);
   useEffect(() => {
-    if (leaderboardUpdatedMessage != null) {
+    const leaderboardUpdated = user.leaderboard.leaderboardUpdated;
+    if (user.leaderboard.leaderboardUpdated != null) {
+      setModalMessage(
+        `You finished ${leaderboardUpdated.lastPlace} place last weekend`
+      );
+      setShowModal(true);
+    } else {
+      setShowModal(false);
     }
   }, [user.leaderboard.leaderboardUpdatedMessage]);
   return (
     <LeaderboardUpdatesContext.Provider value={{}}>
       {children}
+      {showModal && (
+        <Modal
+          visible={showModal}
+          transparent={true}
+          onRequestClose={() => setShowModal(false)}
+        >
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
+            <View style={{ backgroundColor: "#fff", padding: 20 }}>
+              <Text>{modalMessage}</Text>
+            </View>
+          </View>
+        </Modal>
+      )}
     </LeaderboardUpdatesContext.Provider>
   );
 };
