0ae4cfcf0565ed08a20613ea7f2df824e7508965
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-01 08:44:32 +0300

    modal logic

--- hooks/useLeaderboardUpdates.js
+++ hooks/useLeaderboardUpdates.js
@@ -1,40 +1,54 @@
 import { useEffect, createContext, useContext, useState } from "react";
-import { Modal, Text, View } from "react-native";
-
+import {
+  Modal,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from "react-native";
+import languageService from "../services/languageService";
 import useAuth from "./useAuth";
+import AwesomeAlert from "react-native-awesome-alerts";
 const LeaderboardUpdatesContext = createContext({});
-export const LeaderboardUpdatesProvider = (children) => {
+export const LeaderboardUpdatesProvider = ({ children }) => {
   const { user } = useAuth();
   const [modalMessage, setModalMessage] = useState("");
   const [showModal, setShowModal] = useState(false);
+  const [testing, setTesting] = useState(true);
   useEffect(() => {
-    const leaderboardUpdated = user.leaderboard.leaderboardUpdated;
-    if (user.leaderboard.leaderboardUpdated != null) {
-      setModalMessage(
-        `You finished ${leaderboardUpdated.lastPlace} place last weekend`
-      );
-      setShowModal(true);
-    } else {
-      setShowModal(false);
-    }
-  }, [user.leaderboard.leaderboardUpdatedMessage]);
+    // if (!user?.leaderboard.leaderboardUpdatedMessage) return;
+    if (!user) return;
+    const leaderboardUpdated = testing
+      ? { lastPlace: 1 }
+      : user.leaderboard.leaderboardUpdated;
+    console.log("Showing leaderboard updated modal!");
+    setTesting(true);
+    setModalMessage(
+      `You finished ${leaderboardUpdated.lastPlace} place last weekend`
+    );
+    setShowModal(true);
+  }, [user?.leaderboard.leaderboardUpdated]);
   return (
     <LeaderboardUpdatesContext.Provider value={{}}>
       {children}
       {showModal && (
-        <Modal
-          visible={showModal}
-          transparent={true}
-          onRequestClose={() => setShowModal(false)}
-        >
-          <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-          >
-            <View style={{ backgroundColor: "#fff", padding: 20 }}>
-              <Text>{modalMessage}</Text>
-            </View>
-          </View>
-        </Modal>
+        <AwesomeAlert
+          show={showModal}
+          showProgress={false}
+          title={modalMessage}
+          message={modalMessage}
+          closeOnTouchOutside={true}
+          closeOnHardwareBackPress={false}
+          showConfirmButton={true}
+          confirmText={languageService[user.language].gotIt}
+          confirmButtonColor="#DD6B55"
+          onCancelPressed={() => {
+            setShowAlert(false);
+          }}
+          onConfirmPressed={() => {
+            setShowAlert(false);
+          }}
+        />
       )}
     </LeaderboardUpdatesContext.Provider>
   );
