de82dd809234a444c85d15ab627297e64df8eced
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-04 11:53:00 +0300

    ConfirmWorkout styling

--- components/ConfirmCurrentWorkoutButton.js
+++ components/ConfirmCurrentWorkoutButton.js
@@ -1,46 +1,45 @@
-import { View, Text, TouchableOpacity, Alert } from "react-native";
+import { View, Text, TouchableOpacity, Dimensions } from "react-native";
 import { workoutTypes } from "./WorkoutType";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import * as appStyle from "../utilities/appStyleSheet";
 import { useNavigation } from "@react-navigation/native";
 import languageService from "../services/languageService";
+import useWebResponsiveness from "../hooks/useWebResponsiveness";
 const ConfirmCurrentWorkoutButton = (props) => {
+  const { windowHeight } = useWebResponsiveness();
   const user = props.user;
   const navigation = useNavigation();
+  const iconSize = windowHeight
+    ? windowHeight / 15
+    : Dimensions.get("window").height / 15;
   return (
-    <View className="w-3/4">
-      <TouchableOpacity
-        onPress={() => navigation.navigate("ConfirmWorkout")}
-        className="rounded-full p-2 flex-row items-center w-full"
-        style={{
-          borderWidth: 2,
-          borderColor: appStyle.color_on_primary,
-          backgroundColor: appStyle.color_bg_variant,
-        }}
+    <TouchableOpacity
+      onPress={() => navigation.navigate("ConfirmWorkout")}
+      className="flex-row items-center w-full rounded"
+      style={{
+        borderWidth: 1,
+        borderColor: appStyle.color_on_primary,
+        backgroundColor: appStyle.color_bg_variant,
+      }}
+    >
+      <View
+        style={{ backgroundColor: appStyle.color_primary, padding: 7 }}
+        className="rounded-l"
       >
-        <View
-          className="rounded-full p-2"
-          style={{
-            borderWidth: 0.5,
-            borderColor: appStyle.color_on_primary,
-            backgroundColor: appStyle.color_primary,
-          }}
-        >
-          <FontAwesomeIcon
-            icon={workoutTypes[props.currentWorkout.type].icon}
-            size={30}
-            color={appStyle.color_on_primary}
-          />
-        </View>
-        <Text
-          className="text-center flex-1"
-          style={{ color: appStyle.color_on_primary }}
-        >
-          {languageService[user.language].confirmWorkout[user.isMale ? 1 : 0]}
-        </Text>
-      </TouchableOpacity>
-    </View>
+        <FontAwesomeIcon
+          icon={workoutTypes[props.currentWorkout.type].icon}
+          size={iconSize}
+          color={appStyle.color_on_primary}
+        />
+      </View>
+      <Text
+        className="text-center flex-1 tracking-widest font-semibold text-lg"
+        style={{ color: appStyle.color_on_primary }}
+      >
+        {languageService[user.language].confirmWorkout[user.isMale ? 1 : 0]}
+      </Text>
+    </TouchableOpacity>
   );
 };
 
