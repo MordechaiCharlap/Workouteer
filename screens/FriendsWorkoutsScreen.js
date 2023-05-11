6349a6f7e1ced5e17b8624ca3f1b9f36fe6d3db4
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-12 01:03:14 +0300

    taking care of not granted location

--- screens/FriendsWorkoutsScreen.js
+++ screens/FriendsWorkoutsScreen.js
@@ -1,24 +1,38 @@
-import { View, StatusBar } from "react-native";
+import { View, Text } from "react-native";
 import { safeAreaStyle } from "../components/safeAreaStyle";
 import Header from "../components/Header";
-import React, { useCallback } from "react";
-import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import React, { useCallback, useEffect, useState } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 import useAuth from "../hooks/useAuth";
-import * as appStyle from "../utilities/appStyleSheet";
 import languageService from "../services/languageService";
 import useNavbarDisplay from "../hooks/useNavbarDisplay";
 import useFriendsWorkouts from "../hooks/useFriendsWorkouts";
 import { FlatList } from "react-native";
 import WorkoutComponent from "../components/WorkoutComponent";
+import AwesomeModal from "../components/AwesomeModal";
+import * as geoService from "../services/geoService";
+import { color_on_primary, color_primary } from "../utilities/appStyleSheet";
 const FriendsWorkoutsScreen = () => {
-  const { friendsWorkouts, setFriendsWorkouts } = useFriendsWorkouts();
+  const { friendsWorkouts } = useFriendsWorkouts();
   const { user } = useAuth();
   const { setCurrentScreen } = useNavbarDisplay();
+  const [currentLocation, setCurrentLocation] = useState();
   useFocusEffect(
     useCallback(() => {
       setCurrentScreen("FriendsWorkouts");
     }, [])
   );
+  useEffect(() => {
+    const getCurrentLocation = async () => {
+      const location = await geoService.getCurrentLocation();
+      console.log(location);
+      setCurrentLocation(location);
+    };
+    getCurrentLocation();
+  }, []);
+  useEffect(() => {
+    console.log(currentLocation);
+  }, [currentLocation]);
   return (
     <View style={safeAreaStyle()}>
       <Header
@@ -26,19 +40,34 @@ const FriendsWorkoutsScreen = () => {
         goBackOption={true}
       />
       <View className="flex-1 px-4">
-        <FlatList
-          className="p-2"
-          showsVerticalScrollIndicator={false}
-          data={friendsWorkouts}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <WorkoutComponent
-              workout={item}
-              isPastWorkout={false}
-              screen={"FriendsWorkouts"}
-            />
-          )}
-        />
+        {currentLocation != null ? (
+          <FlatList
+            className="p-2"
+            showsVerticalScrollIndicator={false}
+            data={friendsWorkouts}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <WorkoutComponent
+                location={currentLocation}
+                workout={item}
+                isPastWorkout={false}
+                screen={"FriendsWorkouts"}
+              />
+            )}
+          />
+        ) : (
+          <View className="flex-1 items-center">
+            <Text
+              style={{
+                color: color_on_primary,
+                backgroundColor: color_primary,
+              }}
+              className="text-2xl px-3 py-2 rounded mt-40"
+            >
+              {languageService[user.language].gettingCurrentLocation}
+            </Text>
+          </View>
+        )}
       </View>
     </View>
   );
