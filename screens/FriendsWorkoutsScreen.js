f7769599593b7fb354787c0a1fc2dd90e1157cb9
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-12 13:49:18 +0300

    less useEffecting

--- screens/FriendsWorkoutsScreen.js
+++ screens/FriendsWorkoutsScreen.js
@@ -29,9 +29,6 @@ const FriendsWorkoutsScreen = () => {
     };
     getCurrentLocation();
   }, []);
-  useEffect(() => {
-    console.log(currentLocation);
-  }, [currentLocation]);
   return (
     <View style={safeAreaStyle()}>
       <Header
