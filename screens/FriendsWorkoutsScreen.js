9cd52e9646b38d216b12a38c18d289a7519d8ace
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-13 00:18:59 +0300

    friendsWorkout using lastLocation

--- screens/FriendsWorkoutsScreen.js
+++ screens/FriendsWorkoutsScreen.js
@@ -9,26 +9,22 @@ import useNavbarDisplay from "../hooks/useNavbarDisplay";
 import useFriendsWorkouts from "../hooks/useFriendsWorkouts";
 import { FlatList } from "react-native";
 import WorkoutComponent from "../components/WorkoutComponent";
-import AwesomeModal from "../components/AwesomeModal";
-import * as geoService from "../services/geoService";
-import { color_on_primary, color_primary } from "../utilities/appStyleSheet";
 const FriendsWorkoutsScreen = () => {
   const { friendsWorkouts } = useFriendsWorkouts();
   const { user } = useAuth();
   const { setCurrentScreen } = useNavbarDisplay();
-  const [currentLocation, setCurrentLocation] = useState();
   useFocusEffect(
     useCallback(() => {
       setCurrentScreen("FriendsWorkouts");
     }, [])
   );
-  useEffect(() => {
-    const getCurrentLocation = async () => {
-      const location = await geoService.getCurrentLocation();
-      setCurrentLocation(location);
-    };
-    getCurrentLocation();
-  }, []);
+  // useEffect(() => {
+  //   const getCurrentLocation = async () => {
+  //     await geoService.getCurrentLocation(user);
+  //   };
+  //   getCurrentLocation(user);
+  // }, []);
+
   return (
     <View style={safeAreaStyle()}>
       <Header
@@ -36,34 +32,19 @@ const FriendsWorkoutsScreen = () => {
         goBackOption={true}
       />
       <View className="flex-1 px-4">
-        {currentLocation != null ? (
-          <FlatList
-            className="p-2"
-            showsVerticalScrollIndicator={false}
-            data={friendsWorkouts}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => (
-              <WorkoutComponent
-                location={currentLocation}
-                workout={item}
-                isPastWorkout={false}
-                screen={"FriendsWorkouts"}
-              />
-            )}
-          />
-        ) : (
-          <View className="flex-1 items-center">
-            <Text
-              style={{
-                color: color_on_primary,
-                backgroundColor: color_primary,
-              }}
-              className="text-2xl px-3 py-2 rounded mt-40"
-            >
-              {languageService[user.language].gettingCurrentLocation}
-            </Text>
-          </View>
-        )}
+        <FlatList
+          className="p-2"
+          showsVerticalScrollIndicator={false}
+          data={friendsWorkouts}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <WorkoutComponent
+              workout={item}
+              isPastWorkout={false}
+              screen={"FriendsWorkouts"}
+            />
+          )}
+        />
       </View>
     </View>
   );
