7940fd9a2e035f881d51ec4b31e596dfb94fefc7
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-13 01:38:34 +0300

    cleaner code, better styling

--- components/WorkoutType.js
+++ components/WorkoutType.js
@@ -1,4 +1,10 @@
-import { View, Text, FlatList, TouchableOpacity, Platform } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import * as appStyle from "../utilities/appStyleSheet";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
@@ -44,40 +50,26 @@ const WorkoutType = (props) => {
           typeClicked(type.id);
         }}
         className="rounded-lg mb-3"
-        style={{
-          width: "48%",
-          borderWidth: 1,
-          borderColor:
-            type.id == chosenType
-              ? appStyle.color_on_primary
-              : appStyle.color_primary,
-        }}
+        style={style.typeButton}
       >
         <View
           style={{
             backgroundColor:
               type.id == chosenType
-                ? appStyle.color_primary
-                : appStyle.color_bg,
+                ? appStyle.color_primary_variant
+                : appStyle.color_primary,
           }}
           className={`py-4 px-2 items-center rounded-lg`}
         >
           <FontAwesomeIcon
-            color={
-              type.id == chosenType
-                ? appStyle.color_on_primary
-                : appStyle.color_primary
-            }
+            color={appStyle.color_on_primary}
             icon={type.icon}
             size={iconSize}
           />
           <Text
             className="text-center"
             style={{
-              color:
-                type.id == chosenType
-                  ? appStyle.color_on_primary
-                  : appStyle.color_primary,
+              color: appStyle.color_on_primary,
             }}
           >
             {languageService[props.language].workoutTypes[type.id]}
@@ -88,42 +80,26 @@ const WorkoutType = (props) => {
   };
   return (
     <View>
-      <View className="flex-row flex-wrap justify-between">
-        {renderWorkoutTypeButton(workoutTypes[1])}
-        {renderWorkoutTypeButton(workoutTypes[2])}
-        {renderWorkoutTypeButton(workoutTypes[3])}
-        {renderWorkoutTypeButton(workoutTypes[4])}
-      </View>
       {props.everythingOption == true && (
         <TouchableOpacity
           onPress={() => {
             typeClicked(workoutTypes[0].id);
           }}
-          className="rounded-lg mb-5"
-          style={{
-            borderWidth: 1,
-            borderColor:
-              workoutTypes[0].id == chosenType
-                ? appStyle.color_on_primary
-                : appStyle.color_primary,
-          }}
+          className="rounded-lg mb-3"
         >
           <View
             style={{
               backgroundColor:
                 workoutTypes[0].id == chosenType
-                  ? appStyle.color_primary
-                  : appStyle.color_bg,
+                  ? appStyle.color_primary_variant
+                  : appStyle.color_primary,
             }}
             className={`p-4 items-center rounded-lg`}
           >
             <Text
               className="text-center font-bold tracking-widest"
               style={{
-                color:
-                  workoutTypes[0].id == chosenType
-                    ? appStyle.color_on_primary
-                    : appStyle.color_primary,
+                color: appStyle.color_on_primary,
               }}
             >
               {languageService[props.language].workoutTypes[0]}
@@ -131,8 +107,16 @@ const WorkoutType = (props) => {
           </View>
         </TouchableOpacity>
       )}
+      <View className="flex-row flex-wrap justify-between">
+        {renderWorkoutTypeButton(workoutTypes[1])}
+        {renderWorkoutTypeButton(workoutTypes[2])}
+        {renderWorkoutTypeButton(workoutTypes[3])}
+        {renderWorkoutTypeButton(workoutTypes[4])}
+      </View>
     </View>
   );
 };
-
+const style = StyleSheet.create({
+  typeButton: { width: "48%", borderWidth: 1 },
+});
 export default WorkoutType;
