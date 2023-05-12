3d33960c488dcca0fbc700fae4a51ee254288f79
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-13 00:21:41 +0300

    updating user each getCurrentLocation

--- services/geoService.js
+++ services/geoService.js
@@ -1,5 +1,6 @@
 import * as Location from "expo-location";
-export const getCurrentLocation = async () => {
+import { updateUser } from "./firebase";
+export const getCurrentLocation = async (user) => {
   let { status } = await Location.requestForegroundPermissionsAsync();
   if (status !== "granted") {
     return false;
@@ -12,5 +13,10 @@ export const getCurrentLocation = async () => {
     latitude: location.coords.latitude,
     longitude: location.coords.longitude,
   };
+  if (user && latLongLocation) {
+    const userClone = { ...user };
+    userClone.lastLocation = latLongLocation;
+    await updateUser(userClone);
+  }
   return latLongLocation;
 };
