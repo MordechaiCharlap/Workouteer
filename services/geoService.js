7b21241d5b9015f959a7826292355fee1dc836e2
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-13 14:45:02 +0300

    adding city and country of user

--- services/geoService.js
+++ services/geoService.js
@@ -1,5 +1,8 @@
 import * as Location from "expo-location";
 import { updateUser } from "./firebase";
+import Geocoder from "react-native-geocoding";
+import { addCountryAndCityToDbIfNeeded } from "./firebase";
+
 export const getCurrentLocation = async (user) => {
   let { status } = await Location.requestForegroundPermissionsAsync();
   if (status !== "granted") {
@@ -16,6 +19,10 @@ export const getCurrentLocation = async (user) => {
   if (user && latLongLocation) {
     const userClone = { ...user };
     userClone.lastLocation = latLongLocation;
+    const cityAndCountry = await addCountryAndCityToDbIfNeeded(latLongLocation);
+    console.log(cityAndCountry);
+    userClone.city = cityAndCountry.city;
+    userClone.country = cityAndCountry.country;
     await updateUser(userClone);
   }
   return latLongLocation;
