5a11f2c09cc84cd0994604051c799b35bf06faa0
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-16 23:48:25 +0300

    slide backOrExit button created

---
+++ components/slides/BackOrExitButton.js
@@ -0,0 +1,24 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import * as appStyle from "../../utilities/appStyleSheet";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faX, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+const BackOrExitButton = (props) => {
+  return (
+    <View className="flex-row">
+      <TouchableOpacity
+        onPress={props.handlePrevPage}
+        style={{ marginTop: 20, marginHorizontal: 20 }}
+      >
+        <FontAwesomeIcon
+          icon={props.firstPage ? faX : faChevronLeft}
+          size={props.style.size}
+          color={props.style.color}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BackOrExitButton;
