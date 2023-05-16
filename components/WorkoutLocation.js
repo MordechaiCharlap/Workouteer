5b799564c0dcf9ca51c182f777a99dbf7e658027
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-16 13:42:55 +0300

    cleaner code, less states, map buug fixed

--- components/WorkoutLocation.js
+++ components/WorkoutLocation.js
@@ -22,59 +22,23 @@ const WorkoutLocation = (props) => {
   const { user } = useAuth();
   const [showMap, setShowMap] = useState(false);
   const [location, setLocation] = useState(null);
-  const [markerCoords, setMarkerCoords] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [address, setAddress] = useState(null);
   useEffect(() => {
-    const initialShowMap = async () => {
-      setIsLoading(true);
-      if (user.lastLocation) {
-        setMarkerCoords(user.lastLocation);
-        setLocation(user.lastLocation);
-      } else {
-        const latLongLocation = await geoService.getCurrentLocation(user);
-        setMarkerCoords(latLongLocation);
-        setLocation(latLongLocation);
-      }
-      setShowMap(true);
-      setIsLoading(false);
-    };
     if (props.value) {
       setLocation(props.value);
-    } else if (props.initialShow) {
-      initialShowMap();
+    } else {
+      if (user.lastLocation) {
+        setLocation(user.lastLocation);
+      }
     }
   }, []);
-
-  // const getAddress = () => {
-  //   fetch(
-  //     `https://maps.googleapis.com/maps/api/geocode/json?latlng=${
-  //       location.latitude
-  //     },${location.longitude}&language=${
-  //       user.language == "hebrew" ? "he" : "en"
-  //     }&key=${mapsApiKey}`
-  //   )
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       if (
-  //         data.results[0].types.includes("gym") ||
-  //         data.results[0].types.includes("health")
-  //       ) {
-  //         fetch(
-  //           `https://maps.googleapis.com/maps/api/place/details/json?place_id=${data.results[0].place_id}&key=${mapsApiKey}`
-  //         )
-  //           .then((response) => response.json())
-  //           .then((data) => {
-  //             const addressRef = data.result.name;
-  //             setAddress(addressRef);
-  //           });
-  //       } else {
-  //         const addressRef = data.results[0].formatted_address;
-  //         setAddress(addressRef);
-  //       }
-  //     })
-  //     .catch((error) => console.error(error));
-  // };
+  const getCurrentLocation = async () => {
+    setIsLoading(true);
+    const latLongLocation = await geoService.getCurrentLocation(user);
+    console.log(latLongLocation);
+    setLocation(latLongLocation);
+  };
   const locationPinned = (coords) => {
     if (coords == location) {
       return;
@@ -82,26 +46,79 @@ const WorkoutLocation = (props) => {
     setLocation(coords);
   };
   useEffect(() => {
-    if (location) props.locationChanged(location);
+    if (location) {
+      props.locationChanged(location);
+      setShowMap(true);
+    }
+    setIsLoading(false);
   }, [location]);
   return (
     <View>
       <View className="items-center">
-        {showMap &&
-          (isLoading ? (
+        {isLoading ? (
+          <View
+            className="rounded"
+            style={{
+              borderWidth: 0.5,
+              borderColor: appStyle.color_on_primary,
+              backgroundColor: appStyle.color_bg_variant,
+            }}
+          >
             <Text
               className="text-lg p-3"
-              style={{ backgroundColor: appStyle.color_secondary }}
+              style={{ color: appStyle.color_on_primary }}
             >
-              {languageService[props.language].gettingCurrentLocation}
+              {languageService[user.language].gettingCurrentLocation}
             </Text>
-          ) : (
-            <PinOnMap
-              language={props.language}
-              defaultMarker={markerCoords}
-              saveLocation={locationPinned}
-            />
-          ))}
+          </View>
+        ) : showMap ? (
+          <PinOnMap
+            language={user.language}
+            defaultMarker={location}
+            saveLocation={locationPinned}
+          />
+        ) : location != false ? (
+          <TouchableOpacity
+            onPress={getCurrentLocation}
+            className="justify-center items-center px-3 rounded"
+            style={{ backgroundColor: appStyle.color_primary, height: 50 }}
+          >
+            <Text style={{ color: appStyle.color_on_primary }}>
+              {
+                languageService[user.language].clickForLocation[
+                  user.isMale ? 1 : 0
+                ]
+              }
+            </Text>
+          </TouchableOpacity>
+        ) : (
+          <View
+            className="justify-center items-center p-3 rounded"
+            style={{ backgroundColor: appStyle.color_primary }}
+          >
+            <Text
+              className="text-lg"
+              style={{ color: appStyle.color_on_primary }}
+            >
+              {
+                languageService[user.language]
+                  .locationPermissionOrInternetProblem
+              }
+            </Text>
+            <TouchableOpacity
+              className="px-3 py-1 rounded-full m-2"
+              onPress={getCurrentLocation}
+              style={{ backgroundColor: appStyle.color_bg }}
+            >
+              <Text
+                className="text-lg"
+                style={{ color: appStyle.color_primary }}
+              >
+                {languageService[user.language].tryAgain[user.isMale ? 1 : 0]}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     </View>
   );
@@ -112,3 +129,33 @@ const style = StyleSheet.create({
   },
 });
 export default WorkoutLocation;
+
+// const getAddress = () => {
+//   fetch(
+//     `https://maps.googleapis.com/maps/api/geocode/json?latlng=${
+//       location.latitude
+//     },${location.longitude}&language=${
+//       user.language == "hebrew" ? "he" : "en"
+//     }&key=${mapsApiKey}`
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (
+//         data.results[0].types.includes("gym") ||
+//         data.results[0].types.includes("health")
+//       ) {
+//         fetch(
+//           `https://maps.googleapis.com/maps/api/place/details/json?place_id=${data.results[0].place_id}&key=${mapsApiKey}`
+//         )
+//           .then((response) => response.json())
+//           .then((data) => {
+//             const addressRef = data.result.name;
+//             setAddress(addressRef);
+//           });
+//       } else {
+//         const addressRef = data.results[0].formatted_address;
+//         setAddress(addressRef);
+//       }
+//     })
+//     .catch((error) => console.error(error));
+// };
