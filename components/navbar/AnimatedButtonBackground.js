cdac5d7f400251abf10f27e79a019093c5287758
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-17 17:16:58 +0300

    Navbar button animated background created

---
+++ components/navbar/AnimatedButtonBackground.js
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+import { color_primary } from "../../utilities/appStyleSheet";
+
+export const AnimatedButtonBackround = ({ index, fixedWidth }) => {
+  const position = useSharedValue(2);
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: position.value }],
+      width: fixedWidth / 5, // Assuming you have 5 buttons in the navbar
+    };
+  });
+
+  // Update the position when the index changes
+  useEffect(() => {
+    console.log(index);
+    position.value = withTiming(index * (fixedWidth / 5));
+    console.log(position.value);
+  }, [index]);
+
+  return (
+    <Animated.View style={[styles.animatedView, animatedStyle]}>
+      <View
+        style={{
+          backgroundColor: color_primary,
+          flex: 1,
+          width: "100%",
+          borderRadius: 10,
+        }}
+      ></View>
+    </Animated.View>
+  );
+};
+const styles = StyleSheet.create({
+  animatedView: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    padding: 5,
+  },
+});
