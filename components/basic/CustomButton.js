fa555990658e28140495458850323001d82f8b5c
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-05-18 16:23:26 +0300

    better customButton

--- components/basic/CustomButton.js
+++ components/basic/CustomButton.js
@@ -2,21 +2,15 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 
 const CustomButton = ({ children, style, onPress, disabled }) => {
+  const buttonStyle = {
+    borderRadius: 4,
+    padding: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  const merged = { ...buttonStyle, ...style };
   return (
-    <TouchableOpacity
-      disabled={disabled}
-      onPress={onPress}
-      style={[
-        {
-          borderRadius: 4,
-          paddingHorizontal: 12,
-          paddingVertical: 8,
-          justifyContent: "center",
-          alignItems: "center",
-        },
-        style,
-      ]}
-    >
+    <TouchableOpacity disabled={disabled} onPress={onPress} style={merged}>
       {children}
     </TouchableOpacity>
   );
