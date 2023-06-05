4f7680c14ad85c50a15ff4a1ae1b1c462dd60841
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-05 18:21:34 +0300

    SO CLEAN

--- hooks/useMaintenance.js
+++ hooks/useMaintenance.js
@@ -1,28 +1,18 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import AwesomeModal from "../components/AwesomeModal";
 import useAppData from "./useAppData";
 
 const MaintenanceContext = createContext({});
 export const MaintenanceProvider = ({ children }) => {
-  console.log("test");
   const { appData } = useAppData();
   const [underMaintenance, setUnderMaintenance] = useState(false);
   useEffect(() => {
-    console.log("testing");
-  }, []);
-  useEffect(() => {
-    console.log("test");
     if (!appData) return;
-    console.log(appData);
-    if (appData.underMaintenance == true) setUnderMaintenance(true);
+    if (appData.underMaintenance) setUnderMaintenance(true);
   }, [appData]);
   return (
     <MaintenanceContext.Provider value={{ underMaintenance }}>
       {children}
-      <AwesomeModal />
     </MaintenanceContext.Provider>
   );
 };
-export default function useMaintenance() {
-  useContext(MaintenanceContext);
-}
+export const useMaintenance = () => useContext(MaintenanceContext);
