119580e333163fb019285f3b6eebf04aeed4ee22
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-07 07:25:22 +0300

    future bug fixed

--- functions/index.js
+++ functions/index.js
@@ -194,7 +194,6 @@ exports.helloWorld = functions.https.onRequest(async (request, response) => {
   await sendPushNotification();
 });
 const sendPushNotification = async (userToSend, title, body, data) => {
-  if (isWeb) return;
   if (userToSend.pushToken) {
     const pushNotification = {
       to: userToSend.pushToken,
