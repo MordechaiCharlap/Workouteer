ee3bae5ac3a712612182a10560e28ad676e6ff10
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-15 15:53:07 +0300

    cleaner useFirebase hook

--- hooks/useFirebase.js
+++ hooks/useFirebase.js
@@ -1,13 +1,24 @@
+import { initializeAuth } from "firebase/auth";
 import { createContext, useContext, useEffect, useState } from "react";
+import { getReactNativePersistence } from "firebase/auth/react-native";
+import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../firebase.config";
+import { getStorage } from "firebase/storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const FirebaseContext = createContext({});
 export const FirebaseProvider = ({ children }) => {
-  const [firestore, setFirestore] = useState();
-  const [storage, setStorage] = useState();
-  const [auth, setAuth] = useState();
-  useEffect(() => {}, []);
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const storage = getStorage(app);
+  const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+  });
   return (
-    <FirebaseContext.Provider value={{}}>{children}</FirebaseContext.Provider>
+    <FirebaseContext.Provider value={{ db, storage, auth }}>
+      {children}
+    </FirebaseContext.Provider>
   );
 };
 export default function useFirebase() {
