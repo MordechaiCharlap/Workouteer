6988333959d7da92271ecaa7f12aa9a748ecc278
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-16 12:44:19 +0300

    mispelling fixed

--- components/slides/Title.js
+++ components/slides/Title.js
@@ -7,8 +7,8 @@ export const Title = ({ title, color }) => {
       <View className="flex-1"></View>
       <CustomText
         className="text-center text-lg"
-        style={{ color: color||appStyle.color_on_background }}
-      >?
+        style={{ color: color || appStyle.color_on_background }}
+      >
         {title}
       </CustomText>
     </View>
