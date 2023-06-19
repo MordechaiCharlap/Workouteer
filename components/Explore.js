8b4095d8649fbc48ed20bc64344b9a95c5d38891
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-19 16:44:22 +0300

    getting Explore working

--- components/Explore.js
+++ components/Explore.js
@@ -1,7 +1,21 @@
 import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import useAuth from "../hooks/useAuth";
+import useFirebase from "../hooks/useFirebase";
+import { collection, limit, orderBy, query, where } from "firebase/firestore";
 
 const Explore = () => {
+  const { user } = useAuth();
+  const { db } = useFirebase();
+  const getLatestWorkouts = async () => {
+    const latestWorkoutsQuery = query(
+      collection(db, "workouts"),
+      where(`members.${user.id}`, "==", null),
+      where("isConfirmed", "==", true),
+      orderBy("startingTime", "desc"),
+      limit(30)
+    );
+  };
   return <ScrollView></ScrollView>;
 };
 
