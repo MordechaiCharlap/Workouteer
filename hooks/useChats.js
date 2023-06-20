be15720cfa7f06b014bd70b23302cc32695446d9
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-20 13:59:33 +0300

    useChats hook created and implemented for semaless

---
+++ hooks/useChats.js
@@ -0,0 +1,30 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import useAuth from "./useAuth";
+import { getChatsArrayIncludeUsers } from "../services/firebase";
+import useAlerts from "./useAlerts";
+const ChatsContext = createContext({});
+export const ChatsProvider = ({ children }) => {
+  const { userLoaded, user } = useAuth();
+  const { chatsAlerts } = useAlerts();
+  const [chats, setChats] = useState();
+  const refreshChats = async () => {
+    var arr = await getChatsArrayIncludeUsers(user);
+    setChats(arr);
+  };
+  useEffect(() => {
+    if (!userLoaded) return;
+    const getChats = async () => {
+      var arr = await getChatsArrayIncludeUsers(user);
+      setChats(arr);
+    };
+    getChats();
+  }, [userLoaded, chatsAlerts]);
+  return (
+    <ChatsContext.Provider value={{ chats, setChats, refreshChats }}>
+      {children}
+    </ChatsContext.Provider>
+  );
+};
+export default function useChats() {
+  return useContext(ChatsContext);
+}
