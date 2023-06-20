fe92f5fd18cb7bf1cf88bc306c93415fe437bf2b
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-20 15:07:22 +0300

    cleaner code

--- hooks/useFirebase.js
+++ hooks/useFirebase.js
@@ -1,26 +1,38 @@
-import { initializeAuth } from "firebase/auth";
+import { getAuth, initializeAuth } from "firebase/auth";
 import { createContext, useContext, useEffect, useState } from "react";
 import { getReactNativePersistence } from "firebase/auth/react-native";
 import { getFirestore } from "firebase/firestore";
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { firebaseConfig } from "../firebase.config";
 import { getStorage } from "firebase/storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { getDb } from "../services/db";
 
 const FirebaseContext = createContext({});
 export const FirebaseProvider = ({ children }) => {
-  const app = initializeApp(firebaseConfig);
-  const db = getDb();
-  const storage = getStorage(app);
-  const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage),
-  });
-  return (
-    <FirebaseContext.Provider value={{ db, storage, auth }}>
-      {children}
-    </FirebaseContext.Provider>
-  );
+  let app, auth, storage, db;
+
+  if (!getApps().length) {
+    try {
+      app = initializeApp(firebaseConfig);
+      auth = initializeAuth(app, {
+        persistence: getReactNativePersistence(AsyncStorage),
+      });
+    } catch (error) {
+      console.log("Error initializing app: " + error);
+    }
+  } else {
+    app = getApp();
+    auth = getAuth(app);
+    db = getFirestore();
+    storage = getStorage(app);
+  }
+
+  if (auth && db && storage && app)
+    return (
+      <FirebaseContext.Provider value={{ db, storage, auth }}>
+        {children}
+      </FirebaseContext.Provider>
+    );
 };
 export default function useFirebase() {
   return useContext(FirebaseContext);
