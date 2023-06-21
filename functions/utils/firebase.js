43f39a167969a854a1a3f188119074908c7d9b7c
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-21 17:40:42 +0300

    verify access token added

--- functions/utils/firebase.js
+++ functions/utils/firebase.js
@@ -2,4 +2,25 @@ const admin = require("firebase-admin");
 
 const db = admin.firestore();
 const bucket = admin.storage().bucket();
-module.exports = { db, bucket };
+
+const verifyAccessToken = (req, res, next) => {
+  try {
+    // Get the access token from the request headers
+    const accessToken = req.headers.authorization;
+
+    // Check if the access token exists
+    if (!accessToken) {
+      return res.status(401).json({ error: "Access token missing" });
+    }
+
+    // Perform token verification logic here
+    // ...
+
+    // If token verification succeeds, proceed to the next middleware
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+module.exports = { db, bucket, verifyAccessToken };
