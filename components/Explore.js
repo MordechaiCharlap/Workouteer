1efaac658ed8ea7b31a14f017a3689db38991182
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-21 11:56:27 +0300

    Logic moved to context hook

--- components/Explore.js
+++ components/Explore.js
@@ -20,49 +20,49 @@ import CustomButton from "./basic/CustomButton";
 import * as appStyle from "../utilities/appStyleSheet";
 import WorkoutComponent from "./WorkoutComponent";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faRotateRight } from "@fortawesome/free-solid-svg-icons";
+import { faRotateRight, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import useExplore from "../hooks/useExplore";
+import CustomText from "./basic/CustomText";
+import languageService from "../services/languageService";
 
 const Explore = () => {
-  const { latestWorkouts, setLatestWorkouts } = useExplore();
-  const refreshLatestWorkouts = async () => {
-    const data = [];
-    const q = query(
-      collection(db, "workouts"),
-      where("confirmed", "==", true),
-      orderBy("startingTime", "desc"),
-      limit(10)
-    );
-    (await getDocs(q)).forEach((doc) => {
-      data.push({ ...doc.data(), id: doc.id });
-    });
-    setLatestWorkouts(data);
-  };
+  const { user } = useAuth();
+  const { latestWorkouts, refreshLatestWorkouts, refreshing } = useExplore();
   return (
-    <View>
-      <View style={{ paddingHorizontal: 16 }}>
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          paddingHorizontal: 16,
+          borderBottomColor: appStyle.color_outline,
+          borderBottomWidth: 0.5,
+        }}
+      >
         <CustomButton
-          onPress={refreshLatestWorkouts}
-          className="self-start my-2"
-          style={{ backgroundColor: appStyle.color_primary }}
+          onPress={!refreshing ? refreshLatestWorkouts : () => {}}
+          className="self-start my-2 flex-row"
+          style={{ backgroundColor: appStyle.color_primary, width: "35%" }}
         >
           <FontAwesomeIcon
-            icon={faRotateRight}
+            icon={!refreshing ? faRotateRight : faSpinner}
             color={appStyle.color_on_primary}
             size={25}
           />
+          <View style={{ width: 10 }} />
+          <CustomText style={{ color: appStyle.color_on_primary }}>
+            {!refreshing
+              ? languageService[user.language].refresh
+              : languageService[user.language].loading}
+          </CustomText>
         </CustomButton>
       </View>
-      <View>
-        <FlatList
-          style={[{ paddingHorizontal: 16 }]}
-          data={latestWorkouts}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <WorkoutComponent workout={item} isPastWorkout={true} />
-          )}
-        />
-      </View>
+      <FlatList
+        style={[{ paddingHorizontal: 16, paddingVertical: 5 }]}
+        data={latestWorkouts}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <WorkoutComponent workout={item} isPastWorkout={true} />
+        )}
+      />
     </View>
   );
 };
