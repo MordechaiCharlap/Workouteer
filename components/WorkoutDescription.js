525d6b1d00b138d57490dff0eeea6b6668acb984
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-22 17:39:46 +0300

    styling

--- components/WorkoutDescription.js
+++ components/WorkoutDescription.js
@@ -3,26 +3,29 @@ import React from "react";
 import * as appStyle from "../utils/appStyleSheet";
 import languageService from "../services/languageService";
 import useAuth from "../hooks/useAuth";
+import appComponentsDefaultStyles from "../utils/appComponentsDefaultStyles";
 
 const WorkoutDescription = ({ descChanged, color }) => {
   const { user } = useAuth();
   return (
     <View>
-      <Text style={{ color: appStyle.color_primary, fontSize: 15 }}>
+      <Text style={{ color: appStyle.color_on_background, fontSize: 15 }}>
         {languageService[user.language].details}:
       </Text>
       <TextInput
         style={{
           textAlignVertical: "top",
-          backgroundColor: color,
+          borderWidth: 0.5,
+          borderColor: appStyle.color_outline,
+          backgroundColor: appStyle.color_surface_variant,
           borderRadius: 4,
           padding: 8,
-          color: appStyle.color_on_primary,
+          color: color,
         }}
         autoCorrect={false}
         multiline
         placeholder={languageService[user.language].optionalText}
-        placeholderTextColor={appStyle.color_lighter}
+        placeholderTextColor={appStyle.color_outline}
         numberOfLines={10}
         onChangeText={(text) => descChanged(text)}
       ></TextInput>
