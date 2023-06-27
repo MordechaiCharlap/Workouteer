6730e313e0410f615d5cd83034ba400478b6b93a
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-27 12:02:14 +0300

    some experimenting

--- screens/IntervalTimerScreen.js
+++ screens/IntervalTimerScreen.js
@@ -1,18 +1,34 @@
 import { View, Text } from "react-native";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useFocusEffect } from "@react-navigation/native";
 import useNavbarDisplay from "../hooks/useNavbarDisplay";
 import CustomButton from "../components/basic/CustomButton";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 import * as appStlye from "../utils/appStyleSheet";
+import DropDownPicker from "react-native-dropdown-picker";
 const IntervalTimerScreen = () => {
   const { setCurrentScreen } = useNavbarDisplay();
+  const [hoursOpen, setHoursOpen] = useState(false);
+  const [hours, setHours] = useState([]);
+  const [hour, setHour] = useState(false);
   useFocusEffect(
     useCallback(() => {
       setCurrentScreen("IntervalTimer");
     }, [])
   );
+  useEffect(() => {
+    setHours([
+      { label: "test1", value: 1 },
+      { label: "test2", value: 2 },
+      { label: "test3", value: 3 },
+      { label: "test4", value: 4 },
+      { label: "test5", value: 5 },
+      { label: "test6", value: 6 },
+      { label: "test7", value: 7 },
+      { label: "test8", value: 8 },
+    ]);
+  }, []);
   return (
     <View style={{ flex: 1, backgroundColor: appStlye.color_primary }}>
       <CustomButton>
@@ -22,6 +38,15 @@ const IntervalTimerScreen = () => {
           size={40}
         />
       </CustomButton>
+      <DropDownPicker
+        style={{}}
+        open={hoursOpen}
+        value={hour}
+        items={hours}
+        setOpen={setHoursOpen}
+        setValue={setHour}
+        setItems={setHours}
+      />
     </View>
   );
 };
