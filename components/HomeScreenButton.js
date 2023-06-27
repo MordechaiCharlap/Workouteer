f4c6995ae3023168edc8183896367a46ac5120de
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-06-27 13:04:46 +0300

    another alert option

--- components/HomeScreenButton.js
+++ components/HomeScreenButton.js
@@ -1,12 +1,12 @@
-import { TouchableOpacity, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import useAuth from "../hooks/useAuth";
-import useAlerts from "../hooks/useAlerts";
 import AlertDot from "./AlertDot";
 import * as appStyle from "../utils/appStyleSheet";
 import CustomButton from "./basic/CustomButton";
+import CustomText from "./basic/CustomText";
 const HomeScreenButton = (props) => {
   const navigation = useNavigation();
   const { user } = useAuth();
@@ -41,6 +41,26 @@ const HomeScreenButton = (props) => {
               />
             </View>
           )}
+          {/* {props.number && (
+            <View className="absolute left-2 top-2" style={{ width: "30%" }}>
+              <AlertDot
+                size={26}
+                text={props.number < 100 ? props.number : "99+"}
+                textColor={appStyle.color_on_primary}
+                color={appStyle.color_primary}
+              />
+              {!props.alert && (
+                <View className="absolute left-0 bottom-0">
+                  <AlertDot
+                    size={10}
+                    color={appStyle.color_error}
+                    borderWidth={1}
+                    borderColor={props.style.backgroundColor}
+                  />
+                </View>
+              )}
+            </View>
+          )} */}
           <FontAwesomeIcon
             icon={props.icon}
             size={props.style.iconSize}
