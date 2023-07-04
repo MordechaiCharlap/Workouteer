a45cf97b922f66ac8c3d620edecbc479ee500311
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-05 00:13:59 +0300

    Search users is much more efficient and better

--- components/SearchUsers.js
+++ components/SearchUsers.js
@@ -5,8 +5,9 @@ import {
   Image,
   Text,
   ScrollView,
+  FlatList,
 } from "react-native";
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 
 import * as appStyle from "../utils/appStyleSheet";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
@@ -17,59 +18,97 @@ import { useNavigation } from "@react-navigation/native";
 import useAuth from "../hooks/useAuth";
 import CustomTextInput from "./basic/CustomTextInput";
 import appComponentsDefaultStyles from "../utils/appComponentsDefaultStyles";
+import useAppData from "../hooks/useAppData";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import useFirebase from "../hooks/useFirebase";
 const SearchUsers = (props) => {
   const { user } = useAuth();
   const navigation = useNavigation();
-  const [searchedUser, setSearchedUser] = useState(null);
+  const { usersData } = useAppData();
+  const { db } = useFirebase();
+  const [shownUsers, setShownUsers] = useState([]);
+  const filterIdsBySearchedText = (searchedText) => {
+    const matchingUserIds = usersData.allUsersIds.filter((userId) =>
+      userId.includes(searchedText.toLowerCase())
+    );
+    return matchingUserIds;
+  };
+  useEffect(() => {
+    console.log(usersData);
+  }, []);
   const textChanged = async (text) => {
-    if (text != "") {
-      props.isEmpty?.(false);
-      const searchedUserDoc = await firebase.searchUser(text);
-      if (searchedUserDoc != null) setSearchedUser(searchedUserDoc.data());
-    } else {
-      props.isEmpty?.(true);
+    if (text.length >= 3) {
+      const matchingUsers = filterIdsBySearchedText(text);
+      console.log(matchingUsers);
+      if (matchingUsers.length == 0) return;
+      const firstTen = matchingUsers.slice(0, 10);
+      const q = query(
+        collection(db, "users"),
+        where("__name__", "in", firstTen)
+      );
+      const querySnapshot = await getDocs(q);
+      const usersDataArr = [];
+      querySnapshot.forEach((doc) => {
+        usersDataArr.push(doc.data());
+      });
+      console.log(usersDataArr.length);
+      setShownUsers(usersDataArr);
+
+      // const searchedUserDoc = await firebase.searchUser(text);
+      // if (searchedUserDoc != null) setSearchedUser(searchedUserDoc.data());
     }
   };
   const userClicked = async (userData) => {
-    const friendshipStatus = await firebase.checkFriendShipStatus(
-      user,
-      userData.id
-    );
-    navigation.navigate("Profile", {
-      shownUser: userData,
-      friendshipStatus: friendshipStatus,
-    });
+    if (userData.id == user.id) {
+      navigation.navigate("MyProfile");
+    } else {
+      const friendshipStatus = await firebase.checkFriendShipStatus(
+        user,
+        userData.id
+      );
+      navigation.navigate("Profile", {
+        shownUser: userData,
+        friendshipStatus: friendshipStatus,
+      });
+    }
   };
-  const renderSearchedUser = () => {
+  renderShownUsers = () => {
     return (
-      <TouchableOpacity
-        style={{ paddingHorizontal: 16 }}
-        onPress={() => userClicked(searchedUser)}
-        className="flex-row items-center mt-2"
-      >
-        <Image
-          source={{
-            uri: searchedUser.img,
-          }}
-          className="h-14 w-14 bg-white rounded-full mr-4"
-        />
-        <View>
-          <Text
-            className="text-xl font-semibold tracking-wider"
-            style={{ color: appStyle.color_on_background }}
+      <FlatList
+        data={shownUsers}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={{ paddingHorizontal: 16 }}
+            onPress={() => userClicked(item)}
+            className="flex-row items-center mt-2"
           >
-            {searchedUser.id}
-          </Text>
-          <Text
-            className="text-md opacity-60 tracking-wider"
-            style={{ color: appStyle.color_on_background }}
-          >
-            {searchedUser.displayName}
-          </Text>
-        </View>
-      </TouchableOpacity>
+            <Image
+              source={{
+                uri: item.img,
+              }}
+              className="h-14 w-14 bg-white rounded-full mr-4"
+            />
+            <View>
+              <Text
+                className="text-xl font-semibold tracking-wider"
+                style={{ color: appStyle.color_on_background }}
+              >
+                {item.id}
+              </Text>
+              <Text
+                className="text-md opacity-60 tracking-wider"
+                style={{ color: appStyle.color_on_background }}
+              >
+                {item.displayName}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
     );
   };
+
   return (
     <View>
       <View
@@ -97,7 +136,7 @@ const SearchUsers = (props) => {
           />
         </View>
       </View>
-      {searchedUser != null && renderSearchedUser()}
+      {shownUsers.length > 0 && renderShownUsers()}
     </View>
   );
 };
