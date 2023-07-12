e39dbe2e0122a9707266286d9c9933b29052a8b8
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-12 15:54:42 +0300

    ProviderWrapper added for cleaner code

---
+++ ProviderWrapper.js
@@ -0,0 +1,58 @@
+import { TailwindProvider } from "tailwindcss-react-native";
+import { AuthPrvider } from "./hooks/useAuth";
+import { NotificationsProvider } from "./hooks/usePushNotifications";
+import { AlertsProvider } from "./hooks/useAlerts";
+import { NavbarNavigationProvider } from "./hooks/useNavbarNavigation";
+import { NavbarDisplayProvider } from "./hooks/useNavbarDisplay";
+import { CurrentWorkoutProvider } from "./hooks/useCurrentWorkout";
+import { ResponsivenessProvider } from "./hooks/useResponsiveness";
+import { FriendsWorkoutsProvider } from "./hooks/useFriendsWorkouts";
+import { LeaderboardProvider } from "./hooks/useLeaderboard";
+import { ConfirmedWorkoutsProvider } from "./hooks/useConfirmedWorkouts";
+import { AppDataProvider } from "./hooks/useAppData";
+import { WorkoutLogicProvider } from "./hooks/useWorkoutLogic";
+import { ConnectionProvider } from "./hooks/useConnection";
+import { FirebaseProvider } from "./hooks/useFirebase";
+import { ChatsProvider } from "./hooks/useChats";
+import { ExploreProvider } from "./hooks/useExplore";
+const ProviderWrapper = ({ children }) => {
+  return (
+    <FirebaseProvider>
+      <AppDataProvider>
+        <AuthPrvider>
+          <ResponsivenessProvider>
+            <ConnectionProvider>
+              <NavbarDisplayProvider>
+                <AlertsProvider>
+                  <ChatsProvider>
+                    <CurrentWorkoutProvider>
+                      <FriendsWorkoutsProvider>
+                        <NotificationsProvider>
+                          <LeaderboardProvider>
+                            <ExploreProvider>
+                              <ConfirmedWorkoutsProvider>
+                                <WorkoutLogicProvider>
+                                  <NavbarNavigationProvider>
+                                    <TailwindProvider>
+                                      {children}
+                                    </TailwindProvider>
+                                  </NavbarNavigationProvider>
+                                </WorkoutLogicProvider>
+                              </ConfirmedWorkoutsProvider>
+                            </ExploreProvider>
+                          </LeaderboardProvider>
+                        </NotificationsProvider>
+                      </FriendsWorkoutsProvider>
+                    </CurrentWorkoutProvider>
+                  </ChatsProvider>
+                </AlertsProvider>
+              </NavbarDisplayProvider>
+            </ConnectionProvider>
+          </ResponsivenessProvider>
+        </AuthPrvider>
+      </AppDataProvider>
+    </FirebaseProvider>
+  );
+};
+
+export default ProviderWrapper;
