11ebb8bfd4f9f964c3d64fbc93d45ee8745f2b0e
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-16 16:21:30 +0300

    so much UI added

--- screens/IntervalTimerScreen.js
+++ screens/IntervalTimerScreen.js
@@ -4,10 +4,24 @@ import { useFocusEffect } from "@react-navigation/native";
 import useNavbarDisplay from "../hooks/useNavbarDisplay";
 import CustomButton from "../components/basic/CustomButton";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPause,
+  faPauseCircle,
+  faPlay,
+  faPlayCircle,
+  faRefresh,
+  faRotateRight,
+  faStop,
+  faStopCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import * as appStlye from "../utils/appStyleSheet";
+import CustomText from "../components/basic/CustomText";
+import { safeAreaStyle } from "../components/safeAreaStyle";
+import { secondsToMinutesPlusSeconds } from "../utils/timeFunctions";
 const IntervalTimerScreen = () => {
   const { setCurrentScreen } = useNavbarDisplay();
+  const [intervalSeconds, setIntervalSeconds] = useState(120);
+  const [isPlaying, setIsPlaying] = useState();
   useFocusEffect(
     useCallback(() => {
       setCurrentScreen("IntervalTimer");
@@ -15,21 +29,78 @@ const IntervalTimerScreen = () => {
   );
   useEffect(() => {}, []);
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: appStlye.color_primary,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <CustomButton>
-        <FontAwesomeIcon
-          icon={faPlayCircle}
-          color={appStlye.color_on_background}
-          size={40}
-        />
-      </CustomButton>
+    <View style={safeAreaStyle()}>
+      <View className="h-1/3 items-center justify-center">
+        <View
+          style={{
+            width: "100%",
+            alignItems: "center",
+            backgroundColor: appStlye.color_primary_container,
+            borderColor: appStlye.color_outline,
+            borderWidth: 1,
+          }}
+        >
+          <CustomText
+            style={{
+              fontSize: 100,
+              color: appStlye.color_on_primary_container,
+            }}
+          >
+            {secondsToMinutesPlusSeconds(intervalSeconds)}
+          </CustomText>
+        </View>
+      </View>
+      <View className="h-1/3 items-center justify-center">
+        {isPlaying == true ? (
+          <View className="flex-row">
+            <CustomButton>
+              <FontAwesomeIcon
+                icon={faStop}
+                size={100}
+                color={appStlye.color_success}
+              />
+            </CustomButton>
+            <CustomButton>
+              <FontAwesomeIcon
+                icon={faPause}
+                size={100}
+                color={appStlye.color_success}
+              />
+            </CustomButton>
+          </View>
+        ) : isPlaying == false ? (
+          <CustomButton>
+            <FontAwesomeIcon
+              icon={faPlay}
+              size={100}
+              color={appStlye.color_success}
+            />
+          </CustomButton>
+        ) : (
+          <CustomButton>
+            <FontAwesomeIcon
+              icon={faRotateRight}
+              size={100}
+              color={appStlye.color_success}
+            />
+          </CustomButton>
+        )}
+      </View>
+      <View className="h-1/3 items-center justify-center">
+        {!isPlaying && (
+          <CustomButton
+            style={{
+              backgroundColor: appStlye.color_surface_variant,
+              borderColor: appStlye.color_outline,
+              borderWidth: 1,
+            }}
+          >
+            <CustomText className="text-xl" style={{}}>
+              Change resting time
+            </CustomText>
+          </CustomButton>
+        )}
+      </View>
     </View>
   );
 };
