a513058acb61211448ca7407773a1403a82c3206
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-16 16:22:10 +0300

    removed all the useless imports

--- App.js
+++ App.js
@@ -1,26 +1,9 @@
-import { TailwindProvider } from "tailwindcss-react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./StackNavigator";
-import { AuthPrvider } from "./hooks/useAuth";
-import { NotificationsProvider } from "./hooks/usePushNotifications";
-import { AlertsProvider } from "./hooks/useAlerts";
 import { initGeocoder } from "./geocoder";
 import * as WebBrowser from "expo-web-browser";
 import { Platform } from "react-native";
-import { NavbarNavigationProvider } from "./hooks/useNavbarNavigation";
-import { NavbarDisplayProvider } from "./hooks/useNavbarDisplay";
-import { CurrentWorkoutProvider } from "./hooks/useCurrentWorkout";
-import { ResponsivenessProvider } from "./hooks/useResponsiveness";
-import { FriendsWorkoutsProvider } from "./hooks/useFriendsWorkouts";
-import { LeaderboardProvider } from "./hooks/useLeaderboard";
-import { ConfirmedWorkoutsProvider } from "./hooks/useConfirmedWorkouts";
-import { AppDataProvider } from "./hooks/useAppData";
 import * as SplashScreen from "expo-splash-screen";
-import { WorkoutLogicProvider } from "./hooks/useWorkoutLogic";
-import { ConnectionProvider } from "./hooks/useConnection";
-import { FirebaseProvider } from "./hooks/useFirebase";
-import { ChatsProvider } from "./hooks/useChats";
-import { ExploreProvider } from "./hooks/useExplore";
 import ProviderWrapper from "./ProviderWrapper";
 export default function App() {
   SplashScreen.preventAutoHideAsync();
