f357f4c4c509622b7f23922dbcfa92a5899aa41f
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-16 23:53:41 +0300

    styling

--- screens/WorkoutProgramScreen.js
+++ screens/WorkoutProgramScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import useNavbarDisplay from "../hooks/useNavbarDisplay";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import React, { useCallback, useEffect, useRef, useState } from "react";
@@ -8,12 +8,16 @@ import CustomButton from "../components/basic/CustomButton";
 import { safeAreaStyle } from "../components/safeAreaStyle";
 import useAuth from "../hooks/useAuth";
 import useFirebase from "../hooks/useFirebase";
-const WorkoutProgramScreen = () => {
+import Header from "../components/Header";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import appComponentsDefaultStyles from "../utils/appComponentsDefaultStyles";
+const WorkoutProgramScreen = ({ route }) => {
   const { setCurrentScreen } = useNavbarDisplay();
   const navigation = useNavigation();
   const { user } = useAuth();
   const { db } = useFirebase();
-  const [savedWorkoutPrograms, setSavedWorkoutPrograms] = useState();
+  const program = route.params.program;
   useFocusEffect(
     useCallback(() => {
       setCurrentScreen("WorkoutProgram");
@@ -21,7 +25,32 @@ const WorkoutProgramScreen = () => {
   );
   return (
     <View style={safeAreaStyle()}>
-      <Text>WorkoutProgramScreen</Text>
+      <Header goBackOption={true} icon={"<"} title={program.name} />
+      <FlatList
+        keyExtractor={(item, index) => index}
+        data={program.workouts}
+        style={{ marginHorizontal: 16 }}
+        renderItem={({ item }) => (
+          <CustomButton
+            className="flex-row justify-between items-center"
+            style={{
+              backgroundColor: appStyle.color_primary_container,
+            }}
+          >
+            <CustomText style={{ fontWeight: 600, fontSize: 30 }}>
+              {item.name}
+            </CustomText>
+            <CustomButton
+              style={{ backgroundColor: appStyle.color_on_primary_container }}
+            >
+              <FontAwesomeIcon
+                icon={faPlay}
+                color={appStyle.color_background}
+              />
+            </CustomButton>
+          </CustomButton>
+        )}
+      />
     </View>
   );
 };
