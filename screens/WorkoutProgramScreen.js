42fcbc7891b90bc00cf874844f96e8d74266fec5
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-16 18:01:52 +0300

    workout program added

---
+++ screens/WorkoutProgramScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const WorkoutProgramScreen = () => {
+  return (
+    <View>
+      <Text>WorkoutProgramScreen</Text>
+    </View>
+  );
+};
+
+export default WorkoutProgramScreen;
