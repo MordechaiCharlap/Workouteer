92c20de4ac88b0388cbfc0474a13a2e9c9596a91
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-16 21:24:43 +0300

    program screen implemented

--- screens/WorkoutProgramScreen.js
+++ screens/WorkoutProgramScreen.js
@@ -1,9 +1,26 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { View, Text, FlatList } from "react-native";
+import useNavbarDisplay from "../hooks/useNavbarDisplay";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import * as appStyle from "../utils/appStyleSheet";
+import CustomText from "../components/basic/CustomText";
+import CustomButton from "../components/basic/CustomButton";
+import { safeAreaStyle } from "../components/safeAreaStyle";
+import useAuth from "../hooks/useAuth";
+import useFirebase from "../hooks/useFirebase";
 const WorkoutProgramScreen = () => {
+  const { setCurrentScreen } = useNavbarDisplay();
+  const navigation = useNavigation();
+  const { user } = useAuth();
+  const { db } = useFirebase();
+  const [savedWorkoutPrograms, setSavedWorkoutPrograms] = useState();
+  useFocusEffect(
+    useCallback(() => {
+      setCurrentScreen("WorkoutProgram");
+    }, [])
+  );
   return (
-    <View>
+    <View style={safeAreaStyle()}>
       <Text>WorkoutProgramScreen</Text>
     </View>
   );
