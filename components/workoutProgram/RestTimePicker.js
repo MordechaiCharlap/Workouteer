801258438be8162f690b5a6402eb760e1c90cc55
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-17 17:19:34 +0300

    rest time picker created

---
+++ components/workoutProgram/RestTimePicker.js
@@ -0,0 +1,135 @@
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import CustomButton from "../basic/CustomButton";
+import CustomText from "../basic/CustomText";
+import AwesomeModal from "../AwesomeModal";
+import CustomTextInput from "../basic/CustomTextInput";
+import * as appStyle from "../../utils/appStyleSheet";
+const RestTimePicker = ({ setTotalRestSeconds, totalRestSeconds }) => {
+  const [showRestModal, setShowRestModal] = useState(false);
+  const [restMinutes, setRestMinutes] = useState(totalRestSeconds / 60);
+  const [restSeconds, setRestSeconds] = useState(totalRestSeconds % 60);
+  const [minutesFocused, setMinutesFocused] = useState(false);
+  const [secondsFocused, setSecondsFocused] = useState(false);
+  useEffect(() => {
+    setTotalRestSeconds(restMinutes * 60 + restSeconds);
+  }, [restMinutes, restSeconds]);
+  const handleMinutesChanged = (text) => {
+    if (!minutesFocused) {
+      console.log("not focused");
+      return;
+    }
+    var validRegex = /(?:[0-5]?[0-9])?/;
+    if (text.match(validRegex)) {
+      console.log("match");
+      setRestMinutes(String(text));
+    } else {
+      console.log("not match");
+      setRestMinutes("00");
+    }
+  };
+  const handleSecondsChanged = (text) => {
+    if (!secondsFocused) return;
+
+    console.log(text);
+    var validRegex = /(?:[0-5]?[0-9])?/;
+    if (text.match(validRegex)) {
+      console.log("match");
+      setRestSeconds(String(text));
+    } else {
+      console.log("not match");
+      setRestSeconds("00");
+    }
+  };
+
+  const minutesBlur = () => {
+    setMinutesFocused(false);
+    setRestMinutes(String(restMinutes).padStart(2, "0"));
+  };
+  const secondsBlur = () => {
+    setSecondsFocused(false);
+    setRestSeconds(String(restSeconds).padStart(2, "0"));
+  };
+  useEffect(() => {
+    secondsBlur();
+    minutesBlur();
+  }, [showRestModal]);
+  return (
+    <View>
+      <CustomButton
+        onPress={() => {
+          setShowRestModal(true);
+        }}
+        style={{
+          backgroundColor: appStyle.color_surface_variant,
+          width: 80,
+        }}
+      >
+        <CustomText>
+          {restSeconds != 0 || restMinutes != 0
+            ? `${restMinutes}:${restSeconds}`
+            : "Choose"}
+        </CustomText>
+      </CustomButton>
+      <AwesomeModal
+        onDismiss={() => {
+          setShowRestModal(false);
+        }}
+        onConfirmPressed={() => setShowRestModal(false)}
+        setShowModal={setShowRestModal}
+        title={"Choose duration"}
+        showModal={showRestModal}
+        confirmText={"Set"}
+        customView={
+          <View className="flex-row" style={{ columnGap: 10, height: 50 }}>
+            <View>
+              <CustomText>minutes</CustomText>
+              <View>
+                <CustomTextInput
+                  style={{ backgroundColor: appStyle.color_surface_variant }}
+                  textAlign="center"
+                  value={restMinutes}
+                  onBlur={minutesBlur}
+                  keyboardType="numeric"
+                  maxLength={2}
+                  onFocus={() => setMinutesFocused(true)}
+                  onChangeText={(text) => handleMinutesChanged(text)}
+                ></CustomTextInput>
+              </View>
+            </View>
+            <View>
+              <CustomText></CustomText>
+              <CustomText
+                style={{
+                  minHeight: 40,
+                  fontSize: 20,
+                  textAlign: "center",
+                  textAlignVertical: "center",
+                }}
+              >
+                :
+              </CustomText>
+            </View>
+            <View>
+              <CustomText>seconds</CustomText>
+              <View>
+                <CustomTextInput
+                  style={{ backgroundColor: appStyle.color_surface_variant }}
+                  textAlign="center"
+                  value={restSeconds}
+                  onBlur={secondsBlur}
+                  onFocus={() => setSecondsFocused(true)}
+                  keyboardType="numeric"
+                  maxLength={2}
+                  onChangeText={(text) => handleSecondsChanged(text)}
+                ></CustomTextInput>
+              </View>
+            </View>
+          </View>
+        }
+      />
+    </View>
+  );
+};
+
+export default RestTimePicker;
