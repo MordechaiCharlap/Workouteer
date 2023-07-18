8e1e432319905729ac2637ca39197ae3b7723699
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-18 04:50:19 +0300

    moving to useContext instead of infinite states

--- screens/CreateWorkoutProgramScreen.js
+++ screens/CreateWorkoutProgramScreen.js
@@ -6,7 +6,13 @@ import {
   ScrollView,
   Dimensions,
 } from "react-native";
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, {
+  createContext,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import useNavbarDisplay from "../hooks/useNavbarDisplay";
 import { useFocusEffect } from "@react-navigation/native";
 import { safeAreaStyle } from "../components/safeAreaStyle";
@@ -18,7 +24,7 @@ import CustomButton from "../components/basic/CustomButton";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 import EditingWorkout from "../components/workoutProgram/EditingWorkout";
-
+export const ProgramContext = createContext();
 const CreateWorkoutProgramScreen = () => {
   const { setCurrentScreen } = useNavbarDisplay();
   useFocusEffect(
@@ -26,110 +32,117 @@ const CreateWorkoutProgramScreen = () => {
       setCurrentScreen("CreateWorkoutProgram");
     }, [])
   );
-  const [programName, setProgramName] = useState("");
-  const [workouts, setWorkouts] = useState([
-    { name: "", exercises: [{ name: "", sets: 0, reps: 0 }] },
-  ]);
+  const [programData, setProgramData] = useState({
+    name: "",
+    workouts: [
+      { name: "", restSeconds: 0, exercises: [{ name: "", sets: 0, reps: 0 }] },
+    ],
+  });
   const [maximizedWorkout, setMaximizedWorkout] = useState(0);
   const newWorkout = () => {
     const workoutsClone = workouts.slice();
     workoutsClone.push({
       name: "",
+      restSeconds: 0,
       exercises: [{ name: "", sets: 0, reps: 0 }],
     });
     setWorkouts(workoutsClone);
   };
-  const deleteWorkout = (index) => {
-    const workoutsClone = workouts.slice();
-    workoutsClone.splice(index, 1);
-    if (maximizedWorkout == index) {
-      setMaximizedWorkout();
-    } else if (maximizedWorkout > index) {
-      setMaximizedWorkout((prev) => prev - 1);
-    }
-    setWorkouts(workoutsClone);
+
+  const handleProgramNameChange = (text) => {
+    const dataClone = { ...programData };
+    dataClone.name = text;
+    setProgramData(dataClone);
   };
   return (
-    <View style={safeAreaStyle()}>
-      <Header title={"Create new program"} goBackOption={true} />
-      <View style={{ paddingHorizontal: 16, rowGap: 10, flex: 1 }}>
-        <View className="flex-row items-center" style={{ columnGap: 5 }}>
-          <CustomText>Program name:</CustomText>
-          <CustomTextInput
-            value={programName}
-            onChangeText={(text) => setProgramName(text)}
-            style={{ backgroundColor: appStyle.color_surface_variant }}
-          />
-        </View>
-        <View className="flex-row">
-          <CustomText>Workouts:</CustomText>
-        </View>
-        <FlatList
-          keyboardShouldPersistTaps={"always"}
-          data={workouts}
-          keyExtractor={(_, index) => index}
-          scrollEnabled={true}
-          contentContainerStyle={{ rowGap: 10 }}
-          ListHeaderComponent={
-            <CustomButton
-              className="flex-row"
-              onPress={newWorkout}
-              style={{
-                height: 40,
-                borderColor: appStyle.color_outline,
-                borderWidth: 0.5,
-                columnGap: 3,
-                padding: 10,
-                borderRadius: 8,
-                backgroundColor: appStyle.color_primary,
-              }}
-            >
-              <FontAwesomeIcon
-                color={appStyle.color_on_primary}
-                icon={faPlusCircle}
-                size={15}
-              />
-              <CustomText style={{ color: appStyle.color_on_primary }}>
-                New Workout
-              </CustomText>
-            </CustomButton>
-          }
-          renderItem={({ item, index }) => (
-            <EditingWorkout
-              workout={item}
-              workoutIndex={index}
-              maximized={index == maximizedWorkout}
-              minimizeWorkout={() => setMaximizedWorkout(null)}
-              maximizeWorkout={() => setMaximizedWorkout(index)}
-              deleteWorkout={() => deleteWorkout(index)}
+    <ProgramContext.Provider
+      value={{
+        programData,
+        setProgramData,
+        maximizedWorkout,
+        setMaximizedWorkout,
+      }}
+    >
+      <View style={safeAreaStyle()}>
+        <Header title={"Create new program"} goBackOption={true} />
+        <View style={{ paddingHorizontal: 16, rowGap: 10, flex: 1 }}>
+          <View className="flex-row items-center" style={{ columnGap: 5 }}>
+            <CustomText>Program name:</CustomText>
+            <CustomTextInput
+              value={programData.name}
+              onChangeText={handleProgramNameChange}
+              style={{ backgroundColor: appStyle.color_surface_variant }}
             />
-          )}
-        />
+          </View>
+          <View className="flex-row">
+            <CustomText>Workouts:</CustomText>
+          </View>
+          <FlatList
+            keyboardShouldPersistTaps={"always"}
+            data={programData.workouts}
+            keyExtractor={(_, index) => index}
+            scrollEnabled={true}
+            contentContainerStyle={{ rowGap: 10 }}
+            ListHeaderComponent={
+              <CustomButton
+                className="flex-row"
+                onPress={newWorkout}
+                style={{
+                  height: 40,
+                  borderColor: appStyle.color_outline,
+                  borderWidth: 0.5,
+                  columnGap: 3,
+                  padding: 10,
+                  borderRadius: 8,
+                  backgroundColor: appStyle.color_primary,
+                }}
+              >
+                <FontAwesomeIcon
+                  color={appStyle.color_on_primary}
+                  icon={faPlusCircle}
+                  size={15}
+                />
+                <CustomText style={{ color: appStyle.color_on_primary }}>
+                  New Workout
+                </CustomText>
+              </CustomButton>
+            }
+            renderItem={({ item, index }) => (
+              <EditingWorkout
+                workout={item}
+                workoutIndex={index}
+                maximized={index == maximizedWorkout}
+                minimizeWorkout={() => setMaximizedWorkout(null)}
+                maximizeWorkout={() => setMaximizedWorkout(index)}
+              />
+            )}
+          />
 
-        <CustomButton
-          round
-          style={{
-            flexDirection: "row",
-            height: 40,
-            borderColor: appStyle.color_outline,
-            borderWidth: 0.5,
-            columnGap: 3,
-            padding: 10,
-            borderRadius: 8,
-            backgroundColor: appStyle.color_on_background,
-          }}
-        >
-          <CustomText
+          <CustomButton
+            round
             style={{
-              color: appStyle.color_background,
-              fontWeight: 600,
+              flexDirection: "row",
+              height: 40,
+              borderColor: appStyle.color_outline,
+              borderWidth: 0.5,
+              columnGap: 3,
+              padding: 10,
+              borderRadius: 8,
+              backgroundColor: appStyle.color_on_background,
             }}
           >
-            Create program
-          </CustomText>
-        </CustomButton>
+            <CustomText
+              style={{
+                color: appStyle.color_background,
+                fontWeight: 600,
+              }}
+            >
+              Create program
+            </CustomText>
+          </CustomButton>
+        </View>
       </View>
-    </View>
+    </ProgramContext.Provider>
   );
 };
 
