204a85bad3d36a3cbc84b51731cd281df06267fe
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-24 18:30:20 +0300

    interval timer logic for workout is working

--- screens/IntervalTimerScreen.js
+++ screens/IntervalTimerScreen.js
@@ -1,9 +1,11 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import { useFocusEffect } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import useNavbarDisplay from "../hooks/useNavbarDisplay";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import {
+  faCheck,
+  faFlagCheckered,
   faPause,
   faPlay,
   faRefresh,
@@ -16,25 +18,33 @@ import CustomButton from "../components/basic/CustomButton";
 import { safeAreaStyle } from "../components/safeAreaStyle";
 import { secondsToMinutesPlusSeconds } from "../utils/timeFunctions";
 import Header from "../components/Header";
-import CustomTextInput from "../components/basic/CustomTextInput";
-import AwesomeModal from "../components/AwesomeModal";
-import languageService from "../services/languageService";
 import useAuth from "../hooks/useAuth";
-const IntervalTimerScreen = () => {
+import ProgressBar from "../components/ProgressBar";
+import appComponentsDefaultStyles from "../utils/appComponentsDefaultStyles";
+const IntervalTimerScreen = ({ route }) => {
   const { user } = useAuth();
+  const navigation = useNavigation();
+  const workout = route.params?.workout;
+  const [allSets, setAllSets] = useState();
+  const [currentSetIndex, setCurrentSetIndex] = useState(0);
   const { setCurrentScreen } = useNavbarDisplay();
-  const [chosenTotalSeconds, setChosenTotalSeconds] = useState(120);
-  const [chosenMinutes, setChosenMinutes] = useState();
-  const [chosenSeconds, setChosenSeconds] = useState();
-  const [intervalSeconds, setIntervalSeconds] = useState(chosenTotalSeconds);
+  const [intervalSeconds, setIntervalSeconds] = useState(workout.restSeconds);
   const [isPlaying, setIsPlaying] = useState(false);
   const [timer, setTimer] = useState();
-  const [showModal, setShowModal] = useState(false);
   useFocusEffect(
     useCallback(() => {
       setCurrentScreen("IntervalTimer");
     }, [])
   );
+  useEffect(() => {
+    const intervalSets = [];
+    workout.exercises.forEach((exercise) => {
+      for (var i = 0; i < exercise.sets; i++) {
+        intervalSets.push({ name: exercise.name, reps: exercise.reps });
+      }
+    });
+    setAllSets(intervalSets);
+  }, []);
   useEffect(() => {
     if (!isPlaying && timer) {
       clearInterval(timer);
@@ -47,7 +57,7 @@ const IntervalTimerScreen = () => {
   }, [isPlaying]);
   const restart = () => {
     setIsPlaying(true);
-    setIntervalSeconds(chosenTotalSeconds);
+    setIntervalSeconds(workout.restSeconds);
     setTimer(
       setInterval(() => {
         setIntervalSeconds((prev) => {
@@ -62,13 +72,15 @@ const IntervalTimerScreen = () => {
   };
   const stop = () => {
     setIsPlaying(false);
-    setIntervalSeconds(chosenTotalSeconds);
+    setIntervalSeconds(restSeconds);
   };
   const pause = () => {
     setIsPlaying(false);
   };
   const end = () => {
     setIsPlaying();
+    setCurrentSetIndex((prev) => prev + 1);
+    setIntervalSeconds(workout.restSeconds);
   };
   const play = () => {
     setIsPlaying(true);
@@ -84,6 +96,9 @@ const IntervalTimerScreen = () => {
       }, 1000)
     );
   };
+  const finishWorkout = () => {
+    navigation.navigate("Home");
+  };
   const handleSecondsChanged = (text) => {
     var validRegex = /(?:[0-5]?[0-9])?/;
     if (text.match(validRegex)) {
@@ -96,151 +111,132 @@ const IntervalTimerScreen = () => {
       setChosenMinutes(String(text));
     }
   };
+  const onContainerColor = isPlaying
+    ? appStyle.color_on_background
+    : appStyle.color_on_primary;
   return (
-    <View style={safeAreaStyle()}>
-      <Header title={"IntervalTimer"} goBackOption={true} icon={"<"} />
-      <View className="h-1/3 items-center justify-center">
+    allSets != null &&
+    currentSetIndex != null && (
+      <View
+        style={[
+          safeAreaStyle(),
+          {
+            backgroundColor:
+              isPlaying == true
+                ? appStyle.color_success
+                : appStyle.color_primary,
+          },
+        ]}
+      >
         <View
-          style={{
-            width: "100%",
-            alignItems: "center",
-            backgroundColor: appStyle.color_primary_container,
-            borderColor: appStyle.color_outline,
-            borderWidth: 1,
-          }}
+          className="h-1/3 items-center justify-center"
+          style={{ paddingHorizontal: 16 }}
         >
           <CustomText
+            className="text-8xl"
             style={{
-              fontSize: 100,
-              color: appStyle.color_on_primary_container,
+              color: onContainerColor,
+              letterSpacing: 4,
             }}
           >
-            {secondsToMinutesPlusSeconds(intervalSeconds)}
+            {isPlaying ? "REST" : "WORK"}
           </CustomText>
+          {!isPlaying && (
+            <View className="absolute w-full bottom-0 items-center">
+              <CustomText
+                className="text-3xl font-semibold"
+                style={{ color: onContainerColor }}
+              >
+                {allSets[currentSetIndex].name}
+              </CustomText>
+              <CustomText
+                className="text-xl font-semibold"
+                style={{ color: onContainerColor, marginBottom: 15 }}
+              >
+                {allSets[currentSetIndex].reps} reps
+              </CustomText>
+            </View>
+          )}
         </View>
-      </View>
-      <View className="h-1/3 items-center justify-center">
-        {isPlaying == true ? (
-          <View className="flex-row">
-            <CustomButton onPress={pause}>
-              <FontAwesomeIcon
-                icon={faPause}
-                size={100}
-                color={appStyle.color_success}
-              />
-            </CustomButton>
-            <CustomButton onPress={stop}>
-              <FontAwesomeIcon
-                icon={faStop}
-                size={100}
-                color={appStyle.color_success}
-              />
+        {!isPlaying ? (
+          <View className="h-1/3 items-center justify-center">
+            <View style={{ height: 20 }} />
+            <CustomText
+              className="text-5xl font-semibold"
+              style={{ color: onContainerColor }}
+            >
+              {allSets.length - 1 == currentSetIndex
+                ? "FINISH WORKOUT"
+                : "FINISH SET"}
+            </CustomText>
+            <CustomButton
+              onPress={() => {
+                allSets.length - 1 == currentSetIndex
+                  ? finishWorkout()
+                  : play();
+              }}
+            >
+              <View
+                className="rounded-full"
+                style={[
+                  {
+                    backgroundColor: appStyle.color_success,
+                    padding: 10,
+                  },
+                ]}
+              >
+                <FontAwesomeIcon
+                  icon={
+                    allSets.length - 1 == currentSetIndex
+                      ? faFlagCheckered
+                      : faCheck
+                  }
+                  size={60}
+                  color={onContainerColor}
+                />
+              </View>
             </CustomButton>
           </View>
-        ) : isPlaying == false ? (
-          <CustomButton onPress={play}>
-            <FontAwesomeIcon
-              icon={faPlay}
-              size={100}
-              color={appStyle.color_success}
-            />
-          </CustomButton>
         ) : (
-          <CustomButton onPress={restart}>
-            <FontAwesomeIcon
-              icon={faRotateRight}
-              size={100}
-              color={appStyle.color_success}
-            />
-          </CustomButton>
-        )}
-      </View>
-      <View className="h-1/3 items-center justify-center">
-        {!isPlaying && (
-          <CustomButton
-            onPress={() => setShowModal(true)}
-            style={{
-              backgroundColor: appStyle.color_surface_variant,
-              borderColor: appStyle.color_outline,
-              borderWidth: 1,
-            }}
-          >
-            <CustomText className="text-xl" style={{}}>
-              Change resting time
+          <View className="h-1/3 justify-center items-center">
+            <CustomText
+              style={{
+                fontSize: 100,
+                color: appStyle.color_on_primary_container,
+              }}
+            >
+              {secondsToMinutesPlusSeconds(intervalSeconds)}
             </CustomText>
-          </CustomButton>
+          </View>
         )}
-      </View>
-      <AwesomeModal
-        onDismiss={() => {
-          setShowModal(false);
-        }}
-        onConfirmPressed={() => {
-          setShowModal(false);
-          setChosenTotalSeconds(
-            60 * parseInt(chosenMinutes || 0) + parseInt(chosenSeconds || 0)
-          );
-          setIntervalSeconds(
-            60 * parseInt(chosenMinutes || 0) + parseInt(chosenSeconds || 0)
-          );
-        }}
-        setShowModal={setShowModal}
-        title={
-          languageService[user.language].choose[user.isMale ? 1 : 0] +
-          " " +
-          languageService[user.language].duration
-        }
-        showModal={showModal}
-        confirmText={languageService[user.language].confirm}
-        customView={
-          <View>
-            <View className="flex-row" style={{ columnGap: 10, height: 50 }}>
-              <View>
-                <CustomText>
-                  {languageService[user.language].minutes}
-                </CustomText>
-                <View>
-                  <CustomTextInput
-                    style={{ backgroundColor: appStyle.color_surface_variant }}
-                    textAlign="center"
-                    keyboardType="numeric"
-                    maxLength={2}
-                    onChangeText={(text) => handleMinutesChanged(text)}
-                  ></CustomTextInput>
-                </View>
-              </View>
-              <View>
-                <CustomText></CustomText>
-                <CustomText
-                  style={{
-                    minHeight: 40,
-                    fontSize: 20,
-                    textAlign: "center",
-                    textAlignVertical: "center",
-                  }}
-                >
-                  :
-                </CustomText>
-              </View>
-              <View>
-                <CustomText>
-                  {languageService[user.language].seconds}
-                </CustomText>
-                <View>
-                  <CustomTextInput
-                    style={{ backgroundColor: appStyle.color_surface_variant }}
-                    textAlign="center"
-                    keyboardType="numeric"
-                    maxLength={2}
-                    onChangeText={(text) => handleSecondsChanged(text)}
-                  ></CustomTextInput>
-                </View>
-              </View>
+        <View className="h-1/3 items-center justify-center">
+          <View className="absolute bottom-5 w-full items-center justify-center">
+            <CustomText
+              style={{ color: onContainerColor }}
+              className="font-semibold text-3xl"
+            >{`${currentSetIndex + 1} out of ${
+              allSets.length
+            } total sets`}</CustomText>
+            <View
+              className="w-full"
+              style={{ height: 15, paddingHorizontal: 30 }}
+            >
+              <ProgressBar
+                previousValue={currentSetIndex}
+                value={currentSetIndex + 1}
+                total={allSets.length}
+                backgroundColor={appStyle.color_background}
+                color={
+                  isPlaying == true
+                    ? appStyle.color_primary
+                    : appStyle.color_success
+                }
+              />
             </View>
           </View>
-        }
-      />
-    </View>
+        </View>
+      </View>
+    )
   );
 };
 
