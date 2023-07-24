a0e345ed0d494c4ccded6c8415af65d1202d1abb
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-24 18:30:41 +0300

    animated progressbar with animation is working

---
+++ components/ProgressBar.js
@@ -0,0 +1,59 @@
+import { View } from "react-native";
+import React, { useEffect } from "react";
+import {
+  color_outline,
+  color_primary,
+  color_surface_variant,
+} from "../utils/appStyleSheet";
+import appComponentsDefaultStyles from "../utils/appComponentsDefaultStyles";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+
+const ProgressBar = ({
+  value,
+  previousValue,
+  total,
+  backgroundColor,
+  color,
+}) => {
+  const progress = useSharedValue(previousValue || 0);
+  const animatedListStyle = useAnimatedStyle(() => {
+    return {
+      width: `${progress.value / (total / 100)}%`,
+    };
+  }, []);
+  useEffect(() => {
+    console.log(value);
+    progress.value = withTiming(value);
+  }, [value]);
+
+  return (
+    <View
+      className="w-full h-full"
+      style={[
+        {
+          borderRadius: 10,
+          backgroundColor: backgroundColor,
+          borderWidth: 0.5,
+          borderColor: color_outline,
+        },
+      ]}
+    >
+      <Animated.View
+        className="h-full"
+        style={[
+          {
+            borderRadius: 10,
+            backgroundColor: color,
+          },
+          animatedListStyle,
+        ]}
+      ></Animated.View>
+    </View>
+  );
+};
+
+export default ProgressBar;
