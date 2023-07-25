ddef2d5bcf9216a837b5a41dea7bbf12c64ddebb
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-25 23:41:09 +0300

    so clean

--- screens/EditWorkoutProgramScreen.js
+++ screens/EditWorkoutProgramScreen.js
@@ -2,14 +2,10 @@ import { View } from "react-native";
 import React, { useCallback } from "react";
 import useNavbarDisplay from "../hooks/useNavbarDisplay";
 import { useFocusEffect } from "@react-navigation/native";
-import Header from "../components/Header";
-import useAuth from "../hooks/useAuth";
-import languageService from "../services/languageService";
 import EditingWorkoutProgram from "../components/workoutProgram/EditingWorkoutProgram";
 import { safeAreaStyle } from "../components/safeAreaStyle";
 const EditWorkoutProgramScreen = ({ route }) => {
   const { setCurrentScreen } = useNavbarDisplay();
-  const { user } = useAuth();
   useFocusEffect(
     useCallback(() => {
       setCurrentScreen("EditWorkoutProgram");
@@ -17,10 +13,6 @@ const EditWorkoutProgramScreen = ({ route }) => {
   );
   return (
     <View style={safeAreaStyle()}>
-      <Header
-        title={languageService[user.language].updateProgram}
-        goBackOption={true}
-      />
       <EditingWorkoutProgram program={route.params.program} />
     </View>
   );
