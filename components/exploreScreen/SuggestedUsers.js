2096cfa8a10dbfd2b26681b5809282112ca5a2b7
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-27 21:44:54 +0300

    translated and styled

--- components/exploreScreen/SuggestedUsers.js
+++ components/exploreScreen/SuggestedUsers.js
@@ -8,6 +8,9 @@ import * as firebase from "../../services/firebase";
 import useAuth from "../../hooks/useAuth";
 import { useNavigation } from "@react-navigation/native";
 import appComponentsDefaultStyles from "../../utils/appComponentsDefaultStyles";
+import { convertHexToRgba } from "../../utils/stylingFunctions";
+import languageService from "../../services/languageService";
+import LoadingAnimation from "../LoadingAnimation";
 const SuggestedUsers = ({ containerColor, onContainerColor }) => {
   const navigation = useNavigation();
   const { SuggestedUsers } = useExplore();
@@ -32,59 +35,90 @@ const SuggestedUsers = ({ containerColor, onContainerColor }) => {
         className="text-xl font-semibold"
         style={{ color: onContainerColor, marginHorizontal: 5 }}
       >
-        Suggested friends for you:
+        {languageService[user.language].suggestedFriends + ":"}
       </CustomText>
-      <FlatList
-        horizontal
-        data={SuggestedUsers}
-        keyExtractor={(suggestedUser) => suggestedUser.id}
-        contentContainerStyle={{
-          columnGap: 5,
-          marginHorizontal: 5,
-        }}
-        renderItem={({ item }) => (
-          <CustomButton
-            onPress={() => {
-              sentRequests.includes(item.id)
-                ? navigation.navigate("Profile", {
-                    shownUser: item,
-                    friendshipStatus: "SentRequest",
-                  })
-                : navigation.navigate("Profile", {
-                    shownUser: item,
-                    friendshipStatus: "None",
-                  });
-            }}
-            className="justify-start w-32"
-            style={[
-              {
-                borderColor: appStyle.color_outline,
-                borderWidth: 1,
-                rowGap: 5,
-                backgroundColor: appStyle.color_surface,
-              },
-              appComponentsDefaultStyles.shadow,
-            ]}
-          >
-            <Image
-              className="w-4/5 aspect-square rounded-full"
-              source={{ uri: item.img }}
-            />
-            <CustomText className="font-semibold ">{item.id}</CustomText>
-            {item.mutualFriendsCount != null && (
-              <CustomText>Mutual friends: {item.mutualFriendsCount}</CustomText>
-            )}
+      {SuggestedUsers == null ? (
+        <View style={{ width: "100%", height: 200 }}>
+          <LoadingAnimation />
+        </View>
+      ) : (
+        <FlatList
+          horizontal
+          data={SuggestedUsers}
+          keyExtractor={(suggestedUser) => suggestedUser.id}
+          contentContainerStyle={{
+            columnGap: 5,
+            marginHorizontal: 5,
+            paddingRight: 10,
+          }}
+          style={{ height: 200 }}
+          renderItem={({ item }) => (
             <CustomButton
-              onPress={() => sendFriendRequest(item.id)}
-              style={{ backgroundColor: appStyle.color_primary, width: "90%" }}
+              onPress={() => {
+                sentRequests.includes(item.id)
+                  ? navigation.navigate("Profile", {
+                      shownUser: item,
+                      friendshipStatus: "SentRequest",
+                    })
+                  : navigation.navigate("Profile", {
+                      shownUser: item,
+                      friendshipStatus: "None",
+                    });
+              }}
+              className="w-32"
+              style={[
+                {
+                  borderColor: appStyle.color_outline,
+                  borderWidth: 1,
+                  rowGap: 5,
+                  backgroundColor: appStyle.color_surface,
+                },
+                appComponentsDefaultStyles.shadow,
+              ]}
             >
-              <CustomText style={{ color: appStyle.color_on_primary }}>
-                Add
-              </CustomText>
+              <Image
+                className="w-4/5 aspect-square rounded-full"
+                source={{ uri: item.img }}
+              />
+              <View className="flex-1 items-center">
+                <CustomText className="font-semibold ">{item.id}</CustomText>
+                {item.mutualFriendsCount != null && (
+                  <CustomText
+                    style={{
+                      color: convertHexToRgba(
+                        appStyle.color_on_surface_variant,
+                        0.5
+                      ),
+                    }}
+                  >
+                    {languageService[user.language].mutualFriends +
+                      ": " +
+                      item.mutualFriendsCount}
+                  </CustomText>
+                )}
+              </View>
+              <CustomButton
+                round
+                onPress={() => sendFriendRequest(item.id)}
+                style={[
+                  {
+                    backgroundColor: appStyle.color_primary,
+                    width: "90%",
+                  },
+                  appComponentsDefaultStyles.shadow,
+                ]}
+              >
+                <CustomText
+                  className="font-semibold"
+                  style={{ color: appStyle.color_on_primary }}
+                >
+                  {languageService[user.language].add}
+                </CustomText>
+              </CustomButton>
             </CustomButton>
-          </CustomButton>
-        )}
-      />
+          )}
+        />
+      )}
     </View>
   );
 };
