0bcdd11704b3a406f8b70dbe98f1a18527c1fa65
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-27 22:25:38 +0300

    list loaded with animation

--- components/exploreScreen/SuggestedUsers.js
+++ components/exploreScreen/SuggestedUsers.js
@@ -1,5 +1,5 @@
 import { View, Text, FlatList, Image } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useExplore from "../../hooks/useExplore";
 import CustomButton from "../basic/CustomButton";
 import CustomText from "../basic/CustomText";
@@ -11,9 +11,14 @@ import appComponentsDefaultStyles from "../../utils/appComponentsDefaultStyles";
 import { convertHexToRgba } from "../../utils/stylingFunctions";
 import languageService from "../../services/languageService";
 import LoadingAnimation from "../LoadingAnimation";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 const SuggestedUsers = ({ containerColor, onContainerColor }) => {
   const navigation = useNavigation();
-  const { SuggestedUsers } = useExplore();
+  const { suggestedUsers } = useExplore();
   const { user } = useAuth();
   const [sentRequests, setSentRequests] = useState([]);
   const sendFriendRequest = (otherUserId) => {
@@ -22,104 +27,121 @@ const SuggestedUsers = ({ containerColor, onContainerColor }) => {
     setSentRequests(sentRequestsClone);
     firebase.sendFriendRequest(user.id, otherUserId);
   };
+  const listHeight = useSharedValue(0);
+  const listAnimatedStyle = useAnimatedStyle(() => {
+    return { height: listHeight.value, paddingVertical: listHeight.value / 25 };
+  });
+  useEffect(() => {
+    if (suggestedUsers == null || suggestedUsers.length == 0) return;
+    console.log(suggestedUsers.length);
+    listHeight.value = withTiming(250);
+  }, [suggestedUsers]);
   return (
-    <View
-      style={{
-        backgroundColor: containerColor,
-        marginTop: 16,
-        paddingVertical: 10,
-        rowGap: 5,
-      }}
+    <Animated.View
+      className="justify-center"
+      style={[
+        {
+          backgroundColor: containerColor,
+          marginTop: 16,
+          rowGap: 5,
+        },
+        listAnimatedStyle,
+      ]}
     >
       <CustomText
         className="text-xl font-semibold"
-        style={{ color: onContainerColor, marginHorizontal: 5 }}
+        style={{
+          color: onContainerColor,
+          marginHorizontal: 5,
+        }}
       >
         {languageService[user.language].suggestedFriends + ":"}
       </CustomText>
-      {SuggestedUsers == null ? (
-        <View style={{ width: "100%", height: 200 }}>
-          <LoadingAnimation />
-        </View>
-      ) : (
-        <FlatList
-          horizontal
-          data={SuggestedUsers}
-          keyExtractor={(suggestedUser) => suggestedUser.id}
-          contentContainerStyle={{
-            columnGap: 5,
-            marginHorizontal: 5,
-            paddingRight: 10,
-          }}
-          style={{ height: 200 }}
-          renderItem={({ item }) => (
+      <FlatList
+        horizontal
+        data={suggestedUsers}
+        keyExtractor={(suggestedUser) => suggestedUser.id}
+        contentContainerStyle={{
+          columnGap: 5,
+          marginHorizontal: 5,
+          paddingRight: 10,
+        }}
+        renderItem={({ item }) => (
+          <CustomButton
+            onPress={() => {
+              sentRequests.includes(item.id)
+                ? navigation.navigate("Profile", {
+                    shownUser: item,
+                    friendshipStatus: "SentRequest",
+                  })
+                : navigation.navigate("Profile", {
+                    shownUser: item,
+                    friendshipStatus: "None",
+                  });
+            }}
+            className="w-32"
+            style={[
+              {
+                borderColor: appStyle.color_outline,
+                borderWidth: 1,
+                rowGap: 5,
+                backgroundColor: appStyle.color_surface,
+              },
+              appComponentsDefaultStyles.shadow,
+            ]}
+          >
+            <Image
+              className="w-4/5 aspect-square rounded-full"
+              source={{ uri: item.img }}
+            />
+            <View className="flex-1 items-center">
+              <CustomText className="font-semibold ">{item.id}</CustomText>
+              {item.mutualFriendsCount != null && (
+                <CustomText
+                  style={{
+                    color: convertHexToRgba(
+                      appStyle.color_on_surface_variant,
+                      0.5
+                    ),
+                  }}
+                >
+                  {languageService[user.language].mutualFriends +
+                    ": " +
+                    item.mutualFriendsCount}
+                </CustomText>
+              )}
+            </View>
             <CustomButton
-              onPress={() => {
-                sentRequests.includes(item.id)
-                  ? navigation.navigate("Profile", {
-                      shownUser: item,
-                      friendshipStatus: "SentRequest",
-                    })
-                  : navigation.navigate("Profile", {
-                      shownUser: item,
-                      friendshipStatus: "None",
-                    });
-              }}
-              className="w-32"
+              round
+              onPress={() => sendFriendRequest(item.id)}
               style={[
                 {
-                  borderColor: appStyle.color_outline,
-                  borderWidth: 1,
-                  rowGap: 5,
-                  backgroundColor: appStyle.color_surface,
+                  backgroundColor: appStyle.color_primary,
+                  width: "90%",
                 },
                 appComponentsDefaultStyles.shadow,
               ]}
             >
-              <Image
-                className="w-4/5 aspect-square rounded-full"
-                source={{ uri: item.img }}
-              />
-              <View className="flex-1 items-center">
-                <CustomText className="font-semibold ">{item.id}</CustomText>
-                {item.mutualFriendsCount != null && (
-                  <CustomText
-                    style={{
-                      color: convertHexToRgba(
-                        appStyle.color_on_surface_variant,
-                        0.5
-                      ),
-                    }}
-                  >
-                    {languageService[user.language].mutualFriends +
-                      ": " +
-                      item.mutualFriendsCount}
-                  </CustomText>
-                )}
-              </View>
-              <CustomButton
-                round
-                onPress={() => sendFriendRequest(item.id)}
-                style={[
-                  {
-                    backgroundColor: appStyle.color_primary,
-                    width: "90%",
-                  },
-                  appComponentsDefaultStyles.shadow,
-                ]}
-              >
+              {sentRequests.includes(item.id) ? (
                 <CustomText
                   className="font-semibold"
                   style={{ color: appStyle.color_on_primary }}
                 >
                   {languageService[user.language].add}
                 </CustomText>
-              </CustomButton>
+              ) : (
+                <CustomText
+                  className="font-semibold"
+                  style={{ color: appStyle.color_on_primary }}
+                >
+                  {languageService[user.language].add}
+                </CustomText>
+              )}
             </CustomButton>
-          )}
-        />
-      )}
-    </View>
+          </CustomButton>
+        )}
+      />
+    </Animated.View>
   );
 };
 
