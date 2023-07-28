cd5c178a9ad738e3eaca4cae808e18a092d5807d
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-28 23:08:56 +0300

    added a zero workouts option

--- screens/SearchedWorkoutsScreen.js
+++ screens/SearchedWorkoutsScreen.js
@@ -4,20 +4,24 @@ import { safeAreaStyle } from "../components/safeAreaStyle";
 import Header from "../components/Header";
 import WorkoutComponent from "../components/WorkoutComponent";
 import useNavbarDisplay from "../hooks/useNavbarDisplay";
-import { useFocusEffect } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import languageService from "../services/languageService";
 import useAuth from "../hooks/useAuth";
-import { color_outline } from "../utils/appStyleSheet";
+import * as appStyle from "../utils/appStyleSheet";
+import CustomText from "../components/basic/CustomText";
+import CustomButton from "../components/basic/CustomButton";
 
 const SearchedWorkoutsScreen = ({ route }) => {
   const { user } = useAuth();
   const { setCurrentScreen } = useNavbarDisplay();
+  const navigation = useNavigation();
   useFocusEffect(
     useCallback(() => {
       setCurrentScreen("SearchedWorkouts");
     }, [])
   );
   const workouts = route.params.workouts;
+
   return (
     <View style={safeAreaStyle()}>
       <Header
@@ -25,22 +29,71 @@ const SearchedWorkoutsScreen = ({ route }) => {
         goBackOption={true}
       />
       <View className="flex-1">
-        <FlatList
-          data={workouts}
-          style={{
-            paddingTop: 5,
-            borderTopColor: color_outline,
-            borderTopWidth: 1,
-          }}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <WorkoutComponent
-              location={route.params.location}
-              workout={item}
-              isPastWorkout={false}
-            />
-          )}
-        />
+        {workouts.length == 0 ? (
+          <View
+            className="items-center gap-y-2"
+            style={{
+              paddingHorizontal: 16,
+              paddingVertical: 8,
+              backgroundColor: appStyle.color_surface_variant,
+            }}
+          >
+            <CustomText
+              className="text-center font-semibold text-lg"
+              style={{
+                color: appStyle.color_on_surface_variant,
+              }}
+            >
+              {languageService[user.language].noWorkoutsMatchedYourSearch}
+            </CustomText>
+            <View className="flex-row items-center gap-x-2">
+              <CustomButton
+                style={{ backgroundColor: appStyle.color_on_background }}
+                round
+                onPress={() => navigation.replace("SearchWorkouts")}
+              >
+                <CustomText
+                  className="font-semibold text-lg"
+                  style={{
+                    color: appStyle.color_background,
+                  }}
+                >
+                  {languageService[user.language].changeFilters}
+                </CustomText>
+              </CustomButton>
+              <CustomButton
+                round
+                onPress={() => navigation.replace("CreateWorkout")}
+                style={{ backgroundColor: appStyle.color_on_background }}
+              >
+                <CustomText
+                  className="font-semibold text-lg rounded-sm"
+                  style={{
+                    color: appStyle.color_background,
+                  }}
+                >
+                  {languageService[user.language].createWorkout}
+                </CustomText>
+              </CustomButton>
+            </View>
+          </View>
+        ) : (
+          <FlatList
+            data={workouts}
+            style={{
+              backgroundColor: appStyle.color_surface_variant,
+              paddingTop: 5,
+            }}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <WorkoutComponent
+                location={route.params.location}
+                workout={item}
+                isPastWorkout={false}
+              />
+            )}
+          />
+        )}
       </View>
     </View>
   );
