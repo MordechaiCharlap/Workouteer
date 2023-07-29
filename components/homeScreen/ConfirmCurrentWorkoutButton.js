fc74cdc5198202cb0d6be800c3a7120b228d62c7
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-30 01:41:02 +0300

    withSpring animation added to button

--- components/homeScreen/ConfirmCurrentWorkoutButton.js
+++ components/homeScreen/ConfirmCurrentWorkoutButton.js
@@ -1,35 +1,72 @@
 import { workoutTypes } from "../WorkoutType";
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import * as appStyle from "../../utils/appStyleSheet";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import useResponsiveness from "../../hooks/useResponsiveness";
 import CustomButton from "../basic/CustomButton";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 const ConfirmCurrentWorkoutButton = (props) => {
   const { windowHeight } = useResponsiveness();
   const navigation = useNavigation();
   buttonHeight = windowHeight / 10;
   const iconSize = windowHeight / 15;
+  const buttonPaddingTop = useSharedValue(80);
+  const buttonAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      paddingTop: buttonPaddingTop.value,
+    };
+  });
+  useFocusEffect(
+    useCallback(() => {
+      buttonPaddingTop.value = 80;
+      setTimeout(() => {
+        buttonPaddingTop.value = withSpring(0, {
+          damping: 100,
+          mass: 2,
+          stiffness: 100,
+          overshootClamping: true,
+        });
+      }, 700);
+    }, [])
+  );
   return (
-    <CustomButton
-      onPress={() => navigation.navigate("ConfirmWorkout")}
-      style={{
-        height: buttonHeight,
-        aspectRatio: 2,
-        borderTopRightRadius: 999,
-        borderTopLeftRadius: 999,
-        itemsAlign: "center",
-        justifyContent: "flex-end",
-        backgroundColor: appStyle.color_primary,
-        paddingBottom: 2,
-      }}
+    <Animated.View
+      style={[
+        {
+          bottom: 0,
+          width: "100%",
+          position: "absolute",
+          alignItems: "center",
+          height: buttonHeight,
+        },
+        buttonAnimatedStyle,
+      ]}
     >
-      <FontAwesomeIcon
-        icon={workoutTypes[props.currentWorkout.type].icon}
-        size={iconSize}
-        color={appStyle.color_on_primary}
-      />
-    </CustomButton>
+      <CustomButton
+        onPress={() => navigation.navigate("ConfirmWorkout")}
+        style={{
+          height: buttonHeight,
+          aspectRatio: 2,
+          borderTopRightRadius: 999,
+          borderTopLeftRadius: 999,
+          itemsAlign: "center",
+          justifyContent: "flex-end",
+          backgroundColor: appStyle.color_primary,
+          paddingBottom: 2,
+        }}
+      >
+        <FontAwesomeIcon
+          icon={workoutTypes[props.currentWorkout.type].icon}
+          size={iconSize}
+          color={appStyle.color_on_primary}
+        />
+      </CustomButton>
+    </Animated.View>
   );
 };
 
