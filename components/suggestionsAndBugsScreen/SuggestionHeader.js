6461b86d3dd4a8f6f3556a4f595d2c385368e87f
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-07-29 17:23:36 +0300

    SuggestionHeader created+implemented with logic

---
+++ components/suggestionsAndBugsScreen/SuggestionHeader.js
@@ -0,0 +1,64 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faMaximize,
+  faMinimize,
+  faTrash,
+  faX,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  color_on_primary_container,
+  color_on_surface_variant,
+  color_primary_container,
+  color_surface_variant,
+} from "../../utils/appStyleSheet";
+import CustomText from "../basic/CustomText";
+import CustomButton from "../basic/CustomButton";
+
+const SuggestionHeader = ({
+  maximized,
+  title,
+  maximizeSuggestion,
+  minimizeSuggestion,
+}) => {
+  return (
+    <View className="flex-row items-center justify-between">
+      <CustomText className="font-semibold text-xl">{title}</CustomText>
+      <View className="flex-row" style={{ columnGap: 5 }}>
+        {maximized && (
+          <CustomButton
+            className="rounded"
+            style={{ backgroundColor: color_surface_variant }}
+            onPress={maximized ? minimizeSuggestion : maximizeSuggestion}
+          >
+            <FontAwesomeIcon
+              icon={faTrash}
+              size={30}
+              color={color_on_surface_variant}
+            />
+          </CustomButton>
+        )}
+        <CustomButton
+          className="rounded"
+          style={{
+            backgroundColor: maximized
+              ? color_primary_container
+              : color_surface_variant,
+          }}
+          onPress={maximized ? minimizeSuggestion : maximizeSuggestion}
+        >
+          <FontAwesomeIcon
+            icon={maximized ? faMinimize : faMaximize}
+            size={30}
+            color={
+              maximized ? color_on_primary_container : color_on_surface_variant
+            }
+          />
+        </CustomButton>
+      </View>
+    </View>
+  );
+};
+
+export default SuggestionHeader;
