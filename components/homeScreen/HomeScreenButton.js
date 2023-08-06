88dc0e81312a0477e038e74bf5977bf5548e1caf
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-08-06 14:27:34 +0300

    number option added to hoomeScreenButton

--- components/homeScreen/HomeScreenButton.js
+++ components/homeScreen/HomeScreenButton.js
@@ -7,6 +7,7 @@ import AlertDot from "../AlertDot";
 import * as appStyle from "../../utils/appStyleSheet";
 import CustomButton from "../basic/CustomButton";
 import CustomText from "../basic/CustomText";
+import appComponentsDefaultStyles from "../../utils/appComponentsDefaultStyles";
 const HomeScreenButton = (props) => {
   const navigation = useNavigation();
   const { user } = useAuth();
@@ -19,48 +20,53 @@ const HomeScreenButton = (props) => {
         ></View>
       ) : (
         <CustomButton
-          style={{
-            backgroundColor: props.style.backgroundColor,
-            width: props.style.size,
-            height: props.style.size,
-            borderRadius: 10,
-            borderWidth: 0.5,
-            borderColor: appStyle.color_outline,
-          }}
+          style={[
+            {
+              backgroundColor: props.style.backgroundColor,
+              width: props.style.size,
+              height: props.style.size,
+              borderRadius: 10,
+              borderWidth: 0.5,
+              borderColor: appStyle.color_outline,
+            },
+            appComponentsDefaultStyles.shadow,
+          ]}
           onPress={() =>
             navigation.navigate(props.navigateScreen, { user: user })
           }
         >
-          {props.alert && (
+          {props.number != null && props.number != 0 && props.number != "0" ? (
             <View className="absolute left-2 top-2">
               <AlertDot
                 size={26}
-                text={props.alertNumber}
+                text={props.number}
                 textColor={appStyle.color_on_primary}
                 color={appStyle.color_primary}
               />
-            </View>
-          )}
-          {/* {props.number && (
-            <View className="absolute left-2 top-2" style={{ width: "30%" }}>
-              <AlertDot
-                size={26}
-                text={props.number < 100 ? props.number : "99+"}
-                textColor={appStyle.color_on_primary}
-                color={appStyle.color_primary}
-              />
-              {!props.alert && (
-                <View className="absolute left-0 bottom-0">
+              {props.alert && (
+                <View className="absolute right-0 bottom-0">
                   <AlertDot
                     size={10}
                     color={appStyle.color_error}
-                    borderWidth={1}
                     borderColor={props.style.backgroundColor}
+                    borderWidth={1.5}
                   />
                 </View>
               )}
             </View>
-          )} */}
+          ) : (
+            props.alert && (
+              <View className="absolute left-2 top-2">
+                <AlertDot
+                  size={26}
+                  text={props.alertNumber}
+                  textColor={appStyle.color_on_primary}
+                  color={appStyle.color_primary}
+                />
+              </View>
+            )
+          )}
+
           <FontAwesomeIcon
             icon={props.icon}
             size={props.style.iconSize}
