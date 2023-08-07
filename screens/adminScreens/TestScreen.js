277043f9f79818eacf36156513143c2569055cfc
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-08-07 12:57:27 +0300

    testing

--- screens/adminScreens/TestScreen.js
+++ screens/adminScreens/TestScreen.js
@@ -2,14 +2,27 @@ import { View, Text } from "react-native";
 import React, { useEffect, useState } from "react";
 import CustomButton from "../../components/basic/CustomButton";
 import CustomText from "../../components/basic/CustomText";
+import { color_on_primary, color_primary } from "../../utils/appStyleSheet";
+import useFirebase from "../../hooks/useFirebase";
+import { doc, updateDoc } from "firebase/firestore";
 const TestScreen = () => {
-  const [systemVolume, setSystemVolume] = useState(0);
-  const showVolume = () => {};
+  const { db } = useFirebase();
+  const testButtonFunction = () => {
+    updateDoc(doc(db, "alerts/charlap"), {
+      chats: {},
+      friendRequests: {},
+      workoutInvites: {},
+      workoutRequests: {},
+      newWorkouts: {},
+    });
+  };
   return (
-    <View className="flex-1 justify-center">
-      <CustomText className="text-center text-4xl">{systemVolume}</CustomText>
-      <CustomButton onPress={showVolume}>
-        <CustomText>TESTButton</CustomText>
+    <View className="flex-1 justify-center items-center">
+      <CustomButton
+        style={{ backgroundColor: color_primary }}
+        onPress={testButtonFunction}
+      >
+        <CustomText style={{ color: color_on_primary }}>TestButton</CustomText>
       </CustomButton>
     </View>
   );
