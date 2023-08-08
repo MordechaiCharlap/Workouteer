39ead924e8d423fac30608c2106da671064436cb
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-08-08 13:23:43 +0300

    animation connection modal added

--- hooks/useConnection.js
+++ hooks/useConnection.js
@@ -1,9 +1,42 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import NetInfo from "@react-native-community/netinfo";
+import { Modal, View } from "react-native";
+import CustomText from "../components/basic/CustomText";
+import Animated, {
+  FadeInDown,
+  FadeInUp,
+  FadeOutDown,
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 const ConnectionContext = createContext({});
 export const ConnectionProvider = ({ children }) => {
   const [connected, setConnected] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const connectedVal = useSharedValue(connected);
+  useEffect(() => {
+    if (!connected) {
+      connectedVal.value = connected;
+      setShowModal(true);
+    } else if (showModal) {
+      connectedVal.value = withTiming(connected);
+      setTimeout(() => {
+        setShowModal(false);
+      }, 1500);
+    }
+  }, [connected]);
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: interpolateColor(
+        connectedVal.value,
+        [true, false],
+        ["green", "black"]
+      ),
+    };
+  });
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener((state) => {
       setConnected(state.isConnected);
@@ -17,6 +50,25 @@ export const ConnectionProvider = ({ children }) => {
   return (
     <ConnectionContext.Provider value={{ connected }}>
       {children}
+      {showModal && (
+        <Animated.View
+          entering={FadeInDown.stiffness()}
+          exiting={FadeOutDown.stiffness()}
+          style={[
+            {
+              position: "absolute",
+              width: "100%",
+              bottom: 0,
+              paddingVertical: 8,
+            },
+            animatedStyle,
+          ]}
+        >
+          <CustomText style={{ color: "white", textAlign: "center" }}>
+            {!connected ? "App is offline" : "Back online"}
+          </CustomText>
+        </Animated.View>
+      )}
     </ConnectionContext.Provider>
   );
 };
