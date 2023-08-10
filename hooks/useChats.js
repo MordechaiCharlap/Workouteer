b0709f13d8c0cdb8b24b45bf44da6837e2adb159
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-08-10 23:18:31 +0300

    clean code

--- hooks/useChats.js
+++ hooks/useChats.js
@@ -1,23 +1,49 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import useAuth from "./useAuth";
-import { getChatsArrayIncludeUsers } from "../services/firebase";
 import useAlerts from "./useAlerts";
+import { getChat } from "../services/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import useFirebase from "./useFirebase";
 const ChatsContext = createContext({});
 export const ChatsProvider = ({ children }) => {
   const { userLoaded, user } = useAuth();
   const { chatsAlerts } = useAlerts();
+  const { db } = useFirebase();
   const [chats, setChats] = useState();
+
   const refreshChats = async () => {
-    var arr = await getChatsArrayIncludeUsers(user);
-    setChats(arr);
+    await getChatsArrayIncludeUsers(user);
   };
   useEffect(() => {
-    if (!userLoaded || !user) return;
-    const getChats = async () => {
-      var arr = await getChatsArrayIncludeUsers(user);
-      setChats(arr);
+    if (!user) return;
+    const getChatsArrayIncludeUsers = async () => {
+      const chatsArr = [];
+      for (var chatId of Object.keys(user.chats)) {
+        var chat = await getChat(chatId);
+        var chatToPush = {
+          chat: {
+            id: chatId,
+            ...chat,
+          },
+        };
+        if (!chat.isGroupChat) {
+          for (var key of Object.keys(chat.members)) {
+            if (key != user.id) {
+              chatToPush = {
+                ...chatToPush,
+                user: (await getDoc(doc(db, "users", key))).data(),
+              };
+            }
+          }
+        }
+        chatsArr.push(chatToPush);
+      }
+      chatsArr.sort(
+        (a, b) => b.chat.lastMessage.sentAt - a.chat.lastMessage.sentAt
+      );
+      setChats(chatsArr);
     };
-    getChats();
+    getChatsArrayIncludeUsers();
   }, [userLoaded, chatsAlerts, user?.chats]);
   return (
     <ChatsContext.Provider value={{ chats, setChats, refreshChats }}>
