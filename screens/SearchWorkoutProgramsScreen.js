1bd6b4369c32140c4b349a2d450f63f47e3aa694
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-08-16 17:24:42 +0300

    bugs fixed, translation added

--- screens/SearchWorkoutProgramsScreen.js
+++ screens/SearchWorkoutProgramsScreen.js
@@ -23,6 +23,8 @@ import CustomText from "../components/basic/CustomText";
 import CustomButton from "../components/basic/CustomButton";
 import appComponentsDefaultStyles from "../utils/appComponentsDefaultStyles";
 import Header from "../components/Header";
+import languageService from "../services/languageService";
+import LoadingAnimation from "../components/LoadingAnimation";
 
 const SearchWorkoutProgramsScreen = () => {
   const { setCurrentScreen } = useNavbarDisplay();
@@ -30,6 +32,7 @@ const SearchWorkoutProgramsScreen = () => {
   const navigation = useNavigation();
   const { db } = useFirebase();
   const [topPrograms, setTopPrograms] = useState([]);
+  const [initialLoading, setInitialLoading] = useState(true);
   const [searchResults, setSearchResults] = useState();
   useFocusEffect(
     useCallback(() => {
@@ -39,16 +42,17 @@ const SearchWorkoutProgramsScreen = () => {
   const setTopFivePrograms = () => {
     const topFiveQuery = query(
       collection(db, "workoutPrograms"),
-      where("isPublic", "==", "true"),
+      where("isPublic", "==", true),
       orderBy("currentUsersCount", "desc"),
       limit(5)
     );
     getDocs(topFiveQuery).then((snap) => {
       const arr = [];
       snap.forEach((doc) => {
-        arr.push(doc.data());
+        arr.push({ ...doc.data(), id: doc.id });
       });
       setTopPrograms(arr);
+      setInitialLoading(false);
     });
   };
   useEffect(() => {
@@ -56,82 +60,99 @@ const SearchWorkoutProgramsScreen = () => {
   }, []);
   return (
     <View className="flex-1">
-      <Header goBackOption={true} title={"Explore new programs"} />
-      <CustomText className="text-xl" style={{ marginHorizontal: 16 }}>
-        {!searchResults ? "Most popular programs:" : "Search results:"}
-      </CustomText>
-      {(searchResults == null && topPrograms.length == 0) ||
-      (searchResults.length == 0 && topPrograms.length == 0) ? (
-        <View className="flex-1 justify-center">
-          <View
-            className="self-center"
-            style={[
-              {
-                rowGap: 10,
-                borderRadius: 16,
-                borderWidth: 0.5,
-                borderColor: color_outline,
-                backgroundColor: color_surface,
-                padding: 16,
-              },
-              appComponentsDefaultStyles.shadow,
-            ]}
-          >
-            <CustomText className="font-semibold text-xl text-center">
-              No public workout programs found.
-            </CustomText>
-            <CustomButton
-              onPress={() =>
-                navigation.replace("WorkoutPrograms", {
-                  createProgramOnEntering: true,
-                })
-              }
-              round
-              style={{ backgroundColor: color_on_background }}
-            >
-              <CustomText
-                className="font-semibold"
-                style={{ color: color_background }}
-              >
-                Create your own
-              </CustomText>
-            </CustomButton>
-          </View>
-        </View>
+      <Header
+        goBackOption={true}
+        title={languageService[user.language].exploreNewPrograms}
+      />
+      {initialLoading ? (
+        <LoadingAnimation />
       ) : (
-        <FlatList
-          style={{}}
-          data={!searchResults ? topPrograms : searchResults}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <CustomButton
-              style={[
-                {
-                  marginHorizontal: 16,
-                  backgroundColor: color_surface_variant,
-                  marginVertical: 8,
-                },
-                appComponentsDefaultStyles.shadow,
-              ]}
-            >
-              <CustomText className="font-bold text-xl">{item.name}</CustomText>
-              <View className="w-full">
-                <View className="flex-row">
-                  <CustomText>Followers: </CustomText>
-                  <CustomText className="font-semibold">
-                    {item.currentUsersCount}
-                  </CustomText>
-                </View>
-                <View className="flex-row">
-                  <CustomText>Workouts: </CustomText>
-                  <CustomText className="font-semibold">
-                    {item.workouts.length}
-                  </CustomText>
-                </View>
-              </View>
-            </CustomButton>
+        <View className="flex-1">
+          {(topPrograms.length != 0 || searchResults != null) && (
+            <CustomText className="text-xl" style={{ marginHorizontal: 16 }}>
+              {!searchResults
+                ? languageService[user.language].mostPopularPrograms + ":"
+                : "Search results:"}
+            </CustomText>
           )}
-        />
+          {(searchResults == null && topPrograms.length == 0) ||
+          (searchResults != null &&
+            searchResults.length == 0 &&
+            topPrograms.length == 0) ? (
+            <View className="flex-1 justify-center">
+              <View
+                className="self-center"
+                style={[
+                  {
+                    rowGap: 10,
+                    borderRadius: 16,
+                    borderWidth: 0.5,
+                    borderColor: color_outline,
+                    backgroundColor: color_surface,
+                    padding: 16,
+                  },
+                  appComponentsDefaultStyles.shadow,
+                ]}
+              >
+                <CustomText className="font-semibold text-xl text-center">
+                  {languageService[user.language].noPublicProgramsFound}
+                </CustomText>
+                <CustomButton
+                  onPress={() =>
+                    navigation.replace("WorkoutPrograms", {
+                      createProgramOnEntering: true,
+                    })
+                  }
+                  round
+                  style={{ backgroundColor: color_on_background }}
+                >
+                  <CustomText
+                    className="font-semibold"
+                    style={{ color: color_background }}
+                  >
+                    {languageService[user.language].createYourOwn}
+                  </CustomText>
+                </CustomButton>
+              </View>
+            </View>
+          ) : (
+            <FlatList
+              style={{}}
+              data={!searchResults ? topPrograms : searchResults}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => (
+                <CustomButton
+                  style={[
+                    {
+                      marginHorizontal: 16,
+                      backgroundColor: color_surface_variant,
+                      marginVertical: 8,
+                    },
+                    appComponentsDefaultStyles.shadow,
+                  ]}
+                >
+                  <CustomText className="font-bold text-xl">
+                    {item.name}
+                  </CustomText>
+                  <View className="w-full">
+                    <View className="flex-row">
+                      <CustomText>Followers: </CustomText>
+                      <CustomText className="font-semibold">
+                        {item.currentUsersCount}
+                      </CustomText>
+                    </View>
+                    <View className="flex-row">
+                      <CustomText>Workouts: </CustomText>
+                      <CustomText className="font-semibold">
+                        {item.workouts.length}
+                      </CustomText>
+                    </View>
+                  </View>
+                </CustomButton>
+              )}
+            />
+          )}
+        </View>
       )}
     </View>
   );
