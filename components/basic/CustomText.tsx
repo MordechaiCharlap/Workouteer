16c8343b2a4ab9cae901772c79d943364660344e
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-08-24 19:03:50 +0300

    text props added

--- components/basic/CustomText.tsx
+++ components/basic/CustomText.tsx
@@ -10,8 +10,16 @@ import React from "react";
 interface CustomTextProps extends TextProps {
   style?: StyleProp<TextStyle>;
 }
-const CustomText: React.FC<CustomTextProps> = ({ children, style }) => {
-  return <Text style={[style]}>{children}</Text>;
+const CustomText: React.FC<CustomTextProps> = ({
+  children,
+  style,
+  ...restProps
+}) => {
+  return (
+    <Text {...restProps} style={[style]}>
+      {children}
+    </Text>
+  );
 };
 
 export default CustomText;
