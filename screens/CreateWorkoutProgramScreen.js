f069881e638a9e686dace1a0bdb79151944d4bbb
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-09-07 14:59:54 +0300

    WorkoutProgramProvider moved to hooks

--- screens/CreateWorkoutProgramScreen.js
+++ screens/CreateWorkoutProgramScreen.js
@@ -1,10 +1,10 @@
 import { View } from "react-native";
-import React, { createContext, useCallback } from "react";
+import React, { useCallback } from "react";
 import useNavbarDisplay from "../hooks/useNavbarDisplay";
 import { useFocusEffect } from "@react-navigation/native";
 import { safeAreaStyle } from "../components/safeAreaStyle";
 import EditingWorkoutProgram from "../components/workoutProgram/EditingWorkoutProgram";
-export const ProgramContext = createContext();
+import { WorkoutProgramProvider } from "../hooks/useWorkoutProgram";
 const CreateWorkoutProgramScreen = ({ route }) => {
   const { setCurrentScreen } = useNavbarDisplay();
   useFocusEffect(
@@ -14,7 +14,9 @@ const CreateWorkoutProgramScreen = ({ route }) => {
   );
   return (
     <View style={safeAreaStyle()}>
-      <EditingWorkoutProgram programName={route.params.programName} />
+      <WorkoutProgramProvider>
+        <EditingWorkoutProgram programName={route.params.programName} />
+      </WorkoutProgramProvider>
     </View>
   );
 };
