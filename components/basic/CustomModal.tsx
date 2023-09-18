ff66f3f05d9e3a192c8291d06e3cb1cab0e69f39
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2023-09-18 11:35:50 +0300

    modal is now working inside a child comps

--- components/basic/CustomModal.tsx
+++ components/basic/CustomModal.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { ViewProps, View, Text, TouchableOpacity } from "react-native";
+import {
+  ViewProps,
+  View,
+  Text,
+  TouchableOpacity,
+  Modal,
+  SafeAreaView,
+} from "react-native";
 import Animated, {
   FadeIn,
   FadeInLeft,
@@ -11,12 +18,17 @@ import CustomButton from "./CustomButton";
 import CustomText from "./CustomText";
 import languageService from "../../services/languageService";
 import useAuth from "../../hooks/useAuth";
+import useResponsiveness from "../../hooks/useResponsiveness";
 interface User {
   language: "string";
 }
 interface userState {
   user: User;
   // Add other properties here if needed
+}
+interface screenSize {
+  windowWidth: "double";
+  windowHeight: "double";
 }
 interface CustomModalProps extends ViewProps {
   showModal: boolean;
@@ -48,107 +60,109 @@ const CustomModal: React.FC<CustomModalProps> = ({
   ...restProps
 }) => {
   const { user } = useAuth() as userState;
+  const { windowWidth, windowHeight } = useResponsiveness() as screenSize;
   const language = user?.language;
   const clientStyle = restProps.style;
   const restPropsWithoutStyle = restProps;
   delete restPropsWithoutStyle.style;
   return (
     showModal && (
-      <Animated.View
-        style={{
-          position: "absolute",
-          width: "100%",
-          height: "100%",
-          backgroundColor: "rgba(0, 0, 0, 0.3)",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        entering={FadeIn.springify()}
-        exiting={FadeOut.springify()}
-      >
-        <TouchableOpacity
-          disabled={!closeOnTouchOutside}
-          style={{
-            height: "100%",
-            width: "100%",
-            position: "absolute",
-          }}
-          onPress={() => (closeOnTouchOutside ? setShowModal(false) : {})}
-        ></TouchableOpacity>
+      <Modal transparent animationType="fade">
         <Animated.View
-          entering={FadeInLeft.springify()}
-          exiting={FadeOutRight.springify()}
-          style={[
-            {
-              margin: 16,
-              borderRadius: 12,
-              backgroundColor: appStyle.color_surface,
-              padding: 8,
-              position: "absolute",
-            },
-            clientStyle,
-          ]}
-          {...restPropsWithoutStyle}
+          style={{
+            width: windowWidth,
+            height: windowHeight,
+            backgroundColor: "rgba(0, 0, 0, 0.3)",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          entering={FadeIn.springify()}
+          exiting={FadeOut.springify()}
         >
-          {children}
-          {(confirmButton || cancelButton) && (
-            <View
-              style={{
-                marginTop: 10,
-                flexDirection: "row",
-                columnGap: confirmButton && cancelButton && 10,
-              }}
-            >
-              {cancelButton && (
-                <CustomButton
-                  onPress={() => {
-                    setShowModal(false);
-                    onCancel && onCancel();
-                  }}
-                  round
-                  style={{
-                    flexGrow: 1,
-                    borderColor: appStyle.color_on_surface,
-                    borderWidth: 0.5,
-                    backgroundColor: cancelButtonColor,
-                  }}
-                >
-                  <CustomText>
-                    {cancelText
-                      ? cancelText
-                      : language
-                      ? languageService[language].cancel
-                      : languageService["english"].cancel}
-                  </CustomText>
-                </CustomButton>
-              )}
-              {confirmButton && (
-                <CustomButton
-                  onPress={() => {
-                    onConfirm && onConfirm();
-                  }}
-                  round
-                  style={{
-                    flexGrow: 1,
-                    borderColor: appStyle.color_on_surface,
-                    borderWidth: 0.5,
-                    backgroundColor:
-                      confirmButtonColor || appStyle.color_on_surface,
-                  }}
-                >
-                  <CustomText style={{ color: appStyle.color_surface }}>
-                    {confirmText
-                      ? confirmText
-                      : language
-                      ? languageService[language].confirm
-                      : languageService["english"].confirm}
-                  </CustomText>
-                </CustomButton>
-              )}
-            </View>
-          )}
+          <TouchableOpacity
+            disabled={!closeOnTouchOutside}
+            style={{
+              height: "100%",
+              width: "100%",
+              position: "absolute",
+            }}
+            onPress={() => (closeOnTouchOutside ? setShowModal(false) : {})}
+          ></TouchableOpacity>
+          <Animated.View
+            entering={FadeInLeft.springify()}
+            exiting={FadeOutRight.springify()}
+            style={[
+              {
+                margin: 16,
+                borderRadius: 12,
+                backgroundColor: appStyle.color_surface,
+                padding: 8,
+                position: "absolute",
+              },
+              clientStyle,
+            ]}
+            {...restPropsWithoutStyle}
+          >
+            {children}
+            {(confirmButton || cancelButton) && (
+              <View
+                style={{
+                  marginTop: 10,
+                  flexDirection: "row",
+                  columnGap: confirmButton && cancelButton && 10,
+                }}
+              >
+                {cancelButton && (
+                  <CustomButton
+                    onPress={() => {
+                      setShowModal(false);
+                      onCancel && onCancel();
+                    }}
+                    round
+                    style={{
+                      flexGrow: 1,
+                      borderColor: appStyle.color_on_surface,
+                      borderWidth: 0.5,
+                      backgroundColor: cancelButtonColor,
+                    }}
+                  >
+                    <CustomText>
+                      {cancelText
+                        ? cancelText
+                        : language
+                        ? languageService[language].cancel
+                        : languageService["english"].cancel}
+                    </CustomText>
+                  </CustomButton>
+                )}
+                {confirmButton && (
+                  <CustomButton
+                    onPress={() => {
+                      onConfirm && onConfirm();
+                    }}
+                    round
+                    style={{
+                      flexGrow: 1,
+                      borderColor: appStyle.color_on_surface,
+                      borderWidth: 0.5,
+                      backgroundColor:
+                        confirmButtonColor || appStyle.color_on_surface,
+                    }}
+                  >
+                    <CustomText style={{ color: appStyle.color_surface }}>
+                      {confirmText
+                        ? confirmText
+                        : language
+                        ? languageService[language].confirm
+                        : languageService["english"].confirm}
+                    </CustomText>
+                  </CustomButton>
+                )}
+              </View>
+            )}
+          </Animated.View>
         </Animated.View>
-      </Animated.View>
+      </Modal>
     )
   );
 };
